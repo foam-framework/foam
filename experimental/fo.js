2ecf2c493e041fe7a3370edcbbda1789143191a3
Adam Van Ymeren <adamvy@google.com>
2013-10-18 16:27:58 -0400

    Expand experimental feature oriented system.

--- experimental/fo.js
+++ experimental/fo.js
@@ -1,39 +1,58 @@
-function buildModel(modelName, model) {
-   model = model || {prototype:{}};
-
-   for ( var i = 0 ; i < features.length ; i++ ) {
-      var f = features[i];
-      if ( f[0] === modelName ) {
-         f.install && f.install(f[2], model);
-         f[1] === 'install' && f[2](f[2], model);
-      }
-   }
-
-   return model;
-}
-
 // TODO: bootstrap 'install'
 
 var features = [
-  ['Model',   'install',  function install(model, f) { scope[model.name] = model; }],
-  [null,       'Feature',  {name: 'Feature'}],
-  ['Feature',  'Property', {name: 'name'}],
+  ['Model',    'Method',  function create(args) {
+    var obj = {
+      model_: this,
+      __proto__: this.prototype,
+      instance_: {}
+    };
+    for (var key in args) {
+      obj[key] = obj.instance_[key] = args[key];
+    }
+    return obj;
+  }],
+  ['Model',    'Method',  function install(o) {
+    o[this.name] = this;
+  }],
+  [null,       'Model', { name: 'Property' }],
+  ['String', 'Method' ,function constantize() { return this.replace(/[a-z][^a-z]/g, function(a) { return a.substring(0,1) + '_' + a.substring(1,2); }).toUpperCase(); } ],
+  ['Property', 'Method', function install(o) {
+    o.model_[this.name.constantize()] = this;
+    var prop = this;
+    Object.defineProperty(o.prototype, this.name, {
+      configurable: false,
+      enumerable: true,
+      writeable: true,
+      get: function() {
+        if ( !this.instance_.hasOwnProperty(prop.name) ) return prop.defaultValue;
+        return this.instance_[prop.name];
+      },
+      set: function(value) {
+        this.instance_[prop.name] = value;
+      }
+    });
+    if ( !o.instance_.hasOwnProperty(this.name) && this.valueFactory ) o[this.name] = this.valueFactory();
+  }],
+  [null,       'Model',    { name: 'Feature' }],
+  ['Feature',  'Property', { name: 'name' }],
+  ['Property', 'Property', { name: 'defaultValue', defaultValue: '' }],
+  [null,       'Model', { name: 'Extends' }],
+  ['Extends',  'Method',  function install(o) {
+    build(o, o.model_.name);
+  }],
+  ['Model', 'Property', { name: 'name' }],
+  ['Property', 'Method', function f(obj) { return obj[this.name]; } ],
+/*  [null,       'Model',    { name: 'Property' }],
 //  [null,       'Model',    {name: 'Model' }],
   ['Model',    'Extends',  'Feature' ],
-  [null,       'Model',    {name: 'Property'}],
+  [null,       'Model',    { name: 'Property' }],
   ['Property', 'Extends',  'Feature' ],
   ['Property', 'Property', { name: 'defaultValue' }],
-  ['Extends',  'install',  function install(model, f) { buildModel(f, model); }],
   ['Method',   'Extends',  'Feature' ],
 //  ['Method',   'install',  function install(model, f) { model.prototype[f.name] = f; }],
   ['Model',    'Method',   function create() { return {__proto__: this.prototype}; }],
-  ['Feature',  'Method',   function toString() { return this.name; }]
-/*
-  ['', '', { }],
-  ['', '', { }],
-  ['', '', { }],
-  ['', '', { }],
-*/
+  ['Feature',  'Method',   function toString() { return this.name; }]*/
 ];
 
 
@@ -53,20 +72,30 @@ function expandFeature(f, a, prefix) {
 function build(scope, opt_whereModel) {
    for ( var i = 0 ; i < features.length ; i++ ) {
       var f = features[i];
-      var model = (f[0] && (scope[f0] || scope[f0] = {})) || scope;
-      var fname = f[1];
-      var feature = f[2];
-      var install = scope[fname] && scope[fname].install;
+      if ( opt_whereModel && f[0] !== opt_whereModel ) continue;
+      var model = f[0] ? (scope[f[0]] ? scope[f[0]] : scope[f[0]] = {}) : scope;
 
-      install && install(model, feature);
+      var fname = f[1];
+      if ( !scope[fname] ) continue;
+
+      var args = f[2];
+      var feature = scope[fname].create(args);
+      feature.install(model);
    }
 }
 
-
 var scope = {
-  Model:  { name: 'Model' },
-  Method: { install: function(scope, fn) { scope.prototype[fn.name] = fn; } }
+  String: { prototype: String.prototype },
+  Model:  { name: 'Model', instance_: { name: 'Model' }, prototype: {} },
+  Method: {
+    install: function(scope) {
+      scope.prototype[this.name] = this;
+    },
+    create: function(args) { args.install = this.install; return args; }
+  }
 };
 
+scope.Model.__proto__ = scope.Model.prototype;
+scope.Model.model_ = scope.Model;
 
 build(scope);
