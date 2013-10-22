7bb59d7de5098fb56dfc191846cdf3e461ffa9fe
Adam Van Ymeren <adamvy@google.com>
2013-10-22 12:11:36 -0400

    More feature work.  Fixed up extends, added bootstrapping of a real Method model

--- experimental/fo.js
+++ experimental/fo.js
@@ -16,12 +16,44 @@
  */
 // TODO: bootstrap 'install'
 
+var CTX = {
+  __proto__: window,
+
+  FObject: { name: 'FObject', prototype: {}, features: [], instance_: {} },
+  Model:  { name: 'Model', prototype: {}, features: [], instance_: {} },
+  Method: {
+    prototype: {
+      create: function(args) {
+        args.install = function(o) {
+          if ( !o.features ) o.features = [this];
+          else o.features = o.features.concat(this);
+          o.prototype[this.name] = this;
+          if (o.prototype.__proto__[this.name]) {
+            o.prototype[this.name].super_ = o.prototype.__proto__[this.name];
+          }
+        };
+        args.initialize = function(){};
+        return args;
+      }
+    }
+  }
+};
+
+CTX.Model.__proto__ = CTX.Model.prototype;
+CTX.Model.__proto__.__proto__ = CTX.FObject.prototype;
+CTX.Model.model_ = CTX.Model;
+CTX.FObject.model_ = CTX.Model;
+CTX.FObject.__proto__ = CTX.Model.prototype;
+
 var features = [
-//  [null,         'Model', { name: 'FObject' }],
+  //  [null,         'Model', { name: 'FObject' }],
   ['FObject',    'Method',  function copyFrom(src) {
     for ( var key in src ) {
+      // This is a workaround.  We should define model_ as a non-cloneable property.
+      if ( key === 'model_' ) continue;
       this[key] = src[key];
     }
+    return this;
   }],
   ['FObject',    'Method',  function create(args) {
     var obj = {
@@ -34,10 +66,20 @@ var features = [
       instance_: {}
     };
     obj.copyFrom(args);
+    obj.init(args);
     return obj;
   }],
+  ['FObject',    'Method',  function init(args) {
+    var features = this.model_.features;
+    if ( ! features ) return;
+    for (var i = 0; i < features.length; i++) {
+      var feature = features[i];
+      feature.initialize(this, args);
+    }
+  }],
+  ['FObject',    'Method',  function initialize() {} ],
   ['FObject',    'Method',  function clone() {
-    return this.model_.create ? this.mdoel_.create(this) : this;
+    return this.model_.create ? this.model_.create(this) : this;
   }],
   ['FObject',    'Method',  function toString() {
     return this.TYPE;
@@ -46,7 +88,15 @@ var features = [
     if ( ! this.features ) return;
     for ( var i = 0; i < this.features.length; i++ ) {
       var feature = this.features[i];
-      if ( CTX['Property'].isInstance(feature) ) return feature;
+      if ( CTX['Property'].isInstance(feature) &&
+          feature.name === name ) return feature;
+    }
+  }],
+  ['FObject',  'Method',  function getExtends() {
+    if ( ! this.features ) return;
+    for ( var i = 0; i < this.features.length; i++ ) {
+      var feature = this.features[i];
+      if ( CTX['Extends'] && CTX['Extends'].isInstance(feature) ) return feature;
     }
   }],
   ['Model',    'Method',  function install(o) {
@@ -65,16 +115,15 @@ var features = [
   [null,       'Model', { name: 'Property' }],
   ['String',   'Method', function constantize() { return this.replace(/[a-z][^a-z]/g, function(a) { return a.substring(0,1) + '_' + a.substring(1,2); }).toUpperCase(); } ],
   ['Property', 'Method', function install(o) {
-    if ( ! o.features ) o.features = [this];
-    else {
-      // TODO this is broken, need to get property from the actual
-      // super class.
-      var superProp = o.getProperty(this.name);
-      if (superProp) {
-        superProp = superProp.clone();
-        this.copyFrom(superProp);
-      }
+    var existing = o.getProperty(this.name);
+
+    if ( ! existing ) {
+      if ( ! o.features ) o.features = [this];
+      else o.features = o.features.concat(this);
+    } else {
+      this.copyFrom(existing.clone().copyFrom(this));
     }
+
     o[this.name.constantize()] = this;
 
     var prop = this;
@@ -89,38 +138,101 @@ var features = [
           this.instance_[prop.name] = value;
         };
     Object.defineProperty(o.prototype, this.name, {
-      configurable: false,
-      enumerable: true,
+      configurable: true,
+      enumerable: this.enumerable === undefined ? true : this.enumerable,
       writeable: true,
       get: get,
       set: set
     });
   }],
+  ['Property', 'Property', { name: 'defaultValue', defaultValue: '' }],
+  ['Property', 'Property', { name: 'valueFactory', defaultValue: '' }],
+  ['Property', 'Property', { name: 'enumerable', defaultValue: true }],
+  ['Property', 'Method', function initialize(o) {
+    if ( ! o.instance_.hasOwnProperty(this.name) &&
+        this.valueFactory ) o.instance_[this.name] = this.valueFactory();
+  }],
   ['FObject',  'Property',
    { name: 'SUPER',
      getter: function f() { return f.caller.super_.bind(this); },
-     setter: function() {} }],
-  ['Model',    'Property', { name: 'features' }],
+     setter: function() {},
+     enumerable: false }],
+  ['Model',    'Property', { name: 'features',
+                             valueFactory: function() { return []; } }],
   [null,       'Model',    { name: 'Feature' }],
   ['Feature',  'Property', { name: 'name' }],
-  ['Property', 'Property', { name: 'defaultValue', defaultValue: '' }],
+
   [null,       'Model', { name: 'Extends' }],
   ['Extends',  'Property', { name: 'model' }],
   ['Extends',  'Method', function create(model) {
     return this.SUPER({ model: CTX[model] });
   }],
   ['Extends',  'Method',  function install(o) {
+    o.features = o.features.concat(this);
     for ( var i = 0; i < this.model.features.length; i++ ) {
-      this.model.features[i].install(o);
+      this.model.features[i].clone().install(o);
     }
   }],
-  ['Feature', 'Property', { name: 'asdf' }],
-  ['Model', 'Property', { name: 'asdf', defaultValue: '12' }],
-  ['Model', 'Extends', 'Feature'],
-  ['Property', 'Extends', 'Feature' ],
-  ['Method', 'Extends', 'Feature' ],
-];
 
+
+  ['Model', 'Extends', 'Feature'],
+  ['Property', 'Extends', 'Feature'],
+
+  [null, 'Model', {
+    name: 'Method',
+    prototype: {
+      __proto__: CTX['FObject'].prototype,
+
+      create: function(args) {
+        var obj = CTX['FObject'].create(args);
+        obj.install = function(o) {
+          o.prototype[this.name] = this.jsCode;
+          if (o.prototype.__proto__[this.name]) {
+            o.prototype[this.name].super_ = o.prototype.__proto__[this.name];
+          }
+        };
+        return obj;
+      }
+    }
+  }],
+  ['Method', 'Property', { name: 'jsCode' }],
+  ['Method', 'Method', {
+    name: 'install',
+    jsCode: function(o) {
+      if ( !o.features ) o.features = [this];
+      else o.features = o.features.concat(this)
+      o.prototype[this.name] = this.jsCode;
+      if (o.prototype.__proto__[this.name]) {
+        o.prototype[this.name].super_ = o.prototype.__proto__[this.name];
+      }
+    }
+  }, true],
+  ['Method', 'Method', {
+    name: 'create',
+    jsCode: function(args) {
+      if ( args instanceof Function ) args = {
+        name: args.name,
+        jsCode: args
+      };
+
+      return this.SUPER(args);
+    }
+  }],
+  ['Method', 'Extends', 'Feature'],
+
+  [null, 'Model', { name: 'AMail' }],
+  ['AMail', 'Property', {
+    name: 'from',
+    defaultValue: "adamvy@google.com",
+    help: "say what" }],
+  ['AMail', 'Method', function send() { console.log('sending from: ' + this.from); }],
+
+  [null, 'Model', { name: 'KMail' }],
+  ['KMail', 'Extends', 'AMail'],
+  ['KMail', 'Property', {
+    name: 'from',
+    defaultValue: "kgr@google.com" }],
+];
 
 function expandFeatures(f, opt_prefix) {
   var prefix = opt_prefix || [];
@@ -153,29 +265,11 @@ function build(scope, opt_whereModel) {
    }
 }
 
-var CTX = {
-  __proto__: window,
 
-  FObject: { name: 'FObject', prototype: {}, features: [], instance_: {} },
-  Model:  { name: 'Model', prototype: {}, features: [], instance_: {} },
-  Method: {
-    prototype: {
-      create: function(args) {
-        args.install = function(o) {
-          o.prototype[this.name] = this;
-          if (o.prototype.__proto__[this.name]) {
-            o.prototype[this.name].super_ = o.prototype.__proto__[this.name];
-          }
-        };
-        return args;
-      }
-    }
-  }
-};
-
-CTX.Model.__proto__ = CTX.Model.prototype;
-CTX.Model.__proto__.__proto__ = CTX.FObject.prototype;
-CTX.FObject.__proto__ = CTX.Model.prototype;
-CTX.Model.model_ = CTX.Model;
-CTX.FObject.model_ = CTX.Model;
 build(CTX);
+
+var amail = CTX.AMail.create();
+var kmail = CTX.KMail.create();
+
+amail.send();
+kmail.send();
