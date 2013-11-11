3b049811245e90985a3980c1f758edb38364ead9
Kevin Greer <kgr@chromium.org>
2013-11-10 22:18:16 -0500

    Moved to new directory structure.

---
+++ core/FOAMmodels.js
@@ -0,0 +1,69 @@
+/**
+ * @license
+ * Copyright 2012 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+var files = [
+  'stdlib',
+  'io',
+  'writer',
+  'socket',
+  'hash',
+  'base64',
+  'utf8',
+  'parse',
+//  'scaleParser',
+  'context',
+//  'context2',
+  'event',
+  'JSONUtil',
+  'XMLUtil',
+  'FOAM',
+  'TemplateUtil',
+  'AbstractPrototype',
+  'ModelProto',
+  'mm1Model',
+  'mm2Property',
+  'mm3Types',
+  'mm4Method',
+  'mm5Misc',
+  'mm6Protobuf',
+  'view',
+  'RichTextView',
+  'listchoiceview',
+  'scroll',
+  'mlang',
+  'QueryParser',
+  'search',
+  'async',
+//  'async_test',
+  'visitor',
+  'dao',
+  'diff',
+  'SplitDAO',
+  'index',
+  'StackView',
+  'DAOController',
+  'ThreePaneController',
+  'experimental/protobufparser',
+  'experimental/protobuf',
+  // remove below here
+  'models',
+  'glang',
+  '../demos/demos',
+  'oauth',
+  '../apps/mailreader/view',
+  '../apps/mailreader/email',
+  'turntable'
+];
