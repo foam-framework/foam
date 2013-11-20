e38da0e3f22d4d3de949dbca7f56413f1d2271bf
Kevin Greer <kgr@chromium.org>
2013-11-20 15:53:08 -0500

    Improved quickcompose layout.

--- apps/quickcompose/bg.js
+++ apps/quickcompose/bg.js
@@ -1,5 +1,5 @@
 function launchComposer() {
-  chrome.app.window.create('empty.html', {width: 365, height: 472}, function(w) {
+  chrome.app.window.create('empty.html', {width: 365, height: 489}, function(w) {
     w.contentWindow.onload = function() {
       var window = self.window = w.contentWindow;
       aseq(
