f819c876a25b2eee32a57daff72c04c667744f73
Kevin Greer <kgr@chromium.org>
2013-11-20 11:24:24 -0500

    Initial check-in of new app 'QuickCompose'.

---
+++ apps/quickcompose/QuickCompose.js
@@ -0,0 +1,85 @@
+/**
+ * @license
+ * Copyright 2013 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+var QuickEMail = Model.create({
+  name: 'QuickEMail',
+});
+
+
+var QuickEMailView = Model.create({
+  name: 'QuickEMailView',
+
+  extendsModel: 'DetailView',
+
+  templates: [
+    {
+      name: "toHTML",
+      description: "",
+      template: '<div id="<%= this.getID() %>">' +
+        '<%= this.createView(QuickEMail.TO).toHTML() %>' +
+        '<%= this.createView(QuickEMail.SUBJECT).toHTML() %>' +
+        '<%= this.createView(QuickEMail.BODY).toHTML() %>' +
+        '</div>';
+    }
+  ]
+});
+
+
+var QuickCompose = Model.create({
+  name: 'QuickCompose',
+
+  properties: [
+    {
+      name: 'window'
+    },
+    {
+      name: 'email',
+      valueFactory: function() { return QuickEMail.create(); }
+    },
+    {
+      name: 'view',
+      valueFactory: function() {
+        return QuickEmailView.create({
+          model: QuickEMail
+        });
+      }
+    },
+    {
+      name: 'EMailDAO',
+      defaultValueFn: function() { return EMailDAO; }
+    },
+    {
+      name: 'ContactDAO',
+      defaultValueFn: function() { return ContactDAO; }
+    },
+  ],
+
+  methods: {
+    initHTML: function() {
+      this.view.value = this.propertyValue('email');
+      this.view.initHTML();
+    }
+  },
+
+  templates: [
+    {
+      name: "toHTML",
+      description: "",
+      template: "<html><head><link rel=\"stylesheet\" type=\"text/css\" href=\"foam.css\" /><link rel=\"stylesheet\" type=\"text/css\" href=\"quickcompose.css\" />\u000a  <title>Quick Compose</title></head><body><%= this.view.toHTML() %></body>\u000a</html>"
+    }
+  ]
+});
---
+++ apps/quickcompose/bg.js
@@ -0,0 +1,24 @@
+function launchComposer() {
+  chrome.app.window.create('empty.html', {width: 400, height: 500}, function(w) {
+    w.contentWindow.onload = function() {
+      var window = self.window = w.contentWindow;
+      apar(
+        arequire('QuickCompose'),
+      )(function () {
+        var b = QuickCompose.create({window: window});
+        window.browser = b; // for debugging
+        $addWindow(window);
+        window.document.body.innerHTML = b.toHTML();
+        b.initHTML();
+        w.focus();
+      });
+    };
+    w.onClosed.addListener(function() {
+      $removeWindow(self.window);
+    });
+ });
+}
+
+if ( chrome.app.runtime ) {
+  chrome.app.runtime.onLaunched.addListener(launchComposer));
+}
