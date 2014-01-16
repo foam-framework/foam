bae520f4bb6061735c6f32dcfb2780a98f817f57
Kevin Greer <kgr@chromium.org>
2014-01-16 14:12:55 -0500

    Crbug: Added url_handler to manifest.

--- apps/crbug/bg.js
+++ apps/crbug/bg.js
@@ -31,9 +31,9 @@ function launchController() {
 }
 
 if ( chrome.app.runtime ) {
-  chrome.app.runtime.onLaunched.addListener(function() {
+  chrome.app.runtime.onLaunched.addListener(function(opt_launchData) {
     console.log('launched');
+    if ( opt_launchData ) console.log(opt_launchData.url);
     launchController();
   });
-
 }
