f6c3ed3224cb00468de8dec991245d605286d764
Adam Van Ymeren <adamvy@localhost>
2014-01-23 16:47:29 -0500

    Add authentication to crbug when running as a packaged app.

--- apps/crbug/bg.js
+++ apps/crbug/bg.js
@@ -5,6 +5,20 @@ function launch() {
 }
 
 if ( chrome.app.runtime ) {
+  ajsonp = (function() {
+    var factory = OAuthXhrFactory.create({
+      authAgent: ChromeAuthAgent.create({}),
+      responseType: "json"
+    });
+
+    return function(url, params) {
+      return function(ret) {
+        var xhr = factory.make();
+        return xhr.asend(ret, "GET", url + (params ? '?' + params.join('&') : ''));
+      };
+    };
+  })();
+
   chrome.app.runtime.onLaunched.addListener(function(opt_launchData) {
     console.log('launched');
     if ( opt_launchData ) console.log(opt_launchData.url);
