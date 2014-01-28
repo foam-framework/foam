76d2439c3bf15e33e3e013514973fafa777ffcbd
Adam Van Ymeren <adamvy@localhost>
2014-01-28 15:11:51 -0500

    Import IssueCommentUpdate from rosy schema

--- apps/crbug/ImportedModels.js
+++ apps/crbug/ImportedModels.js
@@ -290,3 +290,65 @@ var Project = FOAM({
       }
    ]
 });
+var IssueCommentUpdate = FOAM({
+   "model_": "Model",
+   "name": "IssueCommentUpdate",
+   "tableProperties": [
+      "blockedOn",
+      "blocking",
+      "cc",
+      "kind",
+      "labels",
+      "mergedInto",
+      "owner",
+      "status",
+      "summary"
+   ],
+   "properties": [
+      {
+         "model_": "StringArrayProperty",
+         "name": "blockedOn",
+         "help": "Changes made to the list of issues blocked on this issue."
+      },
+      {
+         "model_": "StringArrayProperty",
+         "name": "blocking",
+         "help": "Changes made to the list of issues blocking this issue."
+      },
+      {
+         "model_": "StringArrayProperty",
+         "name": "cc",
+         "help": "Changes made to the issue's cc list."
+      },
+      {
+         "model_": "StringProperty",
+         "name": "kind",
+         "help": "Metadata updates made as part of a comment."
+      },
+      {
+         "model_": "StringArrayProperty",
+         "name": "labels",
+         "help": "Changes made to the issue's labels."
+      },
+      {
+         "model_": "StringProperty",
+         "name": "mergedInto",
+         "help": "ID of the issue this issue has been merged into."
+      },
+      {
+         "model_": "StringProperty",
+         "name": "owner",
+         "help": "Updated owner of the issue."
+      },
+      {
+         "model_": "StringProperty",
+         "name": "status",
+         "help": "Updated status of the issue."
+      },
+      {
+         "model_": "StringProperty",
+         "name": "summary",
+         "help": "Updated summary of the issue."
+      }
+   ]
+});
