2e0979a51cdfe7485fa2950462e76314d6c139f0
Kevin Greer <kgr@chromium.org>
2014-01-29 11:10:38 -0500

    Crbug: Added ability to launch Sync Manager dialog.

--- core/SyncManager.js
+++ core/SyncManager.js
@@ -1,6 +1,6 @@
 /**
  * @license
- * Copyright 2012 Google Inc. All Rights Reserved.
+ * Copyright 2014 Google Inc. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -66,8 +66,7 @@ var SyncManager = FOAM({
       },
       {
          model_: 'StringProperty',
-         name:  'syncStatus',
-         displayWidth: 60
+         name:  'syncStatus'
       },
       {
          model_: 'IntegerProperty',
