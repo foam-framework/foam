56993d2d70f0ddee44c2d9d713ab9115da6173d8
Arthur Hsu <arthurhsu@chromium.org>
2014-02-12 15:36:17 -0800

    Remove/Commented out console.log to reduce noise for clients

--- core/utf8.js
+++ core/utf8.js
@@ -19,7 +19,6 @@ String.fromCharCode = (function() {
   var lookupTable = [];
   return function(a) {
     if (arguments.length == 1) return lookupTable[a] || (lookupTable[a] = oldLookup(a));
-console.log('.');
     var result = "";
     for (var i = 0; i < arguments.length; i++) {
       result += lookupTable[arguments[i]] || (lookupTable[arguments[i]] = oldLookup(arguments[i]));
