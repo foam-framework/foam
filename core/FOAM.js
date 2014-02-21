54e43346cf6b95e4c45617a6f42c740f3f96975a
Kevin Greer <kgr@chromium.org>
2014-02-21 12:51:04 -0500

    Improved FOAM_POWERED formatting.

--- core/FOAM.js
+++ core/FOAM.js
@@ -94,6 +94,6 @@ FOAM.browse = function(model, opt_dao) {
 };
 
 var FOAM_POWERED = '<a style="text-decoration:none;" href="http://code.google.com/p/foam-framework/" target="_blank">\
-<font size=+1 face="catull" style="padding-left:10px;text-shadow:rgba(64,64,64,0.3) 3px 3px 4px;">\
+<font size=+1 face="catull" style="text-shadow:rgba(64,64,64,0.3) 3px 3px 4px;">\
 <font color="#3333FF">F</font><font color="#FF0000">O</font><font color="#FFCC00">A</font><font color="#33CC00">M</font>\
 <font color="#555555" > POWERED</font></font></a>';
