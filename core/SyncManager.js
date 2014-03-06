c063eedf683959877b7989f352a8c45997849359
Kevin Greer <kgr@chromium.org>
2014-03-06 15:55:48 -0500

    Crbug: Added new toolbar icons.

--- core/SyncManager.js
+++ core/SyncManager.js
@@ -200,7 +200,7 @@ var SyncManager = FOAM({
       this.dstDAO.select(MAX(this.modifiedProperty))(function (max) {
         if ( max.max ) self.lastModified = max.max;
         // Postpone starting until we have the lastModified timestamp
-        self.start();
+        // self.start();
       });
     },
 
