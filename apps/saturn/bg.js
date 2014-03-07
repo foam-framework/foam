85040921594d2d397383dca74a801a43b197499f
Mike Carcasole <mcarcaso@google.com>
2014-03-07 11:23:44 -0500

    Removing duplicate arequire.

--- apps/saturn/bg.js
+++ apps/saturn/bg.js
@@ -71,7 +71,6 @@ menu.addActions(FOAM([
 openComposeView = function(email) {
   aseq(arequire('QuickEMailView'),
     arequire('QuickCompose'),
-    arequire('QuickEMailView'),
     arequire('LinkView'),
     arequire('RichTextView'),
     arequire('ContactListTileView'),
