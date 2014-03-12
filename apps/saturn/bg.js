aaba6058fd2d6685a56cbe815c44cc33f432274b
Adam Van Ymeren <adamvy@google.com>
2014-03-12 11:55:39 -0400

    Remove duplicate stack view from saturn, re-use the one in core.

--- apps/saturn/bg.js
+++ apps/saturn/bg.js
@@ -172,39 +172,6 @@ EMail.ATTACHMENTS.compare = function(o1, o2) {
 
 var MIN_SEARCH_W = 40;
 
-// TODO: I would rather have the default semantics be to open a window and then have that
-// revert to a stack in situations where it is necessary.
-var stack = {
-  openWindow: function(view, width, height, opt_cb) {
-     var self = this;
-     chrome.app.window.create('empty.html', {width: width, height: height}, function(w) {
-       w.contentWindow.onload = function() {
-         self.window = w.contentWindow;
-         $addWindow(w.contentWindow);
-         view.window = w.contentWindow;
-         self.window.document.body.innerHTML = view.toHTML();
-         view.initHTML();
-         w.focus();
-         opt_cb && opt_cb(self.window);
-       };
-       w.onClosed.addListener(function() {
-         $removeWindow(self.window);
-       });
-     });
-  },
-  pushView: function(view, name, cb) {
-     // this.window = window.open('','','width=700,height=600');
-     this.openWindow(view, 900, 800, cb);
-  },
-  back: function() {
-    this.window.close();
-    this.window = undefined;
-  },
-  setPreview: function() {
-    /* nop */
-  }
-};
-
 function openMenu(e) {
   var doc = e.toElement.ownerDocument;
   menu.document = doc;
