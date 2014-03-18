76ff103aff4609ff2f6c6429d1ebf57baa0b6bef
Kevin Greer <kgr@chromium.org>
2014-03-18 15:02:30 -0400

    Fixed indentation.

--- core/mm5Misc.js
+++ core/mm5Misc.js
@@ -15,288 +15,288 @@
  * limitations under the License.
  */
 FOAModel({
-     name: 'UnitTest',
-     plural: 'Unit Tests',
-     tableProperties:
-     [
-        'description', 'passed', 'failed'
-     ],
-     properties:
-     [
-        {
-           model_: 'Property',
-           name: 'description',
-           type: 'String',
-           required: true,
-           displayWidth: 70,
-           displayHeight: 1,
-           help: 'A one line description of the unit test.'
-        },
-        {
-           model_: 'Property',
-           name: 'passed',
-           type: 'Integer',
-           required: true,
-           displayWidth: 8,
-           displayHeight: 1,
-           view: 'IntFieldView',
-           help: 'Number of sub-tests to pass.'
-        },
-        {
-           model_: 'Property',
-           name: 'failed',
-           type: 'Integer',
-           required: true,
-           displayWidth: 8,
-           displayHeight: 1,
-           help: 'Number of sub-tests to fail.'
-        },
-        {
-           model_: 'Property',
-           name: 'results',
-           type: 'String',
-           mode: 'read-only',
-           required: true,
-           view: {
-              create: function() { return TextFieldView.create({mode:'read-only', escapeHTML: false}); } },
-           displayWidth: 80,
-           displayHeight: 20
-        },
-        {
-           model_: 'FunctionProperty',
-           name: 'code',
-           label: 'Test Code',
-           required: true,
-           displayWidth: 80,
-           displayHeight: 30
-        }
-     ],
-
-     actions: [
-      {
-         model_: 'Action',
-         name:  'test',
-         help:  'Run the unit tests.',
-
-         isAvailable: function() { return true; },
-         isEnabled:   function() { return true; },
-         action:      function(obj) {
-            console.log("testing", this);
-            this.results = "<table border=1>";
-
-            this.passed = 0;
-            this.failed = 0;
-            this.code();
-
-            this.append("</table>");
-         }
-      }
-     ],
-
-    methods:{
-
-       append: function(str) {
-          this.results = this.results + str;
-       },
-       addHeader: function(name) {
-          this.append('<tr><th colspan=2 class="resultHeader">' + name + '</th></tr>');
-       },
-       addRow: function(comment, condition) {
-           this.append('<tr>' +
-             '<td>' + comment + '</td>' +
-             '<td>' + (condition ? "<font color=green>OK</font>" : "<font color=red>ERROR</font>") + '</td>' +
-             '</tr>');
-       },
-       assert: function(condition, comment) {
-          if ( condition ) this.passed++; else this.failed++;
-          this.addRow(comment, condition);
-       },
-       fail: function(comment) {
-          this.assert(false, comment);
-       },
-       ok: function(comment) {
-          this.assert(true, comment);
-       }
+  name: 'UnitTest',
+  plural: 'Unit Tests',
+  tableProperties:
+  [
+    'description', 'passed', 'failed'
+  ],
+  properties:
+  [
+    {
+      model_: 'Property',
+      name: 'description',
+      type: 'String',
+      required: true,
+      displayWidth: 70,
+      displayHeight: 1,
+      help: 'A one line description of the unit test.'
+    },
+    {
+      model_: 'Property',
+      name: 'passed',
+      type: 'Integer',
+      required: true,
+      displayWidth: 8,
+      displayHeight: 1,
+      view: 'IntFieldView',
+      help: 'Number of sub-tests to pass.'
+    },
+    {
+      model_: 'Property',
+      name: 'failed',
+      type: 'Integer',
+      required: true,
+      displayWidth: 8,
+      displayHeight: 1,
+      help: 'Number of sub-tests to fail.'
+    },
+    {
+      model_: 'Property',
+      name: 'results',
+      type: 'String',
+      mode: 'read-only',
+      required: true,
+      view: {
+        create: function() { return TextFieldView.create({mode:'read-only', escapeHTML: false}); } },
+      displayWidth: 80,
+      displayHeight: 20
+    },
+    {
+      model_: 'FunctionProperty',
+      name: 'code',
+      label: 'Test Code',
+      required: true,
+      displayWidth: 80,
+      displayHeight: 30
     }
- });
+  ],
 
+  actions: [
+    {
+      model_: 'Action',
+      name:  'test',
+      help:  'Run the unit tests.',
 
-FOAModel({
-     name: 'Relationship',
-     tableProperties: [
-        'name', 'label', 'relatedModel', 'relatedProperty'
-     ],
-     properties: [
-       {
-           name:  'name',
-           type:  'String',
-           displayWidth: 30,
-           displayHeight: 1,
-           defaultValueFn: function() { return GLOBAL[this.relatedModel].plural; },
-           help: 'The coding identifier for the action.'
-       },
-       {
-           name: 'label',
-           type: 'String',
-           displayWidth: 70,
-           displayHeight: 1,
-           defaultValueFn: function() { return this.name.labelize(); },
-           help: 'The display label for the action.'
-       },
-       {
-           name: 'help',
-           label: 'Help Text',
-           type: 'String',
-           displayWidth: 70,
-           displayHeight: 6,
-           defaultValue: '',
-           help: 'Help text associated with the relationship.'
-       },
-       {
-           name:  'relatedModel',
-           type:  'String',
-           required: true,
-           displayWidth: 30,
-           displayHeight: 1,
-           defaultValue: '',
-           help: 'The name of the related Model.'
-       },
-       {
-           name:  'relatedProperty',
-           type:  'String',
-           required: true,
-           displayWidth: 30,
-           displayHeight: 1,
-           defaultValue: '',
-           help: 'The join property of the related Model.'
-       }
-     ]/*,
-     methods: {
-       dao: function() {
-         var m = GLOBAL[this.relatedModel];
-         return GLOBAL[m.name + 'DAO'];
-       },
-       JOIN: function(sink, opt_where) {
-         var m = GLOBAL[this.relatedModel];
-         var dao = GLOBAL[m.name + 'DAO'];
-         return MAP(JOIN(
-           dao.where(opt_where || TRUE),
-           m.getProperty(this.relatedProperty),
-           []), sink);
-       }
-     }*/
- });
+      isAvailable: function() { return true; },
+      isEnabled:   function() { return true; },
+      action:      function(obj) {
+        console.log("testing", this);
+        this.results = "<table border=1>";
 
+        this.passed = 0;
+        this.failed = 0;
+        this.code();
 
-FOAModel({
-     name: 'Issue',
-     plural: 'Issues',
-     help: 'An issue describes a question, feature request, or defect.',
-     ids: [
-       'id'
-     ],
-     tableProperties:
-     [
-        'id', 'severity', 'status', 'summary', 'assignedTo'
-     ],
-     properties:
-     [
-        {
-           model_: 'Property',
-           name: 'id',
-           label: 'Issue ID',
-           type: 'String',
-           required: true,
-           displayWidth: 12,
-           displayHeight: 1,
-           defaultValue: 0,
-           view: 'IntFieldView',
-           help: 'Issue\'s unique sequence number.'
-        },
-        {
-           name: 'severity',
-           view: {
-              create: function() { return ChoiceView.create({choices: [
-                 'Feature',
-                 'Minor',
-                 'Major',
-                 'Question'
-              ]});}
-           },
-           defaultValue: 'String',
-           help: 'The severity of the issue.'
-       },
-       {
-           name: 'status',
-           type: 'String',
-           required: true,
-           view: {
-              create: function() { return ChoiceView.create({choices: [
-                 'Open',
-                 'Accepted',
-                 'Complete',
-                 'Closed'
-              ]});}
-           },
-           defaultValue: 'String',
-           help: 'The status of the issue.'
-       },
-       {
-           model_: 'Property',
-           name: 'summary',
-           type: 'String',
-           required: true,
-           displayWidth: 70,
-           displayHeight: 1,
-           help: 'A one line summary of the issue.'
-       },
-       {
-           model_: 'Property',
-           name: 'created',
-           type: 'DateTime',
-           required: true,
-           displayWidth: 50,
-           displayHeight: 1,
-           valueFactory: function() { return new Date(); },
-           help: 'When this issue was created.'
-       },
-       {
-           model_: 'Property',
-           name: 'createdBy',
-           type: 'String',
-           defaultValue: 'kgr',
-           required: true,
-           displayWidth: 30,
-           displayHeight: 1,
-           help: 'Who created the issue.'
-       },
-       {
-           model_: 'Property',
-           name: 'assignedTo',
-           type: 'String',
-           defaultValue: 'kgr',
-           displayWidth: 30,
-           displayHeight: 1,
-           help: 'Who the issue is currently assigned to.'
-       },
-       {
-           model_: 'Property',
-           name: 'notes',
-           displayWidth: 75,
-           displayHeight: 20,
-           view: 'TextAreaView',
-           help: 'Notes describing issue.'
-       }
-     ],
-     tests: [
-        {
-           model_: 'UnitTest',
-           description: 'test1',
-           code: function() {this.addHeader("header");this.ok("pass");this.fail("fail");}
-        }
-     ]
+        this.append("</table>");
+      }
+    }
+  ],
+
+  methods:{
+
+    append: function(str) {
+      this.results = this.results + str;
+    },
+    addHeader: function(name) {
+      this.append('<tr><th colspan=2 class="resultHeader">' + name + '</th></tr>');
+    },
+    addRow: function(comment, condition) {
+      this.append('<tr>' +
+                  '<td>' + comment + '</td>' +
+                  '<td>' + (condition ? "<font color=green>OK</font>" : "<font color=red>ERROR</font>") + '</td>' +
+                  '</tr>');
+    },
+    assert: function(condition, comment) {
+      if ( condition ) this.passed++; else this.failed++;
+      this.addRow(comment, condition);
+    },
+    fail: function(comment) {
+      this.assert(false, comment);
+    },
+    ok: function(comment) {
+      this.assert(true, comment);
+    }
   }
-);
+});
+
+
+FOAModel({
+  name: 'Relationship',
+  tableProperties: [
+    'name', 'label', 'relatedModel', 'relatedProperty'
+  ],
+  properties: [
+    {
+      name:  'name',
+      type:  'String',
+      displayWidth: 30,
+      displayHeight: 1,
+      defaultValueFn: function() { return GLOBAL[this.relatedModel].plural; },
+      help: 'The coding identifier for the action.'
+    },
+    {
+      name: 'label',
+      type: 'String',
+      displayWidth: 70,
+      displayHeight: 1,
+      defaultValueFn: function() { return this.name.labelize(); },
+      help: 'The display label for the action.'
+    },
+    {
+      name: 'help',
+      label: 'Help Text',
+      type: 'String',
+      displayWidth: 70,
+      displayHeight: 6,
+      defaultValue: '',
+      help: 'Help text associated with the relationship.'
+    },
+    {
+      name:  'relatedModel',
+      type:  'String',
+      required: true,
+      displayWidth: 30,
+      displayHeight: 1,
+      defaultValue: '',
+      help: 'The name of the related Model.'
+    },
+    {
+      name:  'relatedProperty',
+      type:  'String',
+      required: true,
+      displayWidth: 30,
+      displayHeight: 1,
+      defaultValue: '',
+      help: 'The join property of the related Model.'
+    }
+  ]/*,
+     methods: {
+     dao: function() {
+     var m = GLOBAL[this.relatedModel];
+     return GLOBAL[m.name + 'DAO'];
+     },
+     JOIN: function(sink, opt_where) {
+     var m = GLOBAL[this.relatedModel];
+     var dao = GLOBAL[m.name + 'DAO'];
+     return MAP(JOIN(
+     dao.where(opt_where || TRUE),
+     m.getProperty(this.relatedProperty),
+     []), sink);
+     }
+     }*/
+});
+
+
+FOAModel({
+  name: 'Issue',
+  plural: 'Issues',
+  help: 'An issue describes a question, feature request, or defect.',
+  ids: [
+    'id'
+  ],
+  tableProperties:
+  [
+    'id', 'severity', 'status', 'summary', 'assignedTo'
+  ],
+  properties:
+  [
+    {
+      model_: 'Property',
+      name: 'id',
+      label: 'Issue ID',
+      type: 'String',
+      required: true,
+      displayWidth: 12,
+      displayHeight: 1,
+      defaultValue: 0,
+      view: 'IntFieldView',
+      help: 'Issue\'s unique sequence number.'
+    },
+    {
+      name: 'severity',
+      view: {
+        create: function() { return ChoiceView.create({choices: [
+          'Feature',
+          'Minor',
+          'Major',
+          'Question'
+        ]});}
+      },
+      defaultValue: 'String',
+      help: 'The severity of the issue.'
+    },
+    {
+      name: 'status',
+      type: 'String',
+      required: true,
+      view: {
+        create: function() { return ChoiceView.create({choices: [
+          'Open',
+          'Accepted',
+          'Complete',
+          'Closed'
+        ]});}
+      },
+      defaultValue: 'String',
+      help: 'The status of the issue.'
+    },
+    {
+      model_: 'Property',
+      name: 'summary',
+      type: 'String',
+      required: true,
+      displayWidth: 70,
+      displayHeight: 1,
+      help: 'A one line summary of the issue.'
+    },
+    {
+      model_: 'Property',
+      name: 'created',
+      type: 'DateTime',
+      required: true,
+      displayWidth: 50,
+      displayHeight: 1,
+      valueFactory: function() { return new Date(); },
+      help: 'When this issue was created.'
+    },
+    {
+      model_: 'Property',
+      name: 'createdBy',
+      type: 'String',
+      defaultValue: 'kgr',
+      required: true,
+      displayWidth: 30,
+      displayHeight: 1,
+      help: 'Who created the issue.'
+    },
+    {
+      model_: 'Property',
+      name: 'assignedTo',
+      type: 'String',
+      defaultValue: 'kgr',
+      displayWidth: 30,
+      displayHeight: 1,
+      help: 'Who the issue is currently assigned to.'
+    },
+    {
+      model_: 'Property',
+      name: 'notes',
+      displayWidth: 75,
+      displayHeight: 20,
+      view: 'TextAreaView',
+      help: 'Notes describing issue.'
+    }
+  ],
+  tests: [
+    {
+      model_: 'UnitTest',
+      description: 'test1',
+      code: function() {this.addHeader("header");this.ok("pass");this.fail("fail");}
+    }
+  ]
+}
+        );
 
 (function() {
   for ( var i = 0 ; i < Model.templates.length ; i++ )
--- core/models.js
+++ core/models.js
@@ -20,224 +20,224 @@
  * All models in this file can be stored and loaded in a DAO.
  **/
 FOAModel({
-   name: 'Timer',
+  name: 'Timer',
 
-   properties: [
-      {
-         name:  'interval',
-         type:  'int',
-         view:  'IntFieldView',
-         help:  'Interval of time between updating time.',
-         units: 'ms',
-         defaultValue: 10
-      },
-      {
-         name:  'i',
-         type:  'int',
-         view:  'IntFieldView',
-         defaultValue: 0
-      },
-      {
-         name:  'timeWarp',
-         type:  'float',
-         view:  'FloatFieldView',
-         defaultValue: 1.0
-      },
-      {
-         name:  'duration',
-         type:  'int',
-         view:  'IntFieldView',
-         units: 'ms',
-         defaultValue: -1
-      },
-      {
-         name: 'percent',
-         type: 'float',
-         view:  'FloatFieldView',
-         defaultValue: 0
-      },
-      {
-         name:  'startTime',
-         type:  'int',
-         view:  'IntFieldView',
-         defaultValue: 0
-      },
-      {
-         name:  'time',
-         type:  'int',
-         help:  'The current time in milliseconds since epoch.',
-         view:  'IntFieldView',
-         defaultValue: 0
-      },
-      {
-         name:  'second',
-         type:  'int',
-         help:  'The second of the current minute.',
-         view:  'IntFieldView',
-         defaultValue: 0
-      },
-      {
-         name:  'minute',
-         type:  'int',
-         help:  'The minute of the current hour.',
-         view:  'IntFieldView',
-         defaultValue: 0
-      },
-      {
-         name:  'hour',
-         type:  'int',
-         help:  'The hour of the current day.',
-         view:  'IntFieldView',
-         defaultValue: 0
-      },
-      {
-        name: 'isStarted',
-        defaultValue: false,
-        hidden: true
+  properties: [
+    {
+      name:  'interval',
+      type:  'int',
+      view:  'IntFieldView',
+      help:  'Interval of time between updating time.',
+      units: 'ms',
+      defaultValue: 10
+    },
+    {
+      name:  'i',
+      type:  'int',
+      view:  'IntFieldView',
+      defaultValue: 0
+    },
+    {
+      name:  'timeWarp',
+      type:  'float',
+      view:  'FloatFieldView',
+      defaultValue: 1.0
+    },
+    {
+      name:  'duration',
+      type:  'int',
+      view:  'IntFieldView',
+      units: 'ms',
+      defaultValue: -1
+    },
+    {
+      name: 'percent',
+      type: 'float',
+      view:  'FloatFieldView',
+      defaultValue: 0
+    },
+    {
+      name:  'startTime',
+      type:  'int',
+      view:  'IntFieldView',
+      defaultValue: 0
+    },
+    {
+      name:  'time',
+      type:  'int',
+      help:  'The current time in milliseconds since epoch.',
+      view:  'IntFieldView',
+      defaultValue: 0
+    },
+    {
+      name:  'second',
+      type:  'int',
+      help:  'The second of the current minute.',
+      view:  'IntFieldView',
+      defaultValue: 0
+    },
+    {
+      name:  'minute',
+      type:  'int',
+      help:  'The minute of the current hour.',
+      view:  'IntFieldView',
+      defaultValue: 0
+    },
+    {
+      name:  'hour',
+      type:  'int',
+      help:  'The hour of the current day.',
+      view:  'IntFieldView',
+      defaultValue: 0
+    },
+    {
+      name: 'isStarted',
+      defaultValue: false,
+      hidden: true
+    }
+  ],
+
+  actions: [
+    {
+      model_: 'Action',
+      name:  'start',
+      help:  'Start the timer.',
+
+      isAvailable: function() { return true; },
+      isEnabled:   function() { return ! this.isStarted; },
+      action:      function() { this.isStarted = true; this.tick(); }
+    },
+    {
+      model_: 'Action',
+      name:  'step',
+      help:  'Step the timer.',
+
+      isAvailable: function() { return true; },
+      action: function()      {
+        this.i++;
+        this.time  += this.interval * this.timeWarp;
+        this.second = this.time /    1000 % 60 << 0;
+        this.minute = this.time /   60000 % 60 << 0;
+        this.hour   = this.time / 3600000 % 24 << 0;
       }
-   ],
+    },
+    {
+      model_: 'Action',
+      name:  'stop',
+      help:  'Stop the timer.',
 
-   actions: [
-     {
-       model_: 'Action',
-       name:  'start',
-       help:  'Start the timer.',
+      isAvailable: function() { return true; },
+      isEnabled: function() { return this.isStarted; },
+      action: function() {
+        this.isStarted = false;
+        if ( this.timeout ) {
+          clearTimeout(this.timeout);
+          this.timeout = undefined;
+        }
+      }
+    }
+  ],
 
-       isAvailable: function() { return true; },
-       isEnabled:   function() { return ! this.isStarted; },
-       action:      function() { this.isStarted = true; this.tick(); }
-     },
-     {
-       model_: 'Action',
-       name:  'step',
-       help:  'Step the timer.',
+  methods: {
+    tick: function() {
+      this.timeout = undefined;
+      if ( ! this.isStarted ) return;
 
-       isAvailable: function() { return true; },
-       action: function()      {
-         this.i++;
-         this.time  += this.interval * this.timeWarp;
-         this.second = this.time /    1000 % 60 << 0;
-         this.minute = this.time /   60000 % 60 << 0;
-         this.hour   = this.time / 3600000 % 24 << 0;
-       }
-     },
-     {
-       model_: 'Action',
-       name:  'stop',
-       help:  'Stop the timer.',
-
-       isAvailable: function() { return true; },
-       isEnabled: function() { return this.isStarted; },
-       action: function() {
-         this.isStarted = false;
-         if ( this.timeout ) {
-           clearTimeout(this.timeout);
-           this.timeout = undefined;
-         }
-       }
-     }
-   ],
-
-   methods: {
-     tick: function() {
-       this.timeout = undefined;
-       if ( ! this.isStarted ) return;
-
-       this.step();
-       this.timeout = setTimeout(this.tick.bind(this), this.interval);
-     }
-   }
+      this.step();
+      this.timeout = setTimeout(this.tick.bind(this), this.interval);
+    }
+  }
 });
 
 
 FOAModel({
-   name: 'Binding',
+  name: 'Binding',
 
-   properties: [
-      // TODO: add support for named sub-contexts
-      {
-         name:  'id',
-         hidden: true
-      },
-      {
-         name:  'value',
-         hidden: true
-      }
-   ]
+  properties: [
+    // TODO: add support for named sub-contexts
+    {
+      name:  'id',
+      hidden: true
+    },
+    {
+      name:  'value',
+      hidden: true
+    }
+  ]
 });
 
 
 FOAModel({
-   name: 'PersistentContext',
+  name: 'PersistentContext',
 
-   properties: [
-      {
-         name:  'dao',
-         label: 'DAO',
-         type: 'DAO',
-         hidden: true
-      },
-      {
-         name:  'context',
-         hidden: true
-      },
-      {
-          name: 'predicate',
-          type: 'Expr',
-          defaultValueFn: function() { return TRUE; },
-          hidden: true
+  properties: [
+    {
+      name:  'dao',
+      label: 'DAO',
+      type: 'DAO',
+      hidden: true
+    },
+    {
+      name:  'context',
+      hidden: true
+    },
+    {
+      name: 'predicate',
+      type: 'Expr',
+      defaultValueFn: function() { return TRUE; },
+      hidden: true
+    }
+  ],
+
+  methods: {
+    /**
+     * Manage persistene for an object.
+     * Resave it in the DAO whenever it first propertyChange events.
+     **/
+    manage: function(name, obj) {
+      obj.addListener(EventService.merged((function() {
+        console.log('PersistentContext', 'updating', name);
+        this.dao.put(Binding.create({
+          id:    name,
+          value: JSONUtil.compact.where(this.predicate).stringify(obj)
+        }));
+      }).bind(this)));
+    },
+    bindObjects: function(a) {
+      // TODO: implement
+    },
+    bindObject: function(name, model, createArgs) {
+      console.log('PersistentContext', 'binding', name);
+      var future = afuture();
+      createArgs = createArgs || {};
+
+      if ( this.context[name] ) {
+        future.set(this.context[name]);
+      } else {
+        this.dao.find(name, {
+          put: function (binding) {
+            console.log('PersistentContext', 'existingInit', name);
+            //                  var obj = JSONUtil.parse(binding.value);
+            //                  var obj = JSON.parse(binding.value);
+            var json = JSON.parse(binding.value);
+            json.__proto__ = createArgs;
+            var obj = JSONUtil.mapToObj(json);
+            this.context[name] = obj;
+            this.manage(name, obj);
+            future.set(obj);
+          }.bind(this),
+          error: function() {
+            console.log('PersistentContext', 'newInit', name);
+            var obj = model.create(createArgs);
+            this.context[name] = obj;
+            this.manage(name, obj);
+            future.set(obj);
+          }.bind(this)
+        });
       }
-   ],
 
-   methods: {
-      /**
-       * Manage persistene for an object.
-       * Resave it in the DAO whenever it first propertyChange events.
-       **/
-      manage: function(name, obj) {
-         obj.addListener(EventService.merged((function() {
-            console.log('PersistentContext', 'updating', name);
-            this.dao.put(Binding.create({
-               id:    name,
-               value: JSONUtil.compact.where(this.predicate).stringify(obj)
-             }));
-         }).bind(this)));
-      },
-      bindObjects: function(a) {
-         // TODO: implement
-      },
-      bindObject: function(name, model, createArgs) {
-         console.log('PersistentContext', 'binding', name);
-        var future = afuture();
-        createArgs = createArgs || {};
-
-         if ( this.context[name] ) {
-            future.set(this.context[name]);
-         } else {
-            this.dao.find(name, {
-               put: function (binding) {
-                  console.log('PersistentContext', 'existingInit', name);
-//                  var obj = JSONUtil.parse(binding.value);
-//                  var obj = JSON.parse(binding.value);
-                  var json = JSON.parse(binding.value);
-                  json.__proto__ = createArgs;
-                  var obj = JSONUtil.mapToObj(json);
-                  this.context[name] = obj;
-                  this.manage(name, obj);
-                  future.set(obj);
-               }.bind(this),
-               error: function() {
-                  console.log('PersistentContext', 'newInit', name);
-                  var obj = model.create(createArgs);
-                  this.context[name] = obj;
-                  this.manage(name, obj);
-                  future.set(obj);
-               }.bind(this)
-            });
-         }
-
-         return future.get;
-      }
+      return future.get;
+    }
   }
 });
 
