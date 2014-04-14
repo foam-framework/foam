c1044011e558dfada52f518fec5ce384e74bb2c3
Kevin Greer <kgr@chromium.org>
2014-04-14 14:10:44 -0400

    Now creates USED_MODELS and UNUSED_MODELS maps.

--- core/FOAM.js
+++ core/FOAM.js
@@ -100,6 +100,9 @@ var FOAModel = function(m) {
 }
 */
 
+var UNUSED_MODELS = {};
+var USED_MODELS = {};
+
 // Lazy Model Definition - Only creates Model when first referenced
 var FOAModel = function(m) {
   // Templates need to access document.currentScript in order to know
@@ -110,8 +113,11 @@ var FOAModel = function(m) {
     return;
   }
 
+  UNUSED_MODELS[m.name] = true;
   Object.defineProperty(GLOBAL, m.name, {
     get: function () {
+      USED_MODELS[m.name] = true;
+      delete UNUSED_MODELS[m.name];
       Object.defineProperty(GLOBAL, m.name, {value: null});
       registerModel(JSONUtil.mapToObj(m, Model));
       return this[m.name];
