05c26d38cec7650746b69a0e84c30474d6fe6992
Kevin Greer <kgr@chromium.org>
2014-04-22 17:46:24 -0400

    Initial check-in of unstyled calculator.

---
+++ apps/calc/Calc.js
@@ -0,0 +1,80 @@
+function makeOp(name, sym, f) {
+  f.toString = function() { return sym; };
+  return {
+    name: name,
+    label: sym,
+    action: function() { this.op = f; }
+  };
+}
+
+var DEFAULT_OP = function(a1) { return a1; };
+DEFAULT_OP.toString = function() { return ""; };
+
+FOAModel({
+  name: 'Calc',
+
+  properties: [
+    { name: 'a1', model_: 'FloatProperty' },
+    { name: 'a2', model_: 'FloatProperty' },
+    { name: 'pos', defaultValue: 1 },
+    {
+      name: 'op',
+      postSet: function() { this.a1 = this.a2; this.a2 = 0; this.pos = 1; },
+      defaultValue: DEFAULT_OP
+    },
+    { name: 'history', model_: 'StringArrayProperty', factory: function() { return []; } }
+  ],
+
+  methods: {
+    num: function(n) {
+      if ( this.pos == 1 ) {
+        this.a2 = 10 * this.a2 + n;
+      } else {
+        this.a2 = this.a2 + n / this.pos;
+        this.pos *= 10;
+      }
+    }
+  },
+
+  actions: [
+    {
+      name: 'ac',
+      label: 'AC',
+      action: function() { this.a1 = this.a2 = 0; this.history = []; this.op = DEFAULT_OP; }
+    },
+    {
+      name: 'sign',
+      label: '+/-',
+      action: function() { this.a2 = - this.a2; }
+    },
+    { name: '1', action: function() { this.num(1); } },
+    { name: '2', action: function() { this.num(2); } },
+    { name: '3', action: function() { this.num(3); } },
+    { name: '4', action: function() { this.num(4); } },
+    { name: '5', action: function() { this.num(5); } },
+    { name: '6', action: function() { this.num(6); } },
+    { name: '7', action: function() { this.num(7); } },
+    { name: '8', action: function() { this.num(8); } },
+    { name: '9', action: function() { this.num(9); } },
+    { name: '0', action: function() { this.num(0); } },
+    makeOp('div', '/', function(a1, a2) { return a1 / a2; }),
+    makeOp('div', '*', function(a1, a2) { return a1 * a2; }),
+    makeOp('div', '+', function(a1, a2) { return a1 + a2; }),
+    makeOp('div', '-', function(a1, a2) { return a1 - a2; }),
+    {
+      name: 'point',
+      label: '.',
+      action: function() { this.pos = 10; }
+    },
+    {
+      name: 'equal',
+      label: '=',
+      action: function() {
+        var a1 = this.a1;
+        var a2 = this.a2;
+        this.a1 = a2;
+        this.history = this.history.concat([this.a2 = this.op(a1, a2)]);
+      }
+    }
+  ]
+});
