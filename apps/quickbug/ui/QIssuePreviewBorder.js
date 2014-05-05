66e9ceb0f0f6dc43c9ce40b0a8345cee43e16ae4
Adam Van Ymeren <adamvy@google.com>
2014-05-05 15:06:18 -0400

    Add missing files.

---
+++ apps/quickbug/ui/QIssuePreviewBorder.js
@@ -0,0 +1,14 @@
+FOAModel({
+  name: 'QIssuePreviewBorder',
+  help: 'Wraps a QIssueDetailView in a manner appropriate for a popup.',
+  
+  methods: {
+    toHTML: function(border, delegate, args) {
+      return '<div id="' + this.getID() + '" ' +
+        'class="QIssuePreview" ' +
+        'style="position:absolute;height:500px;width:875px;background:white">' +
+        this.toInnerHTML() +
+        '</div>';
+    }
+  }
+});
