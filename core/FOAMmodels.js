d23b16d2e913dbc2209ac0e2fe4cfd8d3f40eae5
kgrgreer <kgrgreer@gmail.com>
2014-05-06 15:36:43 -0400

    Fix for firefox detection.

--- core/FOAMmodels.js
+++ core/FOAMmodels.js
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 var files = [
-  ['firefox', function() { return navigator.userAgent.indexOf('firefox') != -1; }],
+  ['firefox', function() { return navigator.userAgent.indexOf('Firefox') != -1; }],
   'stdlib',
   'io',
   'writer',
