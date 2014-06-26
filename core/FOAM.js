16311ea8f835cbc3f9929b275276e27f0631dbcb
kgrgreer <kgrgreer@gmail.com>
2014-06-26 15:09:16 -0400

    Updated 'FOAM POWERED' link to point to GitHub.

--- core/FOAM.js
+++ core/FOAM.js
@@ -241,7 +241,7 @@ function arequire(modelName) {
 }
 
 
-var FOAM_POWERED = '<a style="text-decoration:none;" href="http://code.google.com/p/foam-framework/" target="_blank">\
+var FOAM_POWERED = '<a style="text-decoration:none;" href="https://github.com/foam-framework/foam/" target="_blank">\
 <font size=+1 face="catull" style="text-shadow:rgba(64,64,64,0.3) 3px 3px 4px;">\
 <font color="#3333FF">F</font><font color="#FF0000">O</font><font color="#FFCC00">A</font><font color="#33CC00">M</font>\
 <font color="#555555" > POWERED</font></font></a>';
