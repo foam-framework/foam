e60373e1675c27d8ca472c5de2509bb6d7b087e3
Kevin Greer <kgr@chromium.org>
2014-07-05 12:51:54 -0400

    Set context Model properties to be configurable so that they can be overriden.

--- core/context.js
+++ core/context.js
@@ -24,7 +24,15 @@ function sub(opt_args, opt_name) {
     if ( opt_args.hasOwnProperty(key) ) {
       // It looks like the chrome debug console is overwriting sub.window
       // but this prevents it.
-      Object.defineProperty(sub, key, {value: opt_args[key], writable: key !== 'window'});
+      Object.defineProperty(
+        sub,
+        key,
+        {
+          value: opt_args[key],
+          writable: key !== 'window',
+          configurable: true
+        }
+      );
     }
   }
   if ( opt_name ) {
@@ -104,7 +112,7 @@ function registerModel(model, opt_name) {
       get: function() {
         return ( this === thisX ) ? thisModel : this.registerModel(model);
       },
-      configurabe: true
+      configurable: true
     }
   );
 
