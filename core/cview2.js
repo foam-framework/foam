4977278fa901ac836d882e35f00c1b0ecda41bf4
Adam Van Ymeren <adamvy@google.com>
2014-07-07 10:25:40 -0400

    Add DAOListCView

--- core/cview2.js
+++ core/cview2.js
@@ -389,3 +389,49 @@ MODEL({
     }
   }
 });
+
+MODEL({
+  name: 'DAOListCView',
+  extendsModel: 'CView2',
+
+  properties: [
+    { model_: 'DAOProperty', name: 'dao' },
+    { model_: 'IntProperty', name: 'scrollTop', preSet: function(_,t) { return Math.max(t, 0); }, postSet: function() { this.scroll(); } },
+    { name: 'rowRenderer' },
+    { name: 'objs', postSet: function() { this.view && this.view.paint(); }, factory: function() { return []; } }
+  ],
+
+  methods: {
+    init: function(args) {
+      this.SUPER(args);
+      this.dao.listen(this.scroll);
+    },
+    paintSelf: function() {
+      var renderer = this.rowRenderer;
+
+      var offset = -(this.scrollTop % renderer.height);
+      this.canvas.save();
+      this.canvas.translate(0, offset);
+      for ( var i = 0; i < this.objs.length; i++ ) {
+        renderer.render(this.canvas, this.objs[i]);
+        this.canvas.translate(0, renderer.height);
+      }
+      this.canvas.restore();
+    }
+  },
+
+  listeners: [
+    {
+      name: 'scroll',
+      code: function() {
+        var renderer = this.rowRenderer;
+        var limit = Math.floor(this.height / renderer.height);
+        var skip = Math.floor(this.scrollTop / renderer.height);
+        var self = this;
+        this.dao.skip(skip).limit(limit).select()(function(objs) {
+          self.objs = objs;
+        });
+      }
+    }
+  ]
+});
