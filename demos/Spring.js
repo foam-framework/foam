eddf27dbdf56972ba8b7e948860ebad7771030d6
kgrgreer <kgrgreer@gmail.com>
2014-07-07 15:13:14 -0400

    Updated Spring behaviour to be 2D instead of 2x1D.

--- demos/Spring.js
+++ demos/Spring.js
@@ -16,10 +16,11 @@ function follow(mouse, c, dx, dy) {
     c.y = mouse.y + dy;
   });
 }
-function friction(c) {
+function friction(c, opt_coef) {
+  var coef = opt_coef || 0.9;
   Events.dynamic(function() { c.vx; c.vy; }, function() {
-    c.vx *= 0.9;
-    c.vy *= 0.9;
+    c.vx *= coef;
+    c.vy *= coef;
   });
 }
 function move(c) {
@@ -28,16 +29,31 @@ function move(c) {
     c.y += c.vy;
   });
 }
-function spring(mouse, c, dx, dy) {
+function spring(mouse, c, dx, dy, opt_strength) {
+  var strength = opt_strength || 8;
   Events.dynamic(function() { mouse.x; mouse.y; c.x; c.y; }, function() {
-    c.vx += -(c.x - mouse.x - dx)/Math.max(50, Math.abs(dx));
-    c.vy += -(c.y - mouse.y - dy)/Math.max(50, Math.abs(dy));
+    if ( dx === 0 && dy === 0 ) {
+      c.x = mouse.x;
+      c.y = mouse.y;
+    } else {
+      var d = Math.sqrt(dx*dx + dy*dy);
+      var dx2 = c.x - mouse.x - dx;
+      var dy2 = c.y - mouse.y - dy;
+      var d2 = Math.sqrt(dx2*dx2 + dy2*dy2);
+      var dv = -strength*d2/d;
+      var a = Math.atan2(dy2, dx2);
+      c.vx += dv * Math.cos(a);
+      c.vy += dv * Math.sin(a);
+    }
+// Simpler 2 1-dimensional version
+//    c.vx += -(c.x - mouse.x - dx)/Math.max(50, Math.abs(dx));
+//    c.vy += -(c.y - mouse.y - dy)/Math.max(50, Math.abs(dy));
   });
 }
 function springFollow(mouse, c, dx, dy) {
   spring(mouse, c, dx, dy);
   move(c);
-  friction(c);
+  friction(c, 0.7);
 }
 
 for ( var x = 0 ; x < 5 ; x++ ) {
@@ -45,7 +61,7 @@ for ( var x = 0 ; x < 5 ; x++ ) {
     var c = Circle.create({
       r: 30,
       color: 'white',
-      borderWidth: 12, // 90%
+      borderWidth: 12,
       border: 'hsl(' + x/.005 + ',' + (35+y/.005*60) + '%, 60%)'
     });
 
