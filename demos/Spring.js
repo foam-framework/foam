0bf4af5fb622161b8b94422353b68d773209bb85
kgrgreer <kgrgreer@gmail.com>
2014-07-09 20:32:15 -0400

    Improved initial positioning in Spring demo.

--- demos/Spring.js
+++ demos/Spring.js
@@ -18,11 +18,15 @@ function bounceOnWalls(c, w, h) {
   });
 }
 
+mouse.x = mouse.y = 300;
+
 var N = 19;
 for ( var x = 0 ; x < N ; x++ ) {
   for ( var y = 0 ; y < N ; y++ ) {
     var c = Circle.create({
       r: 4,
+      x: 300 + (x-(N-1)/2)*25,
+      y: 300 + (y-(N-1)/2)*25,
       color: 'white',
       borderWidth: 10,
       border: 'hsl(' + x/N*100 + ',' + (35+y/N*100*60) + '%, 60%)'
@@ -36,4 +40,3 @@ for ( var x = 0 ; x < N ; x++ ) {
   }
 }
 
-mouse.x = mouse.y = 300;
