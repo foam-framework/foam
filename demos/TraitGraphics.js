1fae955b0a650498bf744e00850c4753348b900a
kgrgreer <kgrgreer@gmail.com>
2014-07-14 13:44:01 -0400

    Modified TraitGraphics to show both Traits at once.

--- demos/TraitGraphics.js
+++ demos/TraitGraphics.js
@@ -1,14 +1,16 @@
 var space   = CView2.create({width: 2000, height: 1700});
 var mouse   = Mouse.create();
 
-
+MODEL({
+  name: 'ShadowCircle',
+  extendsModel: 'Circle2',
+  traits: ['Physical', 'Shadow']
+});
 
 MODEL({
-  name: 'Circle',
+  name: 'BlurCircle',
   extendsModel: 'Circle2',
-//  traits: ['Physical', 'MotionBlur']
-  traits: ['Physical', 'Shadow']
-//  traits: ['Physical', 'MotionBlur', 'Shadow']
+  traits: ['Physical', 'MotionBlur']
 });
 
 
@@ -17,19 +19,32 @@ mouse.connect(space.$);
 
 mouse.x = mouse.y = 300;
 
-var N = 7;
+var N = 3;
 for ( var x = 0 ; x < N ; x++ ) {
   for ( var y = 0 ; y < N ; y++ ) {
-    var c = Circle.create({
+    var c = ShadowCircle.create({
       alpha: 1,
-      r: 20,
+      r: 25,
       x: 300 + (x-(N-1)/2)*40,
       y: 300 + (y-(N-1)/2)*40,
       color: 'hsl(' + x/N*100 + ',' + (75+y/N*25) + '%, 60%)'
     });
     space.addChild(c);
 
-    Movement.spring(mouse, c, (x-(N-1)/2)*60, (y-(N-1)/2)*60, 3);
+    Movement.spring(mouse, c, -200+(x-(N-1)/2)*60, (y-(N-1)/2)*60, 3);
+    Movement.inertia(c);
+    Movement.friction(c, 0.9);
+
+    c = BlurCircle.create({
+      alpha: 1,
+      r: 25,
+      x: 700 + (x-(N-1)/2)*40,
+      y: 300 + (y-(N-1)/2)*40,
+      color: 'hsl(' + x/N*100 + ',' + (75+y/N*25) + '%, 60%)'
+    });
+    space.addChild(c);
+
+    Movement.spring(mouse, c, 200+(x-(N-1)/2)*60, (y-(N-1)/2)*60, 3);
     Movement.inertia(c);
     Movement.friction(c, 0.9);
   }
