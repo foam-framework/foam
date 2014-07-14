5321e7693c7d76f30773fdee9f6e639e358ddd0d
Kevin Greer <kgr@chromium.org>
2014-07-14 09:35:17 -0400

    Added styling to Pong.

--- demos/Pong.js
+++ demos/Pong.js
@@ -1,4 +1,3 @@
-// TODO: ensure a nimimum ball x-velocity
 MODEL({
   name: 'Ball',
   extendsModel: 'Circle2',
@@ -85,22 +84,22 @@ MODEL({
     {
       name: 'lUp',
       keyboardShortcuts: [ 81 /* q */ ],
-      action: function() { this.lPaddle.y -= 8; }
+      action: function() { this.lPaddle.y -= 10; }
     },
     {
       name: 'lDown',
       keyboardShortcuts: [ 65 /* a */ ],
-      action: function() { this.lPaddle.y += 8; }
+      action: function() { this.lPaddle.y += 10; }
     },
     {
       name: 'rUp',
       keyboardShortcuts: [ 38 /* up arrow */ ],
-      action: function() { this.rPaddle.y -= 8; }
+      action: function() { this.rPaddle.y -= 10; }
     },
     {
       name: 'rDown',
       keyboardShortcuts: [ 40 /* down arrow */ ],
-      action: function() { this.rPaddle.y += 8; }
+      action: function() { this.rPaddle.y += 10; }
     }
   ],
 
@@ -117,8 +116,8 @@ MODEL({
     },
     initHTML: function() {
       this.view.initHTML();
-      this.lPaddle.x = 30+this.lPaddle.r;
-      this.rPaddle.x = this.WIDTH-30-this.rPaddle.r;
+      this.lPaddle.x = 25+this.lPaddle.r;
+      this.rPaddle.x = this.WIDTH-25-this.rPaddle.r;
       this.lPaddle.y = this.rPaddle.y = (this.HEIGHT-this.rPaddle.height)/2;
 
       Movement.inertia(this.ball);
@@ -128,18 +127,15 @@ MODEL({
       this.ball.vx = this.ball.vy = 10;
 
       this.table.addChildren(
-        CView2.create({x: this.WIDTH/2-10, width:20, height: this.HEIGHT, background: 'white'}),
+        CView2.create({x: this.WIDTH/2-5, width:10, height: this.HEIGHT, background: 'white'}),
         this.ball,
         this.lPaddle,
-        this.rPaddle,
-        CView2.create({x: this.WIDTH/2-10, y:0, width:20, height: this.HEIGHT, background: 'white'}));
+        this.rPaddle);
 
       this.ball.x$.addListener(this.onBallMove);
 
       var collider = Collider.create({pong: this});
-      collider.add(this.ball);
-      collider.add(this.lPaddle);
-      collider.add(this.rPaddle);
+      collider.add(this.ball, this.lPaddle, this.rPaddle);
       collider.start();
 
       this.table.paint();
