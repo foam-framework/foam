aef0e3c333b493f0b1252691658390923e1f2e45
Braden Shepherdson <braden.shepherdson@gmail.com>
2014-07-17 16:10:58 -0400

    Gestures: Add pinch/stretch/twist gesture.

--- core/experimental/touch.js
+++ core/experimental/touch.js
@@ -396,6 +396,115 @@ MODEL({
   ]
 });
 
+MODEL({
+  name: 'PinchTwistGesture',
+  help: 'Gesture that understands a two-finger pinch/stretch and rotation',
+  properties: [
+    {
+      name: 'name',
+      defaultValue: 'pinchTwist'
+    },
+    'handlers', 'points'
+  ],
+
+  methods: {
+    getPoints: function(map) {
+      var keys = Object.keys(map);
+      return [map[keys[0]], map[keys[1]]];
+    },
+
+    recognize: function(map) {
+      // I recognize:
+      // - two points that
+      // - are both not done and
+      // - have begun to move.
+      if ( Object.keys(map).length !== 2 ) return;
+
+      var points = this.getPoints(map);
+      return ! points[0].done &&
+             ! points[1].done &&
+             ( points[0].dx != 0 || points[0].dy != 0 ) &&
+             ( points[1].dx != 0 || points[1].dy != 0 );
+    },
+
+    attach: function(map, handlers) {
+      // I have three callbacks:
+      // function pinchStart();
+      // function pinchMove(scale, rotation);
+      // function pinchEnd();
+      // Scale is a unitless scaling factor, relative to the **start of the gesture**.
+      // Rotation is degrees clockwise relative to the **start of the gesture**.
+      // That is, these values are net totals since the gesture began,
+      // they are not incremental between pinchMove calls, or absolute to the page.
+      // A user of this gesture should save the original values on pinchStart,
+      // and adjust them by the values from each pinchMove to update the UI.
+      // See demos/pinchgesture.html.
+      this.points = this.getPoints(map);
+      this.handlers = handlers || [];
+
+      this.points.forEach(function(p) {
+        p.x$.addListener(this.onMove);
+        p.y$.addListener(this.onMove);
+        p.done$.addListener(this.onDone);
+      }.bind(this));
+
+      // Now send the start event to all the handlers.
+      this.pingHandlers('pinchStart');
+      this.onMove();
+    },
+
+    pingHandlers: function(method, scale, rotation) {
+      for ( var i = 0 ; i < this.handlers.length ; i++ ) {
+        var h = this.handlers[i];
+        h && h[method] && h[method](scale, rotation);
+      }
+    },
+
+    distance: function(x1, y1, x2, y2) {
+      var dx = x2 - x1;
+      var dy = y2 - y1;
+      return Math.sqrt(dx*dx + dy*dy);
+    }
+  },
+
+  listeners: [
+    {
+      name: 'onMove',
+      code: function() {
+        var oldDist = this.distance(this.points[0].x0, this.points[0].y0,
+                                    this.points[1].x0, this.points[1].y0);
+        var newDist = this.distance(this.points[0].x, this.points[0].y,
+                                    this.points[1].x, this.points[1].y);
+        var scale = newDist / oldDist;
+
+        // These are values from -pi to +pi.
+        var oldAngle = Math.atan2(this.points[1].y0 - this.points[0].y0, this.points[1].x0 - this.points[0].x0);
+        var newAngle = Math.atan2(this.points[1].y - this.points[0].y, this.points[1].x - this.points[0].x);
+        var rotation = newAngle - oldAngle;
+        while ( rotation < - Math.PI ) rotation += 2 * Math.PI;
+        while ( rotation > Math.PI ) rotation -= 2 * Math.PI;
+        // That's in radians, so I'll convert to degrees.
+        rotation *= 360;
+        rotation /= 2 * Math.PI;
+
+        this.pingHandlers('pinchMove', scale, rotation);
+      }
+    },
+    {
+      name: 'onDone',
+      code: function(obj, prop, old, nu) {
+        this.points.forEach(function(p) {
+          p.x$.removeListener(this.onMove);
+          p.y$.removeListener(this.onMove);
+          p.done$.removeListener(this.onDone);
+        });
+        this.pingHandlers('pinchEnd');
+      }
+    }
+  ]
+});
+
+
 
 MODEL({
   name: 'GestureTarget',
@@ -432,7 +541,8 @@ MODEL({
         return {
           verticalScroll: VerticalScrollGesture.create(),
           tap: TapGesture.create(),
-          drag: DragGesture.create()
+          drag: DragGesture.create(),
+          pinchTwist: PinchTwistGesture.create()
         };
       }
     },
