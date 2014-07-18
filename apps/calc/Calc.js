b138b5a09d137e9ea0f081c772be120097efcf04
Adam Van Ymeren <adamvy@google.com>
2014-07-18 14:26:48 -0400

    Update calc app to a5560b4 on head

--- apps/calc/Calc.js
+++ apps/calc/Calc.js
@@ -1,3 +1,20 @@
+/**
+ * @license
+ * Copyright 2014 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 /** Make a +-/* Action. **/
 function makeOp(name, sym, keys, f) {
   f.toString = function() { return sym; };
@@ -22,15 +39,15 @@ var DEFAULT_OP = function(a1) { return a1; };
 DEFAULT_OP.toString = function() { return ''; };
 
 /** A subclass of FloatFieldView which doesn't display 0 values. **/
-FOAModel({
+MODEL({
   name:  'CalcFloatFieldView',
   extendsModel: 'FloatFieldView',
   methods: { valueToText: function(v) { return v == 0 ? '' : v.toString(); } }
 });
 
-FOAModel({ name: 'History', properties: [ 'op', 'a2' ] });
+MODEL({ name: 'History', properties: [ 'op', 'a2' ] });
 
-FOAModel({
+MODEL({
   name: 'Calc',
 
   properties: [
@@ -57,8 +74,7 @@ FOAModel({
     {
       model_: 'StringProperty',
       name: 'row1',
-      postSet: function(o, n) { console.log(o, ' -> ', n); },
-      view: 'ALabel'
+      postSet: function(o, n) { console.log(o, ' -> ', n); }
     },
     {
       name: 'history',
@@ -130,5 +146,5 @@ FOAModel({
   ]
 });
 
-FOAModel({ name: 'HistoryView', extendsModel: 'DetailView', templates: [ { name: 'toHTML' } ] });
-FOAModel({ name: 'CalcView', extendsModel: 'DetailView', templates: [ { name: 'toHTML' } ] });
+MODEL({ name: 'HistoryView', extendsModel: 'DetailView', templates: [ { name: 'toHTML' } ] });
+MODEL({ name: 'CalcView', extendsModel: 'DetailView', templates: [ { name: 'toHTML' } ] });
