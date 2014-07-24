28cd9110c1fd2b506f2493923227394c27e938c4
Kevin Greer <kgr@chromium.org>
2014-07-23 23:56:21 -0400

    Initial check-in of ArrayDAO.

--- core/dao.js
+++ core/dao.js
@@ -740,7 +740,7 @@ MODEL({
      },
      {
        model_: 'IntProperty',
-       name: 'rowDelay'xs
+       name: 'rowDelay'
      }
    ],
 
@@ -887,6 +887,7 @@ MODEL({
     removeAll: function(sink, options) { return this.src.removeAll(sink, options); }
   }
 });
+
 
 MODEL({
   name: 'LimitedLiveCachingDAO',
@@ -1071,154 +1072,6 @@ function skipDAO(skip, dao) {
 }
 
 
-// Copy AbstractDAO methods in Array prototype
-
-var pmap = {};
-for ( var key in AbstractDAO.methods ) {
-  pmap[AbstractDAO.methods[key].name] = AbstractDAO.methods[key].code;
-}
-
-defineProperties(Array.prototype, pmap);
-
-defineProperties(Array.prototype, {
-  // Clone this Array and remove 'v' (only 1 instance)
-  // TODO: make faster by copying in one pass, without splicing
-  deleteF: function(v) {
-    var a = this.clone();
-    for (var i = 0; i < a.length; i++) {
-      if ( a[i] === v ) { a.splice(i, 1); break; }
-    }
-    return a;
-  },
-  // Remove 'v' from this array (only 1 instance removed)
-  // return true iff the value was removed
-  deleteI: function(v) {
-    for (var i = 0; i < this.length; i++) {
-      if ( this[i] === v ) { this.splice(i, 1); return true; }
-    }
-    return false;
-  },
-  // Clone this Array and remove first object where predicate 'p' returns true
-  // TODO: make faster by copying in one pass, without splicing
-  removeF: function(p) {
-    var a = this.clone();
-    for (var i = 0; i < a.length; i++) {
-      if (p.f(a[i])) { a.splice(i, 1); break; }
-    }
-    return a;
-  },
-  // Remove first object in this array where predicate 'p' returns true
-  removeI: function(p) {
-    for (var i = 0; i < this.length; i++) {
-      if (p.f(this[i])) { this.splice(i, 1); breeak; }
-    }
-    return this;
-  },
-  pushF: function(obj) {
-    var a = this.clone();
-    a.push(obj);
-    return a;
-  },
-  clone: function() {
-    return this.slice(0);
-  },
-  deepClone: function() {
-    var a = this.slice(0);
-    for ( var i = 0 ; i < a.length ; i++ ) {
-      a[i] = a[i].deepClone();
-    }
-    return a;
-  },
-  put: function(obj, sink) {
-    // With this block of code an [] is a real DAO
-    // but is much slower for collecting results.
-    /*
-      for (var idx in this) {
-      if (this[idx].id === obj.id) {
-      this[idx] = obj;
-      sink && sink.put && sink.put(obj);
-      this.notify_('put', arguments);
-      //        sink && sink.error && sink.error('put', obj, duplicate);
-      return;
-      }
-      }
-    */
-    this.push(obj);
-    this.notify_('put', arguments);
-    sink && sink.put && sink.put(obj);
-  },
-  find: function(query, sink) {
-    if ( query.f ) {
-      for (var idx in this) {
-        if ( query.f(this[idx]) ) {
-          sink && sink.put && sink.put(this[idx]);
-          return;
-        }
-      }
-    } else {
-      for (var idx in this) {
-        if ( this[idx].id === query ) {
-          sink && sink.put && sink.put(this[idx]);
-          return;
-        }
-      }
-    }
-    sink && sink.error && sink.error('find', query);
-  },
-  // TODO: make this faster, should stop after finding first item.
-  remove: function(query, sink) {
-    var id = query.id ? query.id : query;
-    this.removeAll({ remove: sink && sink.remove },
-                   { query: { f: function(obj) { return obj.id ? obj.id === id : obj === id; } } });
-  },
-  removeAll: function(sink, options) {
-    if (!options) options = {};
-    if (!options.query) options.query = { f: function() { return true; } };
-
-    for (var i = 0; i < this.length; i++) {
-      var obj = this[i];
-      if (options.query.f(obj)) {
-        var rem = this.splice(i,1)[0];
-        this.notify_('remove', [rem]);
-        sink && sink.remove && sink.remove(rem);
-        i--;
-      }
-    }
-    sink && sink.eof && sink.eof();
-    return anop();
-  },
-  select: function(sink, options) {
-    sink = sink || [];
-    var hasQuery = options && ( options.query || options.order );
-    var originalsink = sink;
-    sink = this.decorateSink_(sink, options, false, ! hasQuery);
-
-    // Short-circuit COUNT.
-    if ( sink.model_ === CountExpr ) {
-      sink.count = this.length;
-      return aconstant(originalsink);
-    }
-
-    var fc = this.createFlowControl_();
-    var start = Math.max(0, hasQuery ? 0 : ( options && options.skip ) || 0);
-    var end = hasQuery ?
-      this.length :
-      Math.min(this.length, start + ( ( options && options.limit ) || this.length));
-    for ( var i = start ; i < end ; i++ ) {
-      sink.put(this[i], null, fc);
-      if ( fc.stopped ) break;
-      if ( fc.errorEvt ) {
-        sink.error && sink.error(fc.errorEvt);
-        return aconstant(originalsink, fc.errorEvt);
-      }
-    }
-
-    sink.eof && sink.eof();
-
-    return aconstant(originalsink);
-  }
-});
-
 function atxn(afunc) {
   return function(ret) {
     if ( GLOBAL.__TXN__ ) {
@@ -1240,7 +1093,7 @@ function atxn(afunc) {
  * var dao = IDBDAO.create({model: Issue, name: 'ImportantIssues'});
  *
  * TODO:
- * Optimization.  This DAO doesn't use an indexes in indexeddb yet, which
+ * Optimization.  This DAO doesn't use any indexes in indexeddb yet, which
  * means for any query other than a single find/remove we iterate the entire
  * data store.  Obviously this will get slow if you store large amounts
  * of data in the database.
@@ -2429,6 +2282,7 @@ MODEL({
   }
 });
 
+
 MODEL({
   name: 'GDriveDAO',
   properties: [
@@ -2473,6 +2327,7 @@ MODEL({
     }
   }
 });
+
 
 MODEL({
   name: 'RestDAO',
@@ -3228,6 +3083,7 @@ MODEL({
   }
 });
 
+
 MODEL({
   name: 'StoreAndForwardOperation',
   properties: [
@@ -3236,6 +3092,7 @@ MODEL({
     { name: 'obj' },
   ]
 });
+
 
 MODEL({
   name: 'StoreAndForwardDAO',
