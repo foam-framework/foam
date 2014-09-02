a1377ea6738a097d7fce2eccdfff03e29cb4c074
Adam Van Ymeren <adamvy@google.com>
2014-09-02 13:31:23 -0400

    Update todofoam to work using FOAM.browse.

--- apps/todofoam/Todo.js
+++ apps/todofoam/Todo.js
@@ -7,28 +7,30 @@ MODEL({
     "text"
   ],
   properties: [
-    { name: 'id' },
+    {
+      model_: 'IntProperty',
+      name: 'id'
+    },
     { name: 'completed', model_: 'BooleanProperty' },
     { name: 'text' },
     { model_: 'StringArrayProperty', name: 'labels' },
     { model_: 'IntProperty', name: 'priority' },
-    { model_: 'ReferenceProperty', subType: 'Todo', name: 'parent' },
-    { model_: 'ArrayProperty', subType: 'Todo', name: 'children', transient: true }
-  ]
-});
-
-
-var c = AppController.create({
-  model: Todo,
-  dao: EasyDAO.create({model: Todo, seqNo: true, cache: true}),
-  sortChoices: [
-    [ DESC(Todo.ID), 'Default']
+    { model_: 'IntProperty', name: 'parent' }
   ],
-  filterChoices: [
-    ['', 'All'],
-    [ EQ(Todo.COMPLETED, false), 'Active' ],
-    [ EQ(Todo.COMPLETED, true), 'Completed' ]
+  relationships: [
+    {
+      name: 'children',
+      label: 'Todos that this Todo depends upon.',
+      relatedModel: 'Todo',
+      relatedProperty: 'parent'
+    }
   ]
 });
 
-c.write(document);
+var TodoDAO = EasyDAO.create({ daoType: 'MDAO', seqNo: true, model: Todo });
+TodoDAO.put(Todo.create({ id: 1, text: ' Task 1' }));
+TodoDAO.put(Todo.create({ id: 2, text: ' Task 2', parent: 1 }));
+
+window.onload = function() {
+  FOAM.browse('Todo');
+};
