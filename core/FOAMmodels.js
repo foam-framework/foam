639375928dcce48a014e24084f20b5347da383cc
kgrgreer <kgrgreer@gmail.com>
2014-09-04 09:56:16 -0400

    Removed stale debugger statement.

--- core/FOAMmodels.js
+++ core/FOAMmodels.js
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-debugger;
+
 var files = [
   ['firefox', function() { return navigator.userAgent.indexOf('Firefox') != -1; }],
   'stdlib',
