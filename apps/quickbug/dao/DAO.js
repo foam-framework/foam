8f210ae5cadde5b1615076f8cbe7344bd7f52fa3
kgrgreer <kgrgreer@gmail.com>
2014-09-11 14:20:45 -0400

    Made limit be configurable in SplitDAO.

--- apps/quickbug/dao/DAO.js
+++ apps/quickbug/dao/DAO.js
@@ -354,6 +354,10 @@ MODEL({
     {
       name: 'model'
     },
+    {
+      name: 'maxLimit',
+      defaultValue: 500
+    }
   ],
 
   listeners: [
@@ -470,7 +474,7 @@ MODEL({
            if ( options && options.query ) remoteOptions.query = options.query;
            if ( options && options.order ) remoteOptions.order = options.order;
 
-           this.remote.limit(500).select({
+           this.remote.limit(this.maxLimit).select({
              put: (function(obj) {
                // Put the object in the buffer, but also cache it in the local DAO
                if (obj === undefined) debugger;
@@ -520,7 +524,7 @@ MODEL({
            // If it's >= 500, we have only a subset and need to query the server.
            var match = matchingQueries[0];
            match[2].select(COUNT())((function(c) {
-             if ( c.count < 500 ) {
+             if ( c.count < this.maxLimit ) {
                match[2].select(sink, bufOptions)(function(s) {
                  future.set(s);
                });
