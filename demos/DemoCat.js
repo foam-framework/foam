c83d5ac88a592bf7c9bf82124d257d885303362f
kgrgreer <kgrgreer@gmail.com>
2014-09-15 15:10:32 -0400

    Improved DemoCat keywords handling.  Added source code links to DemoCat.

--- demos/DemoCat.js
+++ demos/DemoCat.js
@@ -17,6 +17,19 @@ MODEL({
       name: 'keywords'
     },
     {
+      name: 'src',
+      defaultValueFn: function() {
+        console.log('view-source: ' + window.location.origin + '/demos/' + this.path);
+
+        return 'view-source: ' + window.location.origin + '/demos/' + this.path;
+      }
+      /*
+      preSet: function(_, src) {
+        return src.replace('view-source:', 'view-source:' + window.location.origin + '/demos/');
+      }
+      */
+    },
+    {
       name: 'image'
     },
     {
@@ -43,7 +56,8 @@ var demos = [
     path: 'SolarSystem.html',
     description: 'An animation which demonstrates reactive-programming.  Use the Time-Wheel to spin-time.',
     keywords: ['animation', 'reactive'],
-    image: 'SolarSystem.png'
+    src: 'SolarSystem.js',
+    image: 'SolarSystem.png',
   },
   {
     model_: 'Demo',
@@ -53,7 +67,6 @@ var demos = [
     keywords: ['animation', 'reactive'],
     image: 'ReactiveClocks.png',
     video: 'part11.ogv'
-
   },
   {
     model_: 'Demo',
@@ -112,6 +125,7 @@ var demos = [
     path: 'DragonLiveCoding.html',
     description: 'A version of the dragon animation that you can live-code.  Use the Model editor to update methods while the animation is running.',
     keywords: ['animation', 'live-coding'],
+    src: 'DragonLiveCoding.js',
     image: 'LiveDragon.png',
     video: 'part12.ogv'
   },
@@ -121,6 +135,7 @@ var demos = [
     path: 'Pong.html',
     description: 'A simple pong game which demonstrates the both the use of graphical traits (motion blur and shadow) and of the physics engine.',
     keywords: ['animation', 'game', 'physics', 'traits'],
+    src: 'Pong.js',
     image: 'Pong.png'
   },
   {
@@ -137,6 +152,7 @@ var demos = [
     path: 'Collision.html',
     description: 'Demonstration of collision-detection.',
     keywords: ['physics'],
+    src: 'Collision.js',
     image: 'Collision.png'
   },
   {
@@ -145,6 +161,7 @@ var demos = [
     path: 'CollisionWithSpring.html',
     description: 'A simple physics simulation which shows the use of springs and collision detection.',
     keywords: ['physics'],
+    src: 'CollisionWithSpring.js',
     image: 'CollisionWithSpring.png'
   },
   {
@@ -153,6 +170,7 @@ var demos = [
     path: 'Spring.html',
     description: 'Addictive spring physics simulation.',
     keywords: ['physics'],
+    src: 'Spring.js',
     image: 'Spring.png'
   },
   {
@@ -161,6 +179,7 @@ var demos = [
     path: 'TraitGraphics.html',
     description: 'Demonstrates the use of graphical Traits.  The circles on the left have shadows and those on the right have motion-blure.',
     keywords: ['traits','graphics'],
+    src: 'TraitGraphics.js',
     image: 'TraitGraphics.png'
   },
   {
@@ -169,95 +188,8 @@ var demos = [
     path: 'CropCircle.html',
     description: 'Crop Circle inspired fractals graphics. Can take 10-20 seconds to load on slow machines. Each fractal is implemented in only one line of code.',
     keywords: ['graphics'],
+    src: 'CropCircle.js',
     image: 'CropCircle.png'
-  },
-  {
-    model_: 'Demo',
-    name: 'Dragon',
-    path: 'Tags.html',
-    description: 'Demonstrates use of the FOAM tag to instantiate three views: an animated dragon, a time-wheel, and a DetailView of time.  Use the time-wheel to control the animation.',
-    keywords: [],
-    image: 'Dragon.png'
-  },
-  {
-    model_: 'Demo',
-    name: 'Two-Way Data-Binding',
-    path: 'TwoWayDataBinding.html',
-    description: 'Demonstrates how to do two way data-binding in FOAM. See the same demo implemented with other JS libraries at: http://n12v.com/2-way-data-binding/?hn',
-    keywords: [],
-    image: 'TwoWayDataBinding.png'
-  },
-  {
-    model_: 'Demo',
-    name: 'Calculator',
-    path: '../apps/calc/Calc.html',
-    description: 'A simple calculator application.',
-    keywords: ['app'],
-    image: 'Calc.png'
-  },
-  {
-    model_: 'Demo',
-    name: 'Calculator (Material-Design)',
-    path: '../apps/acalc/Calc.html',
-    description: 'A calculator application with an animated Material-Design interface.',
-    keywords: ['app', 'material-design'],
-    image: 'ACalc.png'
-  },
-  {
-    model_: 'Demo',
-    name: 'GMail (Material-Design)',
-    path: '../apps/gmail/main.html',
-    description: 'A simple mobile GMail client with a Material-Design interface, in less than 1k lines of code.',
-    keywords: ['app', 'material-design', 'gmail', 'mobile'],
-    image: 'GMail.png'
-  },
-  {
-    model_: 'Demo',
-    name: 'QuickBug',
-    path: 'https://chrome.google.com/webstore/detail/quickbug/hmdcjljmcglpjnmmbjhpialomleabcmg',
-    description: 'A Chrome packaged-app clone of the crbug.com issue tracker.  Provides many extra features and improved performance.  Be sure to try out the grid-view with a PIE chart and warped scrolling.',
-    keywords: ['app'],
-    image: 'QuickBug.png'
-  },
-  {
-    model_: 'Demo',
-    name: 'Issue Tracker (Material-Design)',
-    path: '../apps/mbug/main.html',
-    description: 'A simple mobile code.google.com issue-tracker client with a Material-Design interface.  Triage your Crbugs on the go.',
-    keywords: ['app', 'material-design', 'mobile'],
-    image: 'MBug.png'
-  },
-  {
-    model_: 'Demo',
-    name: 'Phone Catalog',
-    path: '../apps/phonecat/Cat.html',
-    description: '',
-    keywords: [],
-    image: 'PhoneCat.png'
-  },
-  {
-    model_: 'Demo',
-    name: 'Quick-Compose',
-    path: 'https://chrome.google.com/webstore/detail/quickcompose/elckoikggmpkacmbmpbgdepginigahja',
-    description: 'A Chrome App for composing (and sending) quick GMails.',
-    keywords: ['app'],
-    image: 'QuickCompose.png'
-  },
-  {
-    model_: 'Demo',
-    name: 'Todo',
-    path: '../apps/todo/Todo.html',
-    description: 'A FOAM implementation of the http://todomvc.com comparison application.',
-    keywords: [],
-    image: 'Todo.png'
-  },
-  {
-    model_: 'Demo',
-    name: 'FOAM Code Browser',
-    path: '../core/fobrowser.html',
-    description: 'A FOAM Model browser.',
-    keywords: [],
-    image: 'FOBrowser.png'
   },
   {
     model_: 'Demo',
@@ -266,6 +198,103 @@ var demos = [
     description: 'An animated colour wheel.  Ported from the Elm demo.',
     keywords: ['animation'],
     image: 'Complements.png'
+  },
+  {
+    model_: 'Demo',
+    name: 'Dragon',
+    path: 'Tags.html',
+    description: 'Demonstrates use of the FOAM tag to instantiate three views: an animated dragon, a time-wheel, and a DetailView of time.  Use the time-wheel to control the animation.',
+    keywords: ['animation'],
+    image: 'Dragon.png'
+  },
+  {
+    model_: 'Demo',
+    name: 'Two-Way Data-Binding',
+    path: 'TwoWayDataBinding.html',
+    description: 'Demonstrates how to do two way data-binding in FOAM. See the same demo implemented with other JS libraries at: http://n12v.com/2-way-data-binding/?hn',
+    keywords: ['tutorial'],
+    image: 'TwoWayDataBinding.png'
+  },
+  {
+    model_: 'Demo',
+    name: 'Calculator',
+    path: '../apps/calc/Calc.html',
+    description: 'A simple calculator application.',
+    keywords: ['app'],
+    src: '../apps/calc/',
+    image: 'Calc.png'
+  },
+  {
+    model_: 'Demo',
+    name: 'Calculator (Material-Design)',
+    path: '../apps/acalc/Calc.html',
+    description: 'A calculator application with an animated Material-Design interface.',
+    keywords: ['app', 'material-design'],
+    src: '../apps/acalc/',
+    image: 'ACalc.png'
+  },
+  {
+    model_: 'Demo',
+    name: 'GMail (Material-Design)',
+    path: '../apps/gmail/main.html',
+    description: 'A simple mobile GMail client with a Material-Design interface, in less than 1k lines of code.',
+    keywords: ['app', 'material-design', 'gmail', 'mobile'],
+    src: '../apps/gmail/',
+    image: 'GMail.png'
+  },
+  {
+    model_: 'Demo',
+    name: 'QuickBug',
+    path: 'https://chrome.google.com/webstore/detail/quickbug/hmdcjljmcglpjnmmbjhpialomleabcmg',
+    description: 'A Chrome packaged-app clone of the crbug.com issue tracker.  Provides many extra features and improved performance.  Be sure to try out the grid-view with a PIE chart and warped scrolling.',
+    keywords: ['app'],
+    src: '../apps/quickbug/',
+    image: 'QuickBug.png'
+  },
+  {
+    model_: 'Demo',
+    name: 'Issue Tracker (Material-Design)',
+    path: '../apps/mbug/main.html',
+    description: 'A simple mobile code.google.com issue-tracker client with a Material-Design interface.  Triage your Crbugs on the go.',
+    keywords: ['app', 'material-design', 'mobile'],
+    src: '../apps/mbug/',
+    image: 'MBug.png'
+  },
+  {
+    model_: 'Demo',
+    name: 'Phone Catalog',
+    path: '../apps/phonecat/Cat.html',
+    description: '',
+    keywords: ['tutorial'],
+    src: '../apps/phonecat/',
+    image: 'PhoneCat.png'
+  },
+  {
+    model_: 'Demo',
+    name: 'Quick-Compose',
+    path: 'https://chrome.google.com/webstore/detail/quickcompose/elckoikggmpkacmbmpbgdepginigahja',
+    description: 'A Chrome App for composing (and sending) quick GMails.',
+    keywords: ['app'],
+    src: '../apps/quickcompose/',
+    image: 'QuickCompose.png'
+  },
+  {
+    model_: 'Demo',
+    name: 'Todo',
+    path: '../apps/todo/Todo.html',
+    description: 'A FOAM implementation of the http://todomvc.com comparison application.',
+    keywords: ['tutorial'],
+    src: '../apps/todo/',
+    image: 'Todo.png'
+  },
+  {
+    model_: 'Demo',
+    name: 'FOAM Code Browser',
+    path: '../core/fobrowser.html',
+    description: 'A FOAM Model browser.',
+    keywords: ['tool', 'dev'],
+    src: '../core/fobrowser.js',
+    image: 'FOBrowser.png'
   },
 ].dao;
 
@@ -279,11 +308,13 @@ MODEL({ name: 'DemoView', extendsModel: 'DetailView', templates: [
   function toHTML() {/*
       <li class="thumbnail">
         <a href="%%data.path" class="thumb">$$name{mode: 'read-only'}</a>
+        <br>
         <% if ( this.data.image ) { %> <br><img width=250 height=250 src="democat/%%data.image"> <% } %>
         <p>$$description{mode: 'read-only'}</p>
-        <b>Keywords:</b> $$keywords{mode: 'read-only'}
+        <b>Keywords:</b> <%= this.data.keywords.join(', ') %><br>
+        <b>Source:</b> <a href="%%data.src">here</a><br>
         <% if ( VIDEO_PATH && this.data.video ) { %>
-        <br><b>Video:</b> <a href="<%= VIDEO_PATH + this.data.video%>"><img style="vertical-align:middle;" width=30 height=30 src="democat/movie-clip-icon.png"></a>
+        <b>Video:</b> <a href="<%= VIDEO_PATH + this.data.video%>"><img style="vertical-align:middle;" width=30 height=30 src="democat/movie-clip-icon.png"></a>
         <% } %>
         <br>
       </li>
