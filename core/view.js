e2ab7e8a58ba4e23a8fc1af5b229c51bdf586670
Kevin Greer <kgr@chromium.org>
2014-10-03 19:16:23 -0400

    Tooltip is now set in PropertyView.

--- core/view.js
+++ core/view.js
@@ -894,6 +894,7 @@ MODEL({
       view.copyFrom(this.args);
       view.parent = this.parent;
       view.prop = this.prop;
+      if ( this.prop.description || this.prop.help ) view.tooltip = this.prop.description || this.prop.help;
 
       this.view = view;
       this.bindData(this.data);
