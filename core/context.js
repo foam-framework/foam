0826fff03f63181bfbf18ecedcbb487ee2a8aa01
kgrgreer <kgrgreer@gmail.com>
2014-10-23 16:39:41 -0400

    Experimental package code.

--- core/context.js
+++ core/context.js
@@ -110,30 +110,9 @@ function subWindow(w, opt_name, isBackground) {
 // Using the existence of 'process' to determine that we're running in Node.
 var X = this.subWindow(window, 'DEFAULT WINDOW', typeof process === 'object').sub({IN_WINDOW: false});
 
-function registerPackagePath(/* String */ path) {
-  if ( ! path ) return this;
-  if ( path ) console.log('path: ', path);
-  var p = path.split('.');
-  var s = this;
-  for ( var i = 0 ; i < p.length ; i++ ) {
-    s = s.registerPackage(p[i]);
-  }
-  return s;
-}
-
-function registerPackage(/* String */ name) {
-  if ( ! this[name] ) {
-    this[name] = {
-      __proto__: this,
-      X: this
-    };
-  }
-
-  return this[name];
-}
-
 function registerModel(model, opt_name) {
-  var thisX = this.registerPackagePath(model.package);
+  if ( this !== GLOBAL ) debugger;
+  var thisX = this;
 
   var thisModel = thisX === GLOBAL ? model : {
     __proto__: model,
