ba10d5cb0c3b027b35564f5c55e30470325c9a2b
kgrgreer <kgrgreer@gmail.com>
2014-10-30 14:16:53 -0400

    Moved 'patterns' to constants:.

--- apps/quickbug/ui/QIssueCLView.js
+++ apps/quickbug/ui/QIssueCLView.js
@@ -8,7 +8,7 @@ var QIssueCLView = FOAM({
     { name: 'dao' }
   ],
 
-  methods: {
+  constants: {
     patterns: [
       [
         /https:\/\/codereview.chromium.org\/(\d+)/,
@@ -26,10 +26,11 @@ var QIssueCLView = FOAM({
         /<a href="\/p\/((\w|-)+)\/source\/detail\?r=(\w+)">revision (\w+)<\/a>/,
         function(r) { console.assert(false, 'Bad pattern: /source/detail'); }
       ],
-    ],
-    toHTML: function() {
-      return '<div id="' + this.id + '"></div>';
-    },
+    ]
+  },
+
+  methods: {
+    toHTML: function() { return '<div id="' + this.id + '"></div>' },
     initHTML: function() {
       var self = this;
       var out = '';
@@ -51,9 +52,7 @@ var QIssueCLView = FOAM({
             }
           }
         }
-      }})(function() {
-        if ( self.$ ) self.$.innerHTML = out;
-      });
+      }})(function() { if ( self.$ ) self.$.innerHTML = out; });
     }
   }
 
