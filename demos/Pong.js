286b1ba9189a3b0eff1a1174ab36bc6d1acc1d7f
kgrgreer <kgrgreer@gmail.com>
2014-11-07 23:26:55 -0500

    Pong: Moved CSS from HTML file to Pong Model.

--- demos/Pong.js
+++ demos/Pong.js
@@ -114,6 +114,24 @@ MODEL({
   ],
 
   templates: [
+    function CSS() {/*
+      span[name="lScore"] {
+        color: white;
+        position: absolute;
+        top: 20;
+        left: 300;
+        font-family: sans-serif;
+        font-size: 70px;
+      }
+      span[name="rScore"] {
+        color: white;
+        position: absolute;
+        top: 20;
+        left: 450;
+        font-family: sans-serif;
+        font-size: 70px;
+      }
+    */},
     function toDetailHTML() {/*
       <div id="%%id">$$lScore{mode: 'read-only'} $$rScore{mode: 'read-only'} <br> %%data.table</div>
     */}
