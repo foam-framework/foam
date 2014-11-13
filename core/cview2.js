d7dcef21ce338973b4320c8366bc7539b758b6b4
Jackson Stone <jacksonic@google.com>
2014-11-13 09:26:30 -0500

    Docs Updated, CView2 paint updates are A-OK

--- core/cview2.js
+++ core/cview2.js
@@ -18,38 +18,72 @@ MODEL({
   name: 'AbstractCViewView',
   extendsModel: 'View',
 
+  documentation: function() {  /*
+    Forming the DOM component for a $$DOC{ref:'CView2',text:'canvas view'},
+    the $$DOC{ref:'.'} provides a canvas and DOM event integration. When you
+    create a $$DOC{ref:'CView2'} and $$DOC{ref:'CView2.write'} it into your
+    document, an $$DOC{ref:'.'} is created automatically to host your view.</p>
+    <p>Changes to your $$DOC{ref:'CView2'} or its children ripple down and
+    cause a repaint, starting with a $$DOC{ref:'AbstractCViewView.paint'} call.
+  */},
+
+
   properties: [
     {
       name: 'cview',
+      type: 'CView2',
       postSet: function(_, cview) {
         cview.view = this;
-      }
+      },
+      documentation: function() {/*
+          The $$DOC{ref:'CView2'} root node that contains all the content to render.
+        */}
     },
     {
       name: 'className',
       help: 'CSS class name(s), space separated.',
-      defaultValue: ''
+      defaultValue: '',
+      documentation: function() {/*
+          CSS class name(s), space separated.
+        */}
     },
     {
       model_: 'IntProperty',
       name: 'scalingRatio',
       preSet: function(_, v) { if ( v < 0 ) return 1; return v; },
       postSet: function(_, v) { console.log('Scaling to: ' , v); },
-      defaultValue: 1
+      defaultValue: 1,
+      documentation: function() {/*
+          If scaling is required to render the canvas at a higher resolution than
+          CSS pixels (for high DPI devices, for instance), the scaling value can
+          be used to set the pixel scale. This is set automatically by
+          $$DOC{ref:'.initHTML'}.
+        */}
     },
     {
       model_: 'IntProperty',
       name:  'width',
-      defaultValue: 100
+      defaultValue: 100,
+      documentation: function() {/*
+          The CSS width of the canvas. See also $$DOC{ref:'.canvasWidth'} and
+          $$DOC{ref:'.styleWidth'}.
+        */}
     },
     {
       model_: 'IntProperty',
       name:  'height',
-      defaultValue: 100
+      defaultValue: 100,
+      documentation: function() {/*
+          The CSS height of the canvas. See also $$DOC{ref:'.canvasHeight'} and
+          $$DOC{ref:'.styleHeight'}.
+        */}
     },
     {
       name: 'canvas',
-      getter: function() { return this.$ && this.$.getContext('2d'); }
+      getter: function() { return this.$ && this.$.getContext('2d'); },
+      documentation: function() {/*
+          The HTML canvas context. Use this to render.
+        */}
     }
   ],
 
@@ -64,7 +98,10 @@ MODEL({
         this.$.height       = this.canvasHeight();
         this.$.style.height = this.styleHeight();
         this.paint();
-      }
+      },
+      documentation: function() {/*
+          Reacts to resize events to fix the size of the canvas.
+        */}
     },
     {
       name: 'paint',
@@ -76,30 +113,35 @@ MODEL({
         this.cview.erase();
         this.cview.paint();
         this.canvas.restore();
-      }
+      },
+      documentation: function() {/*
+          Clears the canvas and triggers a repaint of the root $$DOC{ref:'CView2'}
+          and its children.
+        */}
     }
   ],
 
   methods: {
-    init: function() {
+    init: function() { /* Connects resize listeners. */
       this.SUPER();
       this.X.dynamic(function() { this.scalingRatio; this.width; this.height; }.bind(this),
                      this.resize);
     },
 
-    styleWidth:   function() { return (this.width) + 'px'; },
-    canvasWidth:  function() { return this.width * this.scalingRatio; },
-    styleHeight:  function() { return (this.height) + 'px'; },
-    canvasHeight: function() { return this.height * this.scalingRatio; },
+    styleWidth:   function() { /* The CSS width string */ return (this.width) + 'px'; },
+    canvasWidth:  function() { /* The scaled width */ return this.width * this.scalingRatio; },
+    styleHeight:  function() { /* The CSS height string */ return (this.height) + 'px'; },
+    canvasHeight: function() { /* The scaled height */ return this.height * this.scalingRatio; },
 
-    toString: function() { return 'CViewView(' + this.cview + ')'; },
+    toString: function() { /* The description of this. */ return 'CViewView(' + this.cview + ')'; },
 
-    toHTML: function() {
+    toHTML: function() { /* Creates the canvas element. */
       var className = this.className ? ' class="' + this.className + '"' : '';
       return '<canvas id="' + this.id + '"' + className + ' width="' + this.canvasWidth() + '" height="' + this.canvasHeight() + '" style="width:' + this.styleWidth() + ';height:' + this.styleHeight() + '"></canvas>';
     },
 
-    initHTML: function() {
+    initHTML: function() { /* Computes the scaling ratio from the window.devicePixelRatio
+                              and canvas.backingStoreRatio. */
       if ( ! this.$ ) return;
 
       this.maybeInitTooltip();
@@ -123,7 +165,7 @@ MODEL({
       this.paint();
     },
 
-    destroy: function() {
+    destroy: function() { /* Call to clean up this and child views. */
       this.SUPER();
       this.cview.destroy();
     }
