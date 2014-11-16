d91697784f0924bfea4c9e45e95631fb53719407
kgrgreer <kgrgreer@gmail.com>
2014-11-16 17:14:47 -0500

    DAOView no longer requires an explicit <rowView> inner tag to configure with foam tag.

--- core/daoView.js
+++ core/daoView.js
@@ -478,6 +478,16 @@ MODEL({
         this.children = [];
         this.painting = false;
       }.bind(this));
+    },
+
+    /** Allow rowView to be optional when defined using HTML. **/
+    fromElement: function(e) {
+      var children = e.children;
+      if ( children.length == 1 && children[0].nodeName === 'rowView' ) {
+        this.SUPER(e);
+      } else {
+        this.rowView = e.innerHTML;
+      }
     }
   },
 
@@ -1073,7 +1083,7 @@ MODEL({
           if ( ! self.$ ) return;
           self.container$.style.height = (self.count * self.rowSize) + 'px';
         });
-      
+
       this.X.dynamic(
         function() { self.objs; self.offset; },
         function() { self.render(); });
