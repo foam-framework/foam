8e851a3a796e2d7ae8265eb77fb70132482bbb91
kgrgreer <kgrgreer@gmail.com>
2014-11-24 14:30:22 -0500

    Added comment.

--- core/FObject.js
+++ core/FObject.js
@@ -49,7 +49,7 @@ var FObject = {
     if ( o.model_ ) {
       var agents = this.initAgents();
       for ( var i = 0 ; i < agents.length ; i++ ) agents[i][1](o, o.X, args);
-    } else debugger;
+    } else debugger; // TODO: if this happens, then document why/when, if not, remove
 
     o.init(args);
 
