4a7d0d581885a583acdb115d0c8c7edf1ef367cf
Braden Shepherdson <braden.shepherdson@gmail.com>
2014-12-01 17:29:31 -0500

    Spinner: Lift Polymer's spinner component. Buttery 60fps now.
    Works great on mobile, but broken on Firefox.
    The Polymer one works great on Firefox, not sure what's different about ours.
    It's got the -moz- prefix and so on.

--- core/view.js
+++ core/view.js
@@ -5209,15 +5209,22 @@ CLASS({
   extendsModel: 'View',
   documentation: 'Renders a spinner in the Material Design style.',
   // TODO(braden): This spinner renders badly on Firefox.
+  properties: [
+    {
+      name: 'color',
+      documentation: 'The color to use for the spinner.',
+      defaultValue: '#4285F4'
+    }
+  ],
+
   constants: {
-    COLOR: '#4285F4',
-    OFFSET: '187',
-    DURATION: '1.4s'
+    DURATION: '1333'
   },
 
   templates: [
     function CSS() {/*
       <% var prefixes = ['-webkit-', '-moz-', '']; %>
+      <% var bezier = 'cubic-bezier(0.4, 0.0, 0.2, 1)'; %>
       .spinner-container {
         display: flex;
         align-items: center;
@@ -5225,49 +5232,134 @@ CLASS({
         height: 100%;
         width: 100%;
       }
-      .spinner {
+      .spinner-fixed-box {
+        position: relative;
+        height: 64px;
+        width: 64px;
         <% for (var i = 0; i < prefixes.length; i++) { %>
-          <%= prefixes[i] %>animation: spinner-rotator <%= this.DURATION %> linear infinite;
+          <%= prefixes[i] %>transform: translate3d(0px, 0px, 0px);
         <% } %>
       }
 
+      .spinner-turning-box {
+        <% for (var i = 0; i < prefixes.length; i++) { %>
+          <%= prefixes[i] %>animation: container-rotate 1568ms linear infinite;
+        <% } %>
+        width: 100%;
+        height: 100%;
+      }
+
+      .spinner-layer {
+        position: absolute;
+        height: 100%;
+        width: 100%;
+        <% for (var j = 0; j < prefixes.length; j++) { %>
+          <%= prefixes[j] %>animation: fill-unfill-rotate <%= 4*this.DURATION %>ms <%= bezier %> infinite both;
+        <% } %>
+      }
+
+      .spinner-circle-clipper {
+        overflow: hidden;
+        border-color: inherit;
+        display: inline-block;
+        height: 100%;
+        position: relative;
+        width: 50%;
+      }
+
+      .spinner-circle-clipper.spinner-clipper-left .spinner-circle {
+        <% for (var i = 0; i < prefixes.length; i++) { %>
+          <%= prefixes[i] %>animation: left-spin <%= this.DURATION %>ms <%= bezier %> infinite;
+          <%= prefixes[i] %>transform: rotate(129deg);
+        <% } %>
+        border-right-color: transparent !important;
+      }
+
+      .spinner-circle-clipper.spinner-clipper-right .spinner-circle {
+        <% for (var i = 0; i < prefixes.length; i++) { %>
+          <%= prefixes[i] %>animation: right-spin <%= this.DURATION %>ms <%= bezier %> infinite;
+          <%= prefixes[i] %>transform: rotate(-129deg);
+        <% } %>
+        border-left-color: transparent !important;
+        left: -100%
+      }
+
+      .spinner-circle-clipper .spinner-circle {
+        width: 200%;
+      }
+
+      .spinner-circle {
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        box-sizing: border-box;
+        height: 100%;
+        border-width: 4px;
+        border-style: solid;
+        border-color: inherit;
+        border-bottom-color: transparent !important;
+        border-radius: 50%;
+        <% for (var i = 0; i < prefixes.length; i++) { %>
+          <%= prefixes[i] %>animation: none;
+        <% } %>
+      }
+
+      .spinner-gap-patch {
+        position: absolute;
+        box-sizing: border-box;
+        top: 0;
+        left: 45%;
+        width: 10%;
+        height: 100%;
+        overflow: hidden;
+        border-color: inherit;
+      }
+
+      .spinner-gap-patch .spinner-circle {
+        width: 1000%;
+        left: -450%;
+      }
+
       <% for (var i = 0; i < prefixes.length; i++) { %>
-        @<%= prefixes[i] %>keyframes spinner-rotator {
-          0% { transform: rotate(0deg); }
-          100% { transform: rotate(270deg); }
+        @<%= prefixes[i] %>keyframes fill-unfill-rotate {
+          12.5% { <%= prefixes[i] %>transform: rotate(135deg); }
+          25%   { <%= prefixes[i] %>transform: rotate(270deg); }
+          37.5% { <%= prefixes[i] %>transform: rotate(405deg); }
+          50%   { <%= prefixes[i] %>transform: rotate(540deg); }
+          62.5% { <%= prefixes[i] %>transform: rotate(675deg); }
+          75%   { <%= prefixes[i] %>transform: rotate(810deg); }
+          87.5% { <%= prefixes[i] %>transform: rotate(945deg); }
+          to    { <%= prefixes[i] %>transform: rotate(1080deg); }
         }
-      <% } %>
 
-      .spinner-path {
-        stroke: <%= this.COLOR %>;
-        stroke-dasharray: <%= this.OFFSET %>;
-        stroke-dashoffset: 120;
-        transform-origin: center;
-        <% for (var i = 0; i < prefixes.length; i++) { %>
-          <%= prefixes[i] %>animation: spinner-dash <%= this.DURATION %> ease-in-out infinite;
-        <% } %>
-      }
+        @<%= prefixes[i] %>keyframes left-spin {
+          from { <%= prefixes[i] %>transform: rotate(130deg); }
+          50%  { <%= prefixes[i] %>transform: rotate(-5deg); }
+          to   { <%= prefixes[i] %>transform: rotate(130deg); }
+        }
 
-      <% for (var i = 0; i < prefixes.length; i++) { %>
-        @<%= prefixes[i] %>keyframes spinner-dash {
-          0% { stroke-dashoffset: <%= this.OFFSET %>; }
-          50% {
-            stroke-dashoffset: <%= Math.floor(this.OFFSET / 4) %>;
-            transform: rotate(135deg);
-          }
-          100% {
-            stroke-dashoffset: <%= this.OFFSET %>;
-            transform: rotate(450deg);
-          }
+        @<%= prefixes[i] %>keyframes right-spin {
+          from { <%= prefixes[i] %>transform: rotate(-130deg); }
+          50%  { <%= prefixes[i] %>transform: rotate(5deg); }
+          to   { <%= prefixes[i] %>transform: rotate(-130deg); }
+        }
+
+        @<%= prefixes[i] %>keyframes container-rotate {
+          to { <%= prefixes[i] %>transform: rotate(360deg);
         }
       <% } %>
     */},
     function toHTML() {/*
       <div id="%%id" class="spinner-container">
-        <svg class="spinner" width="3cm" height="3cm" viewBox="0 0 66 66">
-          <circle class="spinner-path" fill="none" stroke-width="6"
-              stroke-linecap="round" cx="33" cy="33" r="30"></circle>
-        </svg>
+        <div class="spinner-fixed-box">
+          <div class="spinner-turning-box">
+            <div class="spinner-layer" style="border-color: <%= this.color %>">
+              <div class="spinner-circle-clipper spinner-clipper-left"><div class="spinner-circle"></div></div><div class="spinner-gap-patch"><div class="spinner-circle"></div></div><div class="spinner-circle-clipper spinner-clipper-right"><div class="spinner-circle"></div></div>
+            </div>
+          </div>
+        </div>
       </div>
     */}
   ]
