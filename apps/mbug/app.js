d5889f61920d8e42ac4d3b37eacf8f748514c502
Adam Van Ymeren <adamvy@google.com>
2014-12-02 15:33:44 -0500

    Remove the big arequire mess in MBug/app.js, require on models requiring what they need instead.

--- apps/mbug/app.js
+++ apps/mbug/app.js
@@ -11,75 +11,10 @@ window.onload = function() {
     name: 'MBug'
   }));
 
-  aseq(apar(arequire('Action'),
-            arequire('Arg'),
-            arequire('Method'),
-            arequire('Interface'),
-            arequire('Template'),
-            arequire('Relationship'),
-            arequire('AutocompleteView'),
-            arequire('WindowHashValue'),
-            arequire('AutocompleteListView'),
-            arequire('VerticalScrollbarView'),
-            arequire('ActionSheetView'),
-            arequire('StackView'),
-            arequire('GridView'),
-            arequire('ScrollView'),
-            arequire('Expr'),
-            arequire('AbstractDAO'),
-            arequire('DAOController'),
-            arequire('DAOCreateController'),
-            arequire('OAuth2ChromeIdentity'),
-            arequire('OAuth2ChromeApp'),
-            arequire('AddBookmarkDialog'),
-            arequire('TextFieldView'),
-            arequire('View'),
-            arequire('AppController'),
-            arequire('MDMonogramStringView'),
-            arequire('CursorView'),
-            arequire('QIssueDetailView'),
-            arequire('QIssueCommentCreateView'),
-            arequire('QIssueCommentUpdateDetailView'),
-            arequire('QIssueTileView'),
-            arequire('ConfigureProjectsView'),
-            arequire('MementoMgr'),
-            arequire('Browser'),
-            arequire('PriorityView'),
-            arequire('PriorityCitationView'),
-            arequire('IssueView'),
-            arequire('CitationView'),
-            arequire('PersonView'),
-            arequire('DefaultRowView'),
-            arequire('DefaultACRowView'),
-            arequire('AddRowView'),
-            arequire('AutocompleteListView'),
-            arequire('mdTextFieldView'),
-            arequire('MDMonogramStringView'),
-            arequire('IssueCitationView'),
-            arequire('CommentView'),
-            arequire('ChangeProjectView'),
-            arequire('Application'),
-            arequire('IDBDAO'),
-            arequire('DAOVersion'),
-            arequire('MBug'),
-            arequire('QBug'),
-            arequire('PersistentContext'),
-            arequire('Binding'),
-            arequire('RestDAO'),
-            arequire('TouchManager'),
-            arequire('GestureManager'),
-            arequire('ScrollGesture'),
-            arequire('TapGesture'),
-            arequire('DragGesture'),
-            arequire('PinchTwistGesture'),
-            arequire('DetailView'),
-            arequire('OAuth2'),
-            arequire('OAuthXhrFactory'),
-            arequire('QIssueCommentAuthorView')),
-       function(ret) {
-         var w = Y.Window.create({ window: window });
-         mbug = Y.MBug.create();
-         w.view = mbug;
-         ret();
-       })(function(){});
+  arequire('MBug')(
+    function() {
+      var w = Y.Window.create({ window: window });
+      mbug = Y.MBug.create();
+      w.view = mbug;
+    });
 };
