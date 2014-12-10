441e49472f7e1ffb57455dd46fbe65a143135ed2
kgrgreer <kgrgreer@gmail.com>
2014-12-10 09:11:20 -0500

    ACalc: remove blank strip between display and keypad that will mess up flair.

--- apps/acalc/Calc.js
+++ apps/acalc/Calc.js
@@ -184,6 +184,7 @@ CLASS({
     }
 
     .edge2 {
+      margin-top: -12px;
       background: linear-gradient(to bottom, rgba(0,0,0,0.5) 0%,
                                              rgba(0,0,0,0) 100%);
       top: 12px;
