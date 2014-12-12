74b6eceda263b9fefc953ffce17f909b35bd34e6
Adam Van Ymeren <adamvy@google.com>
2014-12-12 15:25:46 -0500

    Fix build script for quickbug.

--- apps/quickbug/build.sh
+++ apps/quickbug/build.sh
@@ -4,23 +4,23 @@ rm -rf $BUILD_DIR
 cp -r . $BUILD_DIR
 cp ../../core/foam.css $BUILD_DIR
 cat \
+  ../../core/i18n.js \
   ../../core/stdlib.js \
   ../../core/io.js \
   ../../core/writer.js \
   ../../core/socket.js \
   ../../core/base64.js \
+  ../../core/encodings.js \
   ../../core/utf8.js \
+  ../../core/async.js \
   ../../core/parse.js \
   ../../core/event.js \
   ../../core/JSONUtil.js \
-  ../../core/HTMLParser.js \
   ../../core/XMLUtil.js \
   ../../core/context.js \
-  ../../core/FOAM.js \
   ../../core/JSONParser.js \
   ../../core/TemplateUtil.js \
-  ../../core/async.js \
-  ../../core/oam.js \
+  ../../core/FOAM.js \
   ../../core/FObject.js \
   ../../core/BootstrapModel.js \
   ../../core/mm1Model.js \
@@ -28,38 +28,53 @@ cat \
   ../../core/mm3Types.js \
   ../../core/mm4Method.js \
   ../../core/mm5Misc.js \
+  ../../core/LayoutTraits.js \
   ../../core/value.js \
   ../../core/view.js \
+  ../../core/view2.js \
   ../../core/layout.js \
   ../../core/daoView.js \
   ../../core/ChoiceView.js \
   ../../core/DetailView.js \
   ../../core/TableView.js \
   ../../core/cview.js \
-  ../../core/MementoMgr.js \
+  ../../core/cview2.js \
+  ../../core/RichTextView.js \
   ../../core/listchoiceview.js \
   ../../core/scroll.js \
+  ../../core/CViewPrimitives.js \
+  ../../core/Diagramming.js \
+  ../../core/HTMLParser.js \
   ../../core/mlang.js \
-  ../../core/glang.js \
+  ../../core/mlang2.js \
   ../../core/QueryParser.js \
   ../../core/search.js \
+  ../../core/oam.js \
   ../../core/visitor.js \
+  ../../core/messaging.js \
   ../../core/dao.js \
-  ../../core/ChromeStorage.js \
+  ../../core/dao2.js \
   ../../core/arrayDAO.js \
+  ../../core/ClientDAO.js \
   ../../core/diff.js \
   ../../core/SplitDAO.js \
   ../../core/index.js \
+  ../../core/StackView.js \
+  ../../core/MementoMgr.js \
   ../../core/DAOController.js \
   ../../core/ThreePaneController.js \
   ../../core/experimental/protobufparser.js \
   ../../core/experimental/protobuf.js \
   ../../core/models.js \
-  ../../core/oauth.js \
   ../../core/touch.js \
-  ../../core/ChromeApp.js \
-  ../../core/SyncManager.js \
+  ../../core/glang.js \
+  ../../core/oauth.js \
+  ../../core/busy.js \
   ../../lib/bookmarks/bookmark.js \
+  ../../core/turntable.js \
+  ../../core/ChromeApp.js \
+  ../../core/ChromeStorage.js \
+  ../../core/SyncManager.js \
   ../../core/CORE.js \
   > "$BUILD_DIR/foam.js"
 
