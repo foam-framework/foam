6ab6a363e2cc2eca91001a09c528d3231dde4ad5
Adam Van Ymeren <adamvy@google.com>
2014-12-16 12:02:30 -0500

    Add a basic ModelFileDAO that can load a model from a .js file when given its fully qualifies name.

---
+++ js/foam/core/bootstrap/ModelFileDAO.js
@@ -0,0 +1,25 @@
+MODEL({
+  name: 'ModelFileDAO',
+  package: 'foam.core.bootstrap',
+  methods: {
+    find: function (key, sink) {
+      var X = this.X;
+      var model = FOAM.lookup(key, X);
+
+      var tag = this.X.document.createElement('script');
+      tag.src = '/js/' + key.replace(/\./g, '/') + '.js';
+      this.X.document.head.appendChild(tag);
+
+      tag.onload = function() {
+//        this.removeChild(tag);
+
+        model = FOAM.lookup(key, X);
+        if ( ! model ) {
+          sink && sink.error && sink.error('Model load failed for: ', key);
+          return;
+        }
+        sink && sink.put && sink.put(model);
+      }.bind(this.X.document.head);
+    }
+  }
+});
