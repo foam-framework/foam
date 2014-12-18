09347dda2ec5fa225c0cd5f908ca34f18b2e74d1
Adam Van Ymeren <adamvy@google.com>
2014-12-18 16:03:36 -0500

    Move ResponsiveController into the js/ package hierarchy.

---
+++ js/foam/ui/layout/ControllerOption.js
@@ -0,0 +1,8 @@
+CLASS({
+  package: 'foam.ui.layout',
+  name: 'ControllerOption',
+  properties: [
+    { model_: 'ViewFactoryProperty', name: 'controller' },
+    { model_: 'IntProperty', name: 'minWidth' }
+  ]
+});
