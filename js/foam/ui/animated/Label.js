776c506a1318d8c5f8233e2071d24ae15b20f1f0
Kevin Greer <kgr@chromium.org>
2014-12-19 16:57:59 -0500

    ACalc: Fixed row1 animation consistency.

--- js/foam/ui/animated/Label.js
+++ js/foam/ui/animated/Label.js
@@ -24,13 +24,31 @@ CLASS({
 
   methods: {
     toInnerHTML: function() {
-      return '<div style="white-space: nowrap;position:absolute;transition: left .3s ease;" class="f1"></div><div style="white-space: nowrap;display:inline;visibility:hidden;float: right;" class="f2"></div>';
+      return '<div class="f1"></div><div class="f2"></div>';
     },
     initHTML: function() {
       this.data$.addListener(this.onDataChange);
-      this.window.addEventListener('resize', this.onDataChange);
+      this.window.addEventListener('resize', this.onResize);
     }
   },
+
+  templates: [
+    function CSS() {/*
+      .f1 {
+        position: absolute;
+        white-space: nowrap;
+      }
+      .f1.animated {
+        transition: left .3s ease;
+      }
+      .f2 {
+        display: inline;
+        float: right;
+        visibility: hidden;
+        white-space: nowrap;
+      }
+    */}
+  ],
 
   listeners: [
     {
@@ -46,6 +64,16 @@ CLASS({
 
 //        f1$.style.top  = f2$.offsetTop;
         f1$.style.left = f2$.offsetLeft;
+        DOM.setClass(this.$.querySelector('.f1'), 'animated', true);
+      }
+    },
+    {
+      name: 'onResize',
+      isFramed: true,
+      code: function() {
+        if ( ! this.$ ) return;
+        DOM.setClass(this.$.querySelector('.f1'), 'animated', false);
+        this.onDataChange();
       }
     }
   ]
