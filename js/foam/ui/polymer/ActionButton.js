d0f7115e7e695294333bf15d327340988282b19c
Mark Dittmer <markdittmer@google.com>
2014-12-23 23:04:58 -0500

    Add working polymer attributes to Polymer ActionButton demo, and add a Polymer Tooltip demo

---
+++ js/foam/ui/polymer/ActionButton.js
@@ -0,0 +1,78 @@
+CLASS({
+  name: 'ActionButton',
+  package: 'foam.ui.polymer',
+
+  extendsModel: 'ActionButton',
+  traits: [
+    'foam.ui.polymer.View'
+  ],
+  requires: ['Action'],
+  imports: ['warn'],
+
+  properties: [
+    {
+      type: 'Action',
+      name: 'action',
+      factory: function() {
+        return this.Action.create({
+          action: function() {
+            this.warn('Polymer button: action not set:', this);
+            }.bind(this)
+        });
+      }
+    },
+    {
+      name: 'className',
+      defaultValue: ''
+    },
+    {
+      name: 'tagName',
+      defaultValue: 'paper-button'
+    },
+    {
+      name: 'iconUrl',
+      defaultValue: false
+    },
+    {
+      model_: 'BooleanProperty',
+      name: 'raised',
+      defaultValue: false,
+      postSet: function(prev, next) {
+        this.updateAttribute('raised', prev, next);
+      }
+    },
+    {
+      model_: 'BooleanProperty',
+      name: 'recenteringTouch',
+      defaultValue: false,
+      postSet: function(prev, next) {
+        this.updateAttribute('recenteringTouch', prev, next);
+      }
+    },
+    {
+      model_: 'BooleanProperty',
+      name: 'fill',
+      defaultValue: true,
+      postSet: function(prev, next) {
+        this.updateAttribute('fill', prev, next);
+      }
+    }
+  ],
+
+  constants: {
+    HREF: '/bower_components/paper-button/paper-button.html',
+    POLYMER_ATTRIBUTES: [
+      'raised',
+      'recenteringTouch',
+      'fill'
+    ]
+  },
+
+  templates: [
+    function CSS() {/*
+      paper-button { display: none; }
+      paper-button.actionButton { display: none; }
+      paper-button.available { display: inline-block; }
+    */}
+  ]
+});
