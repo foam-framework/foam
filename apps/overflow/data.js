f183f9a211ac21ccc3e49d28715ede7e5233f6e3
kgrgreer <kgrgreer@gmail.com>
2014-12-27 13:43:38 -0500

    Initial checkin of Q&A browser.

---
+++ apps/overflow/data.js
@@ -0,0 +1,44 @@
+var questions = JSONUtil.arrayToObjArray(X, [
+/*  {
+    id: 0,
+    questionTitle: '',
+    question: function() {/*
+
+
+    *},
+    answer: function() {/*
+
+
+    *},
+  },
+*/
+  {
+    id: 1,
+    questionTitle: 'How do I make my Models not go in the global namespace?',
+    question: function() {/*
+
+
+    *},
+    answer: function() {/*
+      Specify their 'package'.
+
+      Ex.
+      CLASS({
+        name: 'MyModel',
+        package: 'com.somedomain.myApp.somepackage'
+      });
+
+      Is now accessed as com.somedomain.myApp.somepackage.MyModel.
+      However, you would typically list this Model in the 'requires' list of Models that use it, so that you don't need to specify the full path.
+
+      Ex.
+      CLASS({
+        name: 'MyOtherModel',
+        requires: [
+          'com.somedomain.myApp.somepackage.MyModel',           // makes avaialable as 'this.MyModel'
+          'com.somedomain.myApp.somepackage.MyModel as AnAlias' // makes avaialable as this.AnAlias
+        ]
+      });
+    */},
+  }
+], Question).dao;
