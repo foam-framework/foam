42b1c9ff19c156a80dcf452dc235ef1c30b8c6b9
Kevin Greer <kgr@chromium.org>
2014-12-29 18:55:48 -0500

    Added Ask-Question to Overflow.

--- apps/overflow/Controller.js
+++ apps/overflow/Controller.js
@@ -25,17 +25,20 @@ CLASS({
   },
   templates: [
     function CSS() {/*
+      .answer { margin-top: -34px; }
     */},
     function toDetailHTML() {/*
         <h1>FOAM-Overflow</h1>
+        <a href="https://groups.google.com/forum/#!newtopic/foam-framework-discuss
+">Ask Question</a>
         &nbsp;&nbsp; Search: $$search
         <p>
         <foam f="filteredDAO" className="questions" tagName="div">
           <div>
-            $$id{mode: 'read-only'} <!-- $$created{mode: 'read-only'} -->
-            <b>Q. $$title{mode: 'read-only'}</b>
+            <b>Q$$id{mode: 'read-only'}. $$title{mode: 'read-only'}</b>
             <% if ( this.data.question.trim().length ) { %><pre>$$question{mode: 'read-only', escapeHTML: false}</pre><% } %>
-            <pre>$$answer{mode: 'read-only', escapeHTML: false}</pre>
+            <br>
+            <b>A.</b><pre class="answer">$$answer{mode: 'read-only', escapeHTML: false}</pre>
             <b>Labels:</b> <%= this.data.labels.join(', ') %><br>
             <% if ( this.data.src ) { %>
               <b>Source:</b> <a href="%%data.src">here</a><br>
@@ -45,3 +48,7 @@ CLASS({
     */}
   ]
 });
+
+/*
+$$id{mode: 'read-only'} 
+*/
