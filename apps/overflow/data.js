5c021acd5638f1add644dc080fdd331ebf0068fb
Kevin Greer <kgr@chromium.org>
2014-12-29 19:13:33 -0500

    Overflow: added more questions.

--- apps/overflow/data.js
+++ apps/overflow/data.js
@@ -1,14 +1,12 @@
 var questions = JSONUtil.arrayToObjArray(X, [
-/*  {
+/*
+  {
     id: 0,
-    questionTitle: '',
+    title: '',
+    labels: [ 'class', 'package', 'namespace' ],
     question: function() {/*
-
-
     *},
     answer: function() {/*
-
-
     *},
   },
 */
@@ -41,5 +39,52 @@ var questions = JSONUtil.arrayToObjArray(X, [
         ]
       });
     */},
-  }
+  },
+  {
+    id: 2,
+    title: 'My dynamic function which depends on multiple values isn\'t updating properly.',
+    labels: [ 'action', 'dynamic', 'reactive' ],
+    question: function() {/*
+    */},
+    answer: function() {/*
+    */},
+  },
+  {
+    id: 3,
+    title: 'How do I discover errors in my models or templates.',
+    labels: [ 'error', 'template', 'model' ],
+    question: function() {/*
+    */},
+    answer: function() {/*
+    Keep the developer console open as many errors are reported to the console.  Depending on your OS, you can open the developer tools with either F12 or Shift-Ctrl-J.
+    */},
+  },
+  {
+    id: 4,
+    title: 'Where can I find FOAM API documentation?',
+    labels: [ 'documentation', 'api' ],
+    question: function() {/*
+    */},
+    answer: function() {/*
+    */},
+  },
+  {
+    id: 5,
+    title: 'Where do I put my CSS?',
+    labels: [ 'template', 'css' ],
+    question: function() {/*
+    */},
+    answer: function() {/*
+    */},
+  },
+  {
+    id: 6,
+    title: 'Why can\'t I include instance variables in my CSS templates?',
+    labels: [ 'template', 'css' ],
+    question: function() {/*
+    */},
+    answer: function() {/*
+    */},
+  },
+
 ], Question).dao;
