f84db8a7d1983bf1665d3e5b2c567401906ef9fc
kgrgreer <kgrgreer@gmail.com>
2015-01-06 11:39:35 -0500

    Added Overflow.png to DemoCat.

--- demos/DemoCat.js
+++ demos/DemoCat.js
@@ -278,11 +278,11 @@ var demos = JSONUtil.arrayToObjArray(X, [
   {
     name: 'FOAM-Overflow',
     path: '../apps/overflow/Overflow.html',
-    description: 'A simple searchable FOAM Question and Answer database',
+    description: 'A simple searchable FOAM Question and Answer database.',
     keywords: ['docs', 'demo'],
     src: '../apps/overflow/',
     image: 'Overflow.png'
-  },
+  }
 ], Demo).dao;
 
 
