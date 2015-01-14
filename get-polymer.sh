1c618ed59f39a8bc1a403b19d31ba7567b823154
Mark Dittmer <mark.s.dittmer@gmail.com>
2015-01-14 15:57:03 -0500

    Updating get-polymer.sh: Attempts to symlink "nodejs" to "node" for bower's sake. If it can't find "apt-get", then it warns you and just tries to install bower packages (assuming that bower is already installed).

--- get-polymer.sh
+++ get-polymer.sh
@@ -1,19 +1,39 @@
 #!/bin/bash
 
+APT_GET=`which apt-get`
+NODE=`which node`
+NODEJS=`which nodejs`
 NPM=`which npm`
 BOWER=`which bower`
 
-if [ "$NPM" == "" ]; then
+if [ "$APT_GET" != "" ]; then
+  if [ "$NODEJS" == "" ]; then
+    echo "Attempting to install nodejs. You may be asked for your password."
+    sudo apt-get install nodejs
+  fi
+  NODEJS=`which nodejs`
+
+  if [ "$NPM" == "" ]; then
     echo "Attempting to install npm. You may be asked for your password."
     sudo apt-get install npm || return $?
-fi
-NPM=`which npm`
+  fi
+  NPM=`which npm`
 
-if [ "$BOWER" == "" ]; then
+  if [ "$NODE" == "" ]; then
+    NODEJS_DIR=$(dirname ${NODEJS})
+    echo "Attempting to symlink nodejs to node. You may be asked for your password."
+    sudo ln -s $NODEJS $NODEJS_DIR/node
+  fi
+  NODE=`which node`
+
+  if [ "$BOWER" == "" ]; then
     echo "Attempting to install Bower. You may be asked for your password."
     sudo $NPM install -g bower || return $?
+  fi
+  BOWER=`which bower`
+else
+  echo "You do not have apt-get. Attempting install ASSUMING you have bower (see http://bower.io)."
 fi
-BOWER=`which bower`
 
 $BOWER install Polymer/polymer
 $BOWER install Polymer/paper-elements
