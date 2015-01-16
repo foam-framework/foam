8fbeec4af3d3680971edf9d6868943209c60eaeb
Mark Dittmer <markdittmer@google.com>
2015-01-16 15:24:57 -0500

    Almost-complete DAO implementation of ChromeFileSystemDAO. No support for eof (events appear to occur strangely out of order to support this). Started but not finished naive "put everything" implementation of select.

--- js/foam/dao/ChromeFileSystem.js
+++ js/foam/dao/ChromeFileSystem.js
@@ -19,12 +19,25 @@ MODEL({
   name: 'ChromeFileSystem',
   package: 'foam.dao',
 
+  requires: [
+    'foam.dao.ChromeFile'
+  ],
+
   properties: [
     {
       name: 'cfs',
       factory: function() {
         return chrome.fileSystem;
       }
+    },
+    {
+      name: 'root',
+      defaultValue: null
+    },
+    {
+      name: 'rootChromeFile',
+      type: 'foam.dao.ChromeFile',
+      defaultValue: null
     },
     {
       model_: 'StringProperty',
@@ -67,6 +80,20 @@ MODEL({
       name: 'init',
       code: function() {
         this.SUPER();
+
+        Events.map(
+            this.root$,
+            this.rootChromeFile$,
+            function(dirEntry) {
+              return ! dirEntry ? null : this.ChromeFile.create({
+                path: dirEntry.fullPath,
+                parentPath: dirEntry.fullPath.slice(
+                    0,
+                    dirEntry.fullPath.lastIndexOf('/')),
+                isDirectory: dirEntry.isDirectory
+              });
+            }.bind(this));
+
         var config = { type: 'openDirectory' };
         if ( this.suggestedDirName )
           config.suggestedName = this.suggestedDirName;
@@ -86,73 +113,170 @@ MODEL({
     {
       name: 'getWritableEntry',
       code: function(ret, dirEntry) {
-        if ( chrome.runtime.lastError ) {
-          this.error = chrome.runtime.lastError;
-        }
-        if ( ! dirEntry || this.error ) ret(this.error);
+        if ( this.checkForError(dirEntry, ret, 'getWritableEntry') )
+          return dirEntry;
         this.cfs.getWritableEntry(dirEntry, ret);
+        return dirEntry;
       }
     },
     {
       name: 'putRoot',
       code: function(ret, dirEntry) {
-        if ( chrome.runtime.lastError ) {
-          this.error = chrome.runtime.lastError;
-        }
-        if ( ! dirEntry || this.error ) {
-          ret(this.error);
-          return this.error;
-        }
+        if ( this.checkForError(dirEntry, ret, 'putRoot') ) return dirEntry;
         this.root = dirEntry;
         this.ready = true;
-        this.error = undefined;
+        this.clearError();
         ret && ret(dirEntry);
         return dirEntry;
       }
     },
     {
-      name: 'write',
+      name: 'awrite',
       code: function(rawPath, data) {
         if ( this.mode !== 'read-write' ) throw 'Cannot write to ' +
             'non-read-write Chrome filesystem';
         var path = this.canonicalizePath(rawPath);
         if ( path[0] === '..' ) throw 'Cannot write to path: ' + path;
+        var writeCtx = { path: '/' + path.join('/') };
         var seq = [];
         for ( var i = 0; i < path.length - 1; ++i ) {
-          seq.push(this.getDirectory.bind(this, path[i]));
+          seq.push(this.getDirectory.bind(this, writeCtx, path[i]));
         }
-        seq.push(this.getFile.bind(this, path[path.length - 1]));
-        seq.push(this.createWriter.bind(this));
-        seq.push(this.writeFile.bind(this, data));
-        return aseq.apply(null, seq)();
+        seq.push(this.getFile.bind(this, writeCtx, path[path.length - 1]));
+        seq.push(this.createWriter.bind(this, writeCtx));
+        seq.push(this.writeFile.bind(this, writeCtx, data));
+        return aseq.apply(null, seq);
       }
     },
     {
-      name: 'read',
-      code: function(rawPath) {
+      name: 'aread',
+      code: function(rawPath, opt_mimeType) {
+        console.log('aread', rawPath, opt_mimeType);
         var path = this.canonicalizePath(rawPath);
         if ( path[0] === '..' ) throw 'Cannot read from: ' + path;
+        var readCtx = { mimeType: opt_mimeType };
         var seq = [];
         for ( var i = 0; i < path.length - 1; ++i ) {
-          seq.push(this.getDirectory.bind(this, path[i]));
+          seq.push(this.getDirectory.bind(this, readCtx, path[i]));
         }
-        seq.push(this.getFile.bind(this, path[path.length - 1]));
-        seq.push(this.getFile_.bind(this));
-        seq.push(this.readFile.bind(this));
-        return aseq.apply(null, seq)(function(e) {
-          console.log('Got file read', e.target.result);
-        }.bind(this));
+        seq.push(this.getFile.bind(this, readCtx, path[path.length - 1]));
+        seq.push(this.getFile_.bind(this, readCtx));
+        seq.push(this.readFile.bind(this, readCtx));
+        return aseq.apply(null, seq);
+      }
+    },
+    {
+      name: 'aentries',
+      code: function(rawPath, sink) {
+        var path = this.canonicalizePath(rawPath);
+        if ( path[0] === '..' ) throw 'Cannot get entries from: ' + path;
+        var readCtx = { sink: sink, entries: [] };
+        var seq = [];
+        for ( var i = 0; i < path.length; ++i ) {
+          seq.push(this.getDirectory.bind(this, readCtx, path[i]));
+        }
+        seq.push(this.startReadEntries.bind(this, readCtx));
+        seq.push(this.readMoreEntries.bind(this, readCtx));
+        return aseq.apply(null, seq);
+      }
+    },
+    {
+      name: 'aentriesAll',
+      code: function(rawPath, sink) {
+        console.log('aentriesAll_', rawPath);
+        var ctx = {
+          sink: sink,
+          future: afuture(),
+          entries: []
+        };
+        var allSink = {
+          put: function(X, o) {
+            X.entries.push(o);
+            X.sink && X.sink.put && X.sink.put(o);
+          }.bind(this, ctx),
+          error: function(X, e) {
+            X.sink && X.sink.error && X.sink.error(e);
+          }.bind(this, ctx)
+        };
+        this.aentriesAll_(ctx, rawPath, allSink);
+        return ctx.future.get;
+      }
+    },
+    {
+      name: 'aentriesAll_',
+      code: function(X, rawPath, sink) {
+        console.log('aentriesAll_', rawPath);
+        this.aentries(rawPath, sink)(function(X, sink, chromeFiles) {
+          chromeFiles.forEach(function(X, sink, chromeFile) {
+            if ( chromeFile.isDirectory ) {
+              var properDirName = chromeFile.path.slice(
+                  this.rootChromeFile.path.length);
+              console.log('Recurse into dir', properDirName);
+              this.aentriesAll_(X, properDirName, sink);
+            }
+          }.bind(this, X, sink));
+        }.bind(this, X, sink));
+      }
+    },
+    {
+      name: 'startReadEntries',
+      code: function(X, ret, dirEntry) {
+        console.log('startReadEntries', dirEntry);
+        if ( this.checkForError(dirEntry, undefined, 'startReadEntries') ) {
+          X && X.sink && X.sink.error && X.sink.error(this.error);
+          ret && ret(this.error);
+          return dirEntry;
+        }
+        var dirReader = dirEntry.createReader();
+        X.dirReader = dirReader;
+        dirReader.readEntries(ret, ret);
+        return dirReader;
+      }
+    },
+    {
+      name: 'readMoreEntries',
+      code: function(X, ret, entries) {
+        console.log('readMoreEntries', entries);
+        if ( this.checkForError(entries, undefined, 'startReadEntries') ) {
+          X && X.sink && X.sink.error && X.sink.error(this.error);
+          ret && ret(this.error);
+          return entries;
+        }
+        if ( ! entries.length ) {
+          ret && ret(X.entries);
+          return entries;
+        }
+        entries.forEach(function(X, entry) {
+          var chromeFile = this.ChromeFile.create({
+            path: entry.fullPath,
+            parentPath: entry.fullPath.slice(
+                0,
+                entry.fullPath.lastIndexOf('/')),
+            isDirectory: entry.isDirectory
+          });
+          if ( entry.isDirectory ) {
+            X && X.sink && X.sink.put && X.sink.put(chromeFile);
+            X.entries.push(chromeFile);
+          } else {
+            this.getFile_({}, function(chromeFile, file) {
+              chromeFile.mimeType = file.type;
+              X && X.sink && X.sink.put && X.sink.put(chromeFile);
+              X.entries.push(chromeFile);
+            }.bind(this, chromeFile), entry);
+          }
+        }.bind(this, X));
+        var cb = this.readMoreEntries.bind(this, X, ret);
+        X.dirReader.readEntries(cb, cb);
+        return X.dirReader;
       }
     },
     {
       name: 'getDirectory',
-      code: function(dirName, ret, dirEntry) {
-        if ( this.isFileError(dirEntry) ) {
-          this.error = dirEntry;
-          ret(this.error);
-          return this.error;
-        }
-        return (dirEntry || this.root).getDirectory(
+      code: function(X, dirName, ret, dirEntry) {
+        console.log('getDirectory', dirName, dirEntry);
+        var source = dirEntry || this.root;
+        if ( this.checkForError(source, ret, 'getDirectory') ) return source;
+        return source.getDirectory(
             dirName,
             this.getDirectoryConfig,
             ret,
@@ -161,13 +285,11 @@ MODEL({
     },
     {
       name: 'getFile',
-      code: function(fileName, ret, dirEntry) {
-        if ( this.isFileError(dirEntry) ) {
-          this.error = dirEntry;
-          ret(this.error);
-          return this.error;
-        }
-        return (dirEntry || this.root).getFile(
+      code: function(X, fileName, ret, dirEntry) {
+        console.log('getFile', fileName, dirEntry);
+        var source = dirEntry || this.root;
+        if ( this.checkForError(source, ret, 'getFile') ) return source;
+        return source.getFile(
             fileName,
             this.getFileConfig,
             ret,
@@ -176,32 +298,46 @@ MODEL({
     },
     {
       name: 'getFile_',
-      code: function(ret, fileEntry) {
-        if ( this.isFileError(fileEntry) ) {
-          this.error = fileEntry;
-          ret(this.error);
-          return this.error;
-        }
-        if ( ! fileEntry ) return undefined;
+      code: function(X, ret, fileEntry) {
+        console.log('getfile_', fileEntry);
+        if ( this.checkForError(fileEntry, ret, 'getFile_') ) return fileEntry;
+        X.fileEntry = fileEntry;
         return fileEntry.file(ret, ret);
       }
     },
     {
       name: 'readFile',
-      code: function(ret, file) {
-        if ( this.isFileError(file) ) {
-          this.error = file;
-          ret(this.error);
-          return this.error;
+      code: function(X, ret, file) {
+        console.log('readFile', file);
+        if ( this.checkForError(file, ret, 'readFile') ) return file;
+        if ( X.mimeType && file.type !== X.mimeType ) {
+          var errMsg = 'Unexpected MIME type: "' + file.type + '" ' +
+              '(expected "' + X.mimeType + '")';
+          this.console.warn(errMsg);
+          ret && ret({ error: { message: errMsg } });
+          return file;
         }
+
+        X.file = file;
         var fileReader = new FileReader();
-        var X = { done: false };
-        var cb = function(X, ret) {
+        var cbX = Object.create(X);
+        cbX.done = false;
+
+        var cb = function(X, ret, e) {
           if ( X.done ) return;
+          // TODO(markdittmer): We do not handle errors here (yet).
           var args = argsToArray(arguments);
           X.done = true;
-          ret && ret.apply(this, args.slice(2));
-        }.bind(this, X, ret);
+          ret && ret.call(this, this.ChromeFile.create({
+            path: X.fileEntry.fullPath,
+            parentPath: X.fileEntry.fullPath.slice(
+                0,
+                X.fileEntry.fullPath.lastIndexOf('/')),
+            isDirectory: false,
+            mimeType: X.file.type,
+            contents: e.target.result
+          }));
+        }.bind(this, cbX, ret);
         fileReader.onloadend = cb;
         fileReader.onerror = cb;
         fileReader.readAsText(file);
@@ -210,34 +346,37 @@ MODEL({
     },
     {
       name: 'createWriter',
-      code: function(ret, fileEntry) {
-        if ( this.isFileError(fileEntry) ) {
-          this.error = fileEntry;
-          ret(this.error);
-          return this.error;
-        }
+      code: function(X, ret, fileEntry) {
+        console.log('createWriter', fileEntry);
+        if ( this.checkForError(fileEntry, ret, 'crateWriter') )
+          return fileEntry;
         fileEntry.createWriter(ret, ret);
         return fileEntry;
       }
     },
     {
       name: 'writeFile',
-      code: function(data, ret, fileWriter) {
-        if ( this.isFileError(fileWriter) ) {
-          this.error = fileWriter;
-          ret(this.error);
-          return this.error;
-        }
-        var X = { done: false };
-        var cb = function(X, ret) {
+      code: function(X, data, ret, fileWriter) {
+        console.log('writeFile', data.slice(0, 19), fileWriter);
+        if ( this.checkForError(fileWriter, ret, 'writeFile') )
+          return fileWriter;
+        var cbX = Object.create(X);
+        cbX.done = false;
+        cbX.contents = data;
+        cbX.mimeType = 'text/plain';
+        var cb = function(X, ret, e) {
           if ( X.done ) return;
-          var args = argsToArray(arguments);
+          if ( this.isFileError(e) ) {
+            this.error = e;
+            X.error = e;
+          }
+          X.event = e;
           X.done = true;
-          ret && ret.apply(this, args.slice(2));
-        }.bind(this, X, ret);
+          ret && ret.call(this, X);
+        }.bind(this, cbX, ret);
         fileWriter.onwriteend = cb;
         fileWriter.onerror = cb;
-        fileWriter.write(new Blob([data], {type: 'text/plain'}));
+        fileWriter.write(new Blob([data], {type: cbX.mimeType}));
         return fileWriter;
       }
     },
@@ -259,9 +398,32 @@ MODEL({
     {
       name: 'isFileError',
       code: function(o) {
-        if ( ! o ) return false;
-        return Object.getPrototypeOf(o) === FileError.prototype;
+        return o && o.constructor &&
+            o.constructor.toString().indexOf('function FileError(') === 0;
       }
+    },
+    {
+      name: 'checkForError',
+      code: function(arg, ret, contextStr) {
+        if ( chrome.runtime.lastError ) {
+          this.error = chrome.runtime.lastError;
+        } else if ( this.isFileError(arg) ) {
+          this.error = arg;
+        } else if ( ! arg ) {
+          this.error = {
+            message: 'Missing argument in context: "' + contextStr + '"'
+          };
+        }
+        if ( this.error ) {
+          ret && ret({ error: this.error });
+          return true;
+        }
+        return false;
+      }
+    },
+    {
+      name: 'clearError',
+      code: function() { this.error = undefined; }
     }
   ]
 });
--- js/foam/dao/ChromeFileSystemDAO.js
+++ js/foam/dao/ChromeFileSystemDAO.js
@@ -24,6 +24,10 @@ MODEL({
     'foam.dao.ChromeFileSystem'
   ],
 
+  imports: [
+    'console'
+  ],
+
   properties: [
     {
       name: 'cfs',
@@ -37,21 +41,86 @@ MODEL({
     {
       name: 'put',
       code: function(o, sink) {
+        this.cfs.awrite(o.path, o.contents)(function(o, sink, status) {
+          if ( status.error ) {
+            sink && sink.error && sink.error(status.error);
+            return status.error;
+          }
+          if ( status.mimeType ) o.mimeType = status.mimeType;
+          sink && sink.put && sink.put(o);
+          return o;
+        }.bind(this, o, sink));
       }
-    }
+    },
+    {
+      name: 'find',
+      code: function(hash, sink) {
+        this.console.assert(hash.path);
+        this.cfs.aread(hash.path)(function(sink, o) {
+          if ( o.error ) {
+            sink && sink.error && sink.error(o.error);
+            return o.error;
+          }
+          sink && sink.put && sink.put(o);
+          return o;
+        }.bind(this, sink));
+      }
+    }// ,
+    // {
+    //   name: 'select',
+    //   code: function(sink, options) {
+    //     return this.cfs.aentriesAll(this.decorateSink_(sink, options));
+    //   }
+    // }
   ],
 
   actions: [
     {
       name: 'testWrite',
       action: function() {
-        this.cfs.write('test/test.txt', 'Hello world!\n');
+        this.cfs.awrite('test/test.txt', 'Hello world!\n')(function() {
+          console.log('Write', arguments);
+          this.cfs.clearError();
+        }.bind(this));
       }
     },
     {
       name: 'testRead',
       action: function() {
-        this.cfs.read('test/test.txt');
+        this.cfs.aread('test/test.txt')(function() {
+          console.log('Read', arguments);
+          this.cfs.clearError();
+        }.bind(this));
+      }
+    },
+    {
+      name: 'testEntries',
+      action: function() {
+        this.cfs.aentries(
+            '/test',
+            {
+              put: function(o) { console.log('Put', o); },
+              error: function(e) { console.log('Error', e); },
+              eof: function() { console.log('EOF'); }
+            })(function() {
+              console.log('Entries done', arguments);
+              this.cfs.clearError();
+            }.bind(this));
+      }
+    },
+    {
+      name: 'testEntriesAll',
+      action: function() {
+        this.cfs.aentriesAll(
+            '/',
+            {
+              put: function(o) { console.log('Put', o.path); },
+              error: function(e) { console.log('Error', e); },
+              eof: function() { console.log('EOF'); }
+            })(function() {
+              console.log('Entries all done', arguments);
+              this.cfs.clearError();
+            }.bind(this));
       }
     }
   ]
