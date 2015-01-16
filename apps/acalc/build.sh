f52d20f86c1eb38e70cfd2575585c8744736bf5b
Adam Van Ymeren <adamvy@google.com>
2015-01-16 16:43:27 -0500

    ACalc: Make build script run from anywhere.

--- apps/acalc/build.sh
+++ apps/acalc/build.sh
@@ -1,3 +1,6 @@
+BASEDIR=$(dirname "$0")
+cd "$BASEDIR"
+
 export BUILD_DIR=~/Downloads/acalc
 rm -rf $BUILD_DIR
 cp -r . $BUILD_DIR
@@ -66,10 +69,7 @@ cat \
   > "$BUILD_DIR/foam.js"
 
 # Delete unneeded files
-rm "$BUILD_DIR/Tests.js" "$BUILD_DIR/Tests.html" "$BUILD_DIR/build.sh" "$BUILD_DIR/package.sh"
-
-# Delete emacs temporary files
-rm "$BUILD_DIR/*~"
+rm "$BUILD_DIR/Tests.js" "$BUILD_DIR/Tests.html" "$BUILD_DIR/build.sh" "$BUILD_DIR/package.sh" "$BUILD_DIR/*~"
 
 # For code compression, uncomment the following line:
 # ~/node_modules/uglify-js/bin/uglifyjs --overwrite "$BUILD_DIR/foam.js"
