8f4a806c06e4d45e379c2c23fd63eb2581f8a247
kgrgreer <kgrgreer@gmail.com>
2015-01-21 16:53:58 -0500

    ACalc: decreased opacity of display shadow.

--- apps/acalc/Calc.js
+++ apps/acalc/Calc.js
@@ -262,7 +262,7 @@ CLASS({
 
     .edge2 {
       margin-top: -12px;
-      background: linear-gradient(to bottom, rgba(0,0,0,0.5) 0%,
+      background: linear-gradient(to bottom, rgba(0,0,0,0.25) 0%,
                                              rgba(0,0,0,0) 100%);
       top: 12px;
       height: 12px;
