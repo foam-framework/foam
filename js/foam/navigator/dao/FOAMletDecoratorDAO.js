c7b42dbbcdc9b4c075a149c48e43a6052af3a822
Jackson Stone <jacksonic@google.com>
2015-01-22 15:21:00 -0500

    Navigator: changed FOAMletDecoratorDAO to use AbstractAdapterDAO

--- js/foam/navigator/dao/FOAMletDecoratorDAO.js
+++ js/foam/navigator/dao/FOAMletDecoratorDAO.js
@@ -18,109 +18,35 @@
 CLASS({
   name: 'FOAMletDecoratorDAO',
   package: 'foam.navigator.dao',
-  extendsModel: 'ProxyDAO',
+  extendsModel: 'AbstractAdapterDAO',
 
   requires: [
     'foam.navigator.WrappedFOAMlet'
   ],
 
-
   properties: [
     {
-      name: 'FOAMletModel',
-      model_: 'ModelProperty'
-    },
-    {
-      name: 'put_',
-      model_: 'FunctionProperty',
-      defaultValue: function(sink, obj) {
-        var decoratedObj = this.FOAMletModel.create({ data: obj });
-        sink && sink.put && sink.put(decoratedObj);
-      }
-    },
-    {
-      name: 'error_',
-      defaultValue: function(sink) {
-        var args = argsArray(arguments).slice(1);
-        sink.error.apply(sink, args);
-      }
-    },
-    {
-      name: 'remove_',
-      defaultValue: function(model, sink, id) {
-        sink.remove(id);
-      }
-    },
-    {
-      name: 'relay_'
+      name: 'foamletModel',
+      model_: 'ModelProperty',
+      documentation: function() {/* If a valid WrappedFOAMlet is not specified
+        here, model-for-model will attempt to load the closest it can find.
+      */},  
     }
-  ],
-
-  methods: [
-    {
-      name: 'relay',
-      code: function(sink, id) {
-        if ( ! this.relay_ ) {
-          this.relay_ = this.newRelay(sink, id);
-        }
-
-        return this.relay_;
+  ],   
+  
+  methods: {
+    aToB: function(obj) { 
+      if ( this.foamletModel ) {
+        return this.foamletModel.create({ data: obj });
+      } else {
+        // try model-for-model if none was specified
+        return this.WrappedFOAMlet.create({ model: obj.model_, data: obj });
       }
     },
-    {
-      name: 'newRelay',
-      code: function(sink, id) {
-        return {
-          put: function(o) {
-            sink && sink.put && this.put_.bind(this, sink)(o);
-          }.bind(this),
-          error: sink && sink.error && this.error_.bind(this, sink),
-          remove: model && sink && sink.remove &&
-              this.remove_.bind(this, model, sink)
-        };
-      }
-    },
-    {
-      name: 'put',
-      code: function(obj, opt_sink) {       
-        this.delegate.put(obj.data, this.newRelay(opt_sink));
-      }
-    },
-    {
-      name: 'remove',
-      code: function(id, opt_sink) {
-        this.delegate.remove(id, this.newRelay(opt_sink, id));
-      }
-    },
-    {
-      name: 'find',
-      code: function(id, sink) {
-        this.delegate.find(id, this.newRelay(sink, id));
-      }
-    },
-    {
-      name: 'listen',
-      code: function(sink, options) {
-        if ( ! this.daoListeners_.length && this.delegate ) {
-          this.delegate.listen(this.relay(sink));
-        }
-        this.SUPER(sink, options);
-      }
-    },
-    {
-      name: 'unlisten',
-      code: function(sink) {
-        this.SUPER(sink);
-        if ( ! this.daoListeners_.length && this.delegate ) {
-          this.delegate.unlisten(this.relay());
-        }
-      }
-    },
-    {
-      name: 'select',
-      code: function(sink, options) {
-        return this.delegate.select(this.newRelay(sink), options);
-      }
+    bToA: function(obj) {
+      return obj.data;
     }
-  ]
+    adaptOptions_: function(opts) { return opts; }
+  }
+
 });
