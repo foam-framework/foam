b5b5a3466b1ff9f7690243449f9cdeb422d9ab4c
Jackson Stone <jacksonic@google.com>
2015-01-26 10:20:28 -0500

    ModelFileDAO: Changed find() to not duplicate script tags. Sinks will accumulate until the first load completes, then further requests go straight to FOAM.lookup

--- js/foam/core/bootstrap/ModelFileDAO.js
+++ js/foam/core/bootstrap/ModelFileDAO.js
@@ -18,24 +18,67 @@
 MODEL({
   name: 'ModelFileDAO',
   package: 'foam.core.bootstrap',
-  methods: {
+  methods: {  
+    init: function() {
+      this.SUPER();
+      if ( ! this.X.modelFileDAO$RegisteredModels ) {
+        this.X.set('modelFileDAO$RegisteredModels', {} );
+      }
+    },
+    
     find: function (key, sink) {
+      /* The find function will accumulate duplicate load attempts, chaining
+      the given sinks together until the first onload completes. Subsequent
+      load attempts will immediately put the item to the sink. */
       var X = this.X;
       var model = FOAM.lookup(key, X);
-
-      var tag = this.X.document.createElement('script');
-      tag.src = FOAM_BOOT_DIR + '../js/' + key.replace(/\./g, '/') + '.js';
-      this.X.document.head.appendChild(tag);
+      if ( model ) {
+        sink && sink.put && sink.put(model);
+        return;
+      }
+      
+      // If we've already seen this key, don't load it again
+      if ( this.X.modelFileDAO$RegisteredModels[key] ) {
+        var oldSink = this.X.modelFileDAO$RegisteredModels[key];
+        // decorate the previous sink to chain it with the new one
+        X.modelFileDAO$RegisteredModels[key] = {
+          put: function(item) { 
+            oldSink && oldSink.put && oldSink.put(item);
+            sink && sink.put && sink.put(item);
+          },
+          error: function(item) { 
+            oldSink && oldSink.error && oldSink.error(item);
+            sink && sink.error && sink.error(item);
+          }
+        }
+        return;
+      }
+      
+      // It's a new key, so remember the sink and make a tag
+      X.modelFileDAO$RegisteredModels[key] = sink;
+      
+      //var model = FOAM.lookup(key, X);
+      var sourcePath = FOAM_BOOT_DIR + '../js/' + key.replace(/\./g, '/') + '.js';
+      
+      var tag = X.document.createElement('script');
+      tag.src = sourcePath;
+      X.document.head.appendChild(tag);
 
       tag.onload = function() {
-//        this.removeChild(tag);
-
-        model = FOAM.lookup(key, X);
+        var itemSink = X.modelFileDAO$RegisteredModels[key];
+        var model = FOAM.lookup(key, X);
         if ( ! model ) {
-          sink && sink.error && sink.error('Model load failed for: ', key);
+          console.warn('Model load failed for: ', key);
+          itemSink && itemSink.error && itemSink.error('Model load failed for: ', key);
           return;
         }
-        sink && sink.put && sink.put(model);
+        itemSink && itemSink.put && itemSink.put(model);
+        // reset the sink we remembered, but don't undefine as we want to remember
+        // the load is complete
+        X.modelFileDAO$RegisteredModels[key] = { 
+            put: function() { console.warn("Attempted put into already finished onload: ", key); },
+            error: function() { console.warn("Attempted error into already finished onload: ", key); } 
+          };
       }.bind(this.X.document.head);
     }
   }
