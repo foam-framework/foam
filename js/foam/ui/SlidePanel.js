1a98458b420a414a42a1086b5c1fa83c1b24350d
kgrgreer <kgrgreer@gmail.com>
2015-02-05 11:21:03 -0500

    Moved SlidePanel styling to CSS.

--- js/foam/ui/SlidePanel.js
+++ js/foam/ui/SlidePanel.js
@@ -136,6 +136,10 @@ CLASS({
 
   templates: [
     function CSS() {/*
+      .SlidePanel {
+        display: flex;
+        position: relative;
+      }
       .SlidePanel .shadow {
         background: linear-gradient(to left, rgba(0,0,0,0.15) 0%,
                                              rgba(0,0,0,0) 100%);
@@ -143,15 +147,22 @@ CLASS({
         left: -8px;
         position: absolute;
         width: 8px;
-        }
+      }
+      .SlidePanel .main {
+      }
+      .SlidePanel .panel {
+        position: absolute;
+        top: 0;
+        left: 0;
+      }
     */},
     function toHTML() {/*
-      <div id="%%id" style="display: inline-block;position: relative;" class="SlidePanel">
-        <div id="%%id-main">
+      <div id="%%id" class="SlidePanel">
+        <div id="%%id-main" class="main">
           <div style="width:0;position:absolute;"></div>
           <%= this.mainView() %>
         </div>
-        <div id="%%id-panel" style="position: absolute; top: 0; left: 0">
+        <div id="%%id-panel" class="panel">
           <div id="%%id-shadow" class="shadow"></div>
           <%= this.panelView() %>
         </div>
@@ -214,14 +225,18 @@ CLASS({
       code: function(e) {
         if ( ! this.$ ) return;
         if ( this.parentWidth >= this.minWidth + this.minPanelWidth ) {
+          // if ( this.expanded ) return;
           this.shadow$().style.display = 'none';
           // Expaded mode. Show the two side by side, setting their widths
           // based on the panelRatio.
           this.panelWidth = Math.max(this.panelRatio * this.parentWidth, this.minPanelWidth);
+
+
           this.width = this.parentWidth - this.panelWidth;
           this.panelX = this.width;
           this.expanded = true;
         } else {
+          // if ( ! this.expanded ) return;
           this.shadow$().style.display = 'inline';
           this.width = Math.max(this.parentWidth - this.stripWidth, this.minWidth);
           this.panelWidth = this.minPanelWidth;
