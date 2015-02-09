b98444090fad3321cf7f44031f00ccf44de700a6
Mark Dittmer <markdittmer@google.com>
2015-02-09 16:13:17 -0500

    PCalc: gh-pages build exploits build.sh logic

--- apps/pcalc/gh-build.sh
+++ apps/pcalc/gh-build.sh
@@ -4,28 +4,28 @@ cd "$BASEDIR"
 # Assuming we're in the foam submodule of the gh-pages branch:
 
 # Get out of foam submodule
-pushd ../../../
+pushd ../../../ > /dev/null
 
 ON_GH_PAGES_BRANCH=`git branch | grep '^[*] gh-pages$'`
 
 # Come back to the intended working directory
-popd
+popd > /dev/null
 
 if [ "$ON_GH_PAGES_BRANCH" == "" ]; then
     echo "ERROR: Only for use in gh-pages branch"
     exit 1
 fi
 
-export BUILD_DIR=../../../pcalc
-export FILES=(
-  "GHCalc.html"
-  "Calc.js"
-  "fonts"
-  "CalcConfig.js"
-)
+# build.sh export BUILD_DIR
+BUILD_OUTPUT=`source ./build.sh`
 
-mkdir -p $BUILD_DIR
-rm -rf $BUILD_DIR/*
-for FILE in ${FILES[@]}; do
-  cp -r $FILE $BUILD_DIR/
-done
+if [ "$BUILD_OUTPUT" != "" ]; then
+    echo "ERROR: Expected no output from build.sh (actual output below)"
+    echo "$BUILD_OUTPUT"
+    exit 1
+fi
+
+export PCALC_GH_DIR=../../../pcalc
+
+rm -rf "$PCALC_GH_DIR/*"
+cp -r "$BUILD_DIR/*" "$PCALC_GH_DIR/"
