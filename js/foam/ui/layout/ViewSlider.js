662cbd3cd2054d0a5212760191309416590324f3
Mark Dittmer <markdittmer@google.com>
2015-02-25 11:35:10 -0500

    ViewSlider: Guard access to this.$

--- js/foam/ui/layout/ViewSlider.js
+++ js/foam/ui/layout/ViewSlider.js
@@ -104,11 +104,11 @@ CLASS({
       "code": function (view) {
         if ( this.view ) {
           this.view.destroy();
-          this.$.removeChild(this.view.$);
+          if ( this.$ ) this.$.removeChild(this.view.$);
         }
         this.view = view;
         this.layout();
-        this.$.insertAdjacentHTML('beforeend', view.toHTML());
+        if ( this.$ ) this.$.insertAdjacentHTML('beforeend', view.toHTML());
         view.initHTML();
       },
       "args": []
