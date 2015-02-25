5471f909b30b288bc5eb3799e5133ee84405d57b
Adam Van Ymeren <adamvy@google.com>
2015-02-25 11:59:42 -0500

    Add i18n models back to the calculator build script as they're not being pulled in by default.

--- apps/acalc/build.sh
+++ apps/acalc/build.sh
@@ -13,11 +13,12 @@ cp -r icons "$BUILD_DIR"
 cp bg.js "$BUILD_DIR"
 
 cd ../../
-node --harmony tools/foam.js foam.build.BuildApp controller=Calc defaultView=foam.apps.calc.CalcView coreFiles=stdlib,async,parse,event,JSONUtil,XMLUtil,context,JSONParser,TemplateUtil,../apps/acalc/Opt,FOAM,FObject,BootstrapModel,mm1Model,mm2Property,mm3Types,mm4Method,mm5Misc,value,view,AbstractDAOView,DAOListView,DetailView,HTMLParser,visitor,dao,arrayDAO,touch,../js/foam/ui/Window extraFiles=../apps/acalc/Calc "targetPath=$BUILD_DIR" precompileTemplates
+node --harmony tools/foam.js foam.build.BuildApp controller=Calc defaultView=foam.apps.calc.CalcView coreFiles=stdlib,async,parse,event,JSONUtil,XMLUtil,context,JSONParser,TemplateUtil,../apps/acalc/Opt,FOAM,FObject,BootstrapModel,mm1Model,mm2Property,mm3Types,mm4Method,mm5Misc,value,view,AbstractDAOView,DAOListView,DetailView,HTMLParser,visitor,dao,arrayDAO,touch,../js/foam/ui/Window,../js/foam/i18n/Visitor,../js/foam/i18n/MessagesBuilder,../js/foam/i18n/GlobalController,../js/foam/i18n/ChromeMessagesExtractor,../js/foam/i18n/ChromeMessagesBuilder extraFiles=../apps/acalc/Calc "targetPath=$BUILD_DIR" precompileTemplates
 
 
 cd "$BUILD_DIR"
 # Code compression.
 uglifyjs foam.js -c unused=false > foam-min.js
 mv foam-min.js foam.js
+
 mv main.html AppCalc.html
