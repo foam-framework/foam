98bea221773ccd1c4b0fdd6009106fbc801990cd
kgrgreer <kgrgreer@gmail.com>
2015-03-02 16:43:17 -0500

    Fixed Overflow app.

--- apps/overflow/Controller.js
+++ apps/overflow/Controller.js
@@ -1,7 +1,7 @@
 CLASS({
   name: 'Controller',
-  requires: ['foam.ui.DAOListView']
-  
+  requires: ['foam.ui.DAOListView'],
+
   properties: [
     { name: 'search', view: { factory_: 'TextFieldView', onKeyMode: true } },
     { name: 'dao', defaultValue: questions },
