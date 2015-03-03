80e723e3d0d4970476cc2fd823706ccc9f940453
Adam Van Ymeren <adamvy@google.com>
2015-03-03 10:58:43 -0500

    Package quickbug Cursor and CursorView

--- js/foam/apps/quickbug/model/Cursor.js
+++ js/foam/apps/quickbug/model/Cursor.js
@@ -1,11 +1,29 @@
+/**
+ * @license
+ * Copyright 2015 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 CLASS({
   name: 'Cursor',
+  package: 'foam.apps.quickbug.model',
+
+  imports: [
+    'browser'
+  ],
 
   properties: [
-    {
-      name: 'browser',
-      defaultValueFn: function() { return this.X.browser; }
-    },
     {
       name: 'location',
       defaultValueFn: function() { return this.browser.location; }
@@ -79,24 +97,4 @@ CLASS({
         });
     }
   }
-});
-
-
-CLASS({
-  name: 'CursorView',
-
-  extendsModel: 'foam.ui.DetailView',
-
-  methods: {
-    initHTML: function() {
-      this.SUPER();
-      this.$.focus();
-    }
-  },
-
-  templates: [
-    function toHTML() {/*
-      <span id="%%id" tabindex=1>$$prev{model_: 'ActionLink'} $$pos{mode: 'read-only'} of $$total{mode: 'read-only'} $$next{model_: 'ActionLink'} $$backToList{model_: 'ActionLink'}</span>
-    */}
-  ]
 });
