45b142cb166777101eb03279aaeb7f9b44c827ff
Adam Van Ymeren <adamvy@google.com>
2015-03-03 13:21:04 -0500

    Package QIssueTileView

--- js/foam/apps/quickbug/ui/QIssueTileView.js
+++ js/foam/apps/quickbug/ui/QIssueTileView.js
@@ -1,6 +1,28 @@
+/**
+ * @license
+ * Copyright 2015 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 CLASS({
   name: 'QIssueTileView',
+  package: 'foam.apps.quickbug.ui',
   label: 'QIssue Tile View',
+
+  requires: [
+    'PropertyView'
+  ],
 
   extendsModel: 'foam.ui.View',
 
@@ -9,8 +31,11 @@ CLASS({
       name:  'browser'
     },
     {
-      name:  'issue',
-      type:  'QIssue'
+      name:  'issue'
+    },
+    {
+      name: 'QIssue',
+      defaultValueFn: function() { return this.X.QIssue; }
     }
   ],
 
@@ -25,7 +50,7 @@ CLASS({
 
     // Implement Adapter
     f: function(issue) {
-      var view = this.X.QIssueTileView.create({ browser: this.browser }, self.Y);
+      var view = this.model_.create({ browser: this.browser }, this.Y);
       view.put(issue);
       return view;
     },
