ebcf2960acd410bcc09032d53d3a11285c181ed1
Adam Van Ymeren <adamvy@google.com>
2015-03-04 11:44:08 -0500

    Model the quickbug background controller.

---
+++ js/foam/apps/quickbug/controller/Background.js
@@ -0,0 +1,46 @@
+CLASS({
+  name: 'Background',
+  package: 'foam.apps.quickbug.controller',
+
+  requires: [
+    'foam.apps.quickbug.model.QBug'
+  ],
+
+  properties: [
+    {
+      name: 'qb',
+      factory: function() {
+        return this.QBug.create();
+      }
+    }
+  ],
+
+  methods: {
+    init: function() {
+      if ( chrome.app && chrome.app.runtime ) {
+        chrome.app.runtime.onLaunched.addListener(this.onLaunch);
+        chrome.runtime.onMessageExternal.addListener(this.onMessageExternal);
+      }
+    },
+  },
+  listeners: [
+    {
+      name: 'onLaunched',
+      code: function() {
+        this.qb.launchBrowser.apply(this.qb, arguments);
+      }
+    },
+    {
+      name: 'onMessageExternal',
+      code: function(msg) {
+        if ( msg && msg.type === 'openUrl' ) {
+          // Extract the project name and call launchBrowser.
+          var start = msg.url.indexOf('/p/') + 3;
+          var end = msg.url.indexOf('/', start);
+          var project = msg.url.substring(start, end);
+          this.onLaunched(project, msg.url);
+        }
+      }
+    }
+  }
+});
