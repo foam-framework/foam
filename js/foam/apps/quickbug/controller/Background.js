65f4a172077c5bb4d7bce9601cdb60b2675130f3
Adam Van Ymeren <adamvy@google.com>
2015-03-04 16:56:46 -0500

    Add license header, DAOVersionDAO, and syntax fixes to quickbug background controller.

--- js/foam/apps/quickbug/controller/Background.js
+++ js/foam/apps/quickbug/controller/Background.js
@@ -1,17 +1,41 @@
+/**
+ * @license
+ * Copyright 2015 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 CLASS({
   name: 'Background',
   package: 'foam.apps.quickbug.controller',
 
   requires: [
     'foam.apps.quickbug.model.QBug',
-    'NullDAO'
+    'NullDAO',
+    'IDBDAO',
+    'DAOVersion'
   ],
 
   exports: [
-    'metricDAO'
+    'metricDAO',
+    'daoVersionDao'
   ],
 
   properties: [
+    {
+      name: 'daoVersionDao',
+      lazyFactory: function() { return this.IDBDAO.create({ model: this.DAOVersion }); }
+    },
     {
       name: 'metricDAO',
       factory: function() {
@@ -27,7 +51,8 @@ CLASS({
   ],
 
   methods: {
-    init: function() {
+    init: function(args) {
+      this.SUPER(args)
       if ( chrome.app && chrome.app.runtime ) {
         chrome.app.runtime.onLaunched.addListener(this.onLaunch);
         chrome.runtime.onMessageExternal.addListener(this.onMessageExternal);
@@ -53,5 +78,5 @@ CLASS({
         }
       }
     }
-  }
+  ]
 });
