7d46f76deff5dbe472653b254ce97a0a1be2f4ae
Adam Van Ymeren <adamvy@google.com>
2015-03-08 21:23:44 -0400

    Root index.js improvements.

--- index.js
+++ index.js
@@ -24,6 +24,8 @@
     delete params.view;
   }
 
+  models.push(arequire('foam.ui.View'));
+
   var showActions = params.showActions;
   if ( showActions ) {
     showActions = showActions.equalsIC('y')    ||
@@ -43,8 +45,8 @@
         view = FOAM.lookup(viewName, X).create({ data: obj });
         // 'CView' refers to old CView
         // TODO(kgr): remove this check when CView's converted to foam.graphics.CView
-      } else if (  ( FOAM.lookup('foam.ui.View', X) && X.foam.ui.View.isInstance(obj) )
-                || ( 'CView' in GLOBAL && CView.isInstance(obj) ) ) { 
+      } else if (  ( FOAM.lookup('foam.ui.View', X).isInstance(obj) )
+                   || ( 'CView' in GLOBAL && CView.isInstance(obj) ) ) {
         view = obj;
       } else if ( obj.toView_ ) {
         view = obj.toView_();
