d98bd6a8592fde5b41f9699cf1d680e08bdf7764
Adam Van Ymeren <adamvy@google.com>
2015-03-12 11:51:47 -0400

    Delete core/touch.js as it was moved to packages and removed from FOAMmodels.js a while ago.

--- apps/acalc/build.sh
+++ apps/acalc/build.sh
@@ -13,7 +13,7 @@ cp -r icons "$BUILD_DIR"
 cp bg.js "$BUILD_DIR"
 
 cd ../../
-node --harmony tools/foam.js foam.build.BuildApp controller=Calc defaultView=foam.apps.calc.CalcView coreFiles=stdlib,async,parse,event,JSONUtil,XMLUtil,context,JSONParser,TemplateUtil,../apps/acalc/Opt,FOAM,FObject,BootstrapModel,mm1Model,mm2Property,mm3Types,mm4Method,mm5Misc,value,view,AbstractDAOView,DAOListView,DetailView,HTMLParser,visitor,dao,arrayDAO,touch,../js/foam/ui/Window,../js/foam/i18n/Visitor,../js/foam/i18n/MessagesExtractor,../js/foam/i18n/ChromeMessagesExtractor,../js/foam/i18n/ChromeMessagesInjector,../js/foam/i18n/GlobalController extraFiles=../apps/acalc/Calc "targetPath=$BUILD_DIR" precompileTemplates
+node --harmony tools/foam.js foam.build.BuildApp controller=Calc defaultView=foam.apps.calc.CalcView coreFiles=stdlib,async,parse,event,JSONUtil,XMLUtil,context,JSONParser,TemplateUtil,../apps/acalc/Opt,FOAM,FObject,BootstrapModel,mm1Model,mm2Property,mm3Types,mm4Method,mm5Misc,value,view,AbstractDAOView,DAOListView,DetailView,HTMLParser,visitor,dao,arrayDAO,../js/foam/ui/Window,../js/foam/i18n/Visitor,../js/foam/i18n/MessagesExtractor,../js/foam/i18n/ChromeMessagesExtractor,../js/foam/i18n/ChromeMessagesInjector,../js/foam/i18n/GlobalController extraFiles=../apps/acalc/Calc "targetPath=$BUILD_DIR" precompileTemplates
 
 
 cd "$BUILD_DIR"
