e6b7e5b8ffd8e5a225f44ca88f417d06f086a7ce
kgrgreer <kgrgreer@gmail.com>
2015-03-14 09:58:48 -0400

    ACalc: Fixed DetailView reference in build.sh.

--- apps/acalc/build.sh
+++ apps/acalc/build.sh
@@ -3,6 +3,8 @@ cd "$BASEDIR"
 
 #   Templates.js \
 
+pushd .
+
 export BUILD_DIR=~/Downloads/acalc
 rm -rf $BUILD_DIR
 mkdir $BUILD_DIR
@@ -13,12 +15,12 @@ cp -r icons "$BUILD_DIR"
 cp bg.js "$BUILD_DIR"
 
 cd ../../
-node --harmony tools/foam.js foam.build.BuildApp controller=Calc defaultView=foam.apps.calc.CalcView coreFiles=stdlib,async,parse,event,JSONUtil,XMLUtil,context,JSONParser,TemplateUtil,../apps/acalc/Opt,FOAM,FObject,BootstrapModel,mm1Model,mm2Property,mm3Types,mm4Method,mm5Misc,value,view,AbstractDAOView,DAOListView,DetailView,HTMLParser,visitor,dao,arrayDAO,../js/foam/ui/Window,../js/foam/i18n/Visitor,../js/foam/i18n/MessagesExtractor,../js/foam/i18n/ChromeMessagesExtractor,../js/foam/i18n/ChromeMessagesInjector,../js/foam/i18n/GlobalController extraFiles=../apps/acalc/Calc "targetPath=$BUILD_DIR" precompileTemplates
-
+node --harmony tools/foam.js foam.build.BuildApp controller=Calc defaultView=foam.apps.calc.CalcView coreFiles=stdlib,async,parse,event,JSONUtil,XMLUtil,context,JSONParser,TemplateUtil,../apps/acalc/Opt,FOAM,FObject,BootstrapModel,mm1Model,mm2Property,mm3Types,mm4Method,mm5Misc,value,view,AbstractDAOView,DAOListView,../js/foam/ui/DetailView,HTMLParser,visitor,dao,arrayDAO,../js/foam/ui/Window,../js/foam/i18n/Visitor,../js/foam/i18n/MessagesExtractor,../js/foam/i18n/ChromeMessagesExtractor,../js/foam/i18n/ChromeMessagesInjector,../js/foam/i18n/GlobalController extraFiles=../apps/acalc/Calc "targetPath=$BUILD_DIR" precompileTemplates
 
 cd "$BUILD_DIR"
 # Code compression.
-# uglifyjs foam.js -c unused=false > foam-min.js
-# mv foam-min.js foam.js
+uglifyjs foam.js -c unused=false > foam-min.js
+mv foam-min.js foam.js
 
 mv main.html AppCalc.html
+popd
