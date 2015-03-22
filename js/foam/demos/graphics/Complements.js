3308843bed9694314a50b0b2a260f3b11a4fc1e8
Kevin Greer <kgr@chromium.org>
2015-03-22 11:12:43 -0400

    Initial check-in of Modelled Complements demo.

---
+++ js/foam/demos/graphics/Complements.js
@@ -0,0 +1,68 @@
+/**
+ * @license
+ * Copyright 2015 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+CLASS({
+  package: 'foam.demos.graphics',
+  name:  'Complements',
+
+  extendsModel: 'foam.graphics.CView',
+
+  requires: [
+    'Timer',
+    'foam.graphics.Circle'
+  ],
+
+  imports: [ 'timer' ],
+
+  properties: [
+    {
+      name: 'timer',
+      factory: function() { return this.Timer.create(); }
+    },
+    {
+      name:  'r',
+      defaultValue: 140,
+      postSet: function(_, r) { this.width = this.height = 2*r + 100; }
+    }
+  ],
+
+  methods: {
+    initCView: function() {
+      var self = this;
+      var R = this.r;
+      var D = R * 2;
+      var timer = this.timer;
+
+      for ( var a = 0 ; a < 2*Math.PI ; a += 2*Math.PI/13 ) {
+        console.log('a: ', a);
+        var circle = this.Circle.create({
+          x:     D/2 + R * Math.sin(a),
+          y:     D/2 + R * Math.cos(a),
+          color: 'hsl(' + 180*a/Math.PI + ', 90%, 60%)'
+        });
+        
+        timer.time$.addListener(function() { if ( self.$ ) self.paint(); });
+        Events.dynamic((function (circle, a) {
+          return function() {         if ( timer.time % 10 == 0 ) console.log('time: ', timer.time);
+circle.r = 10 + 30 * Math.abs(Math.sin(a + timer.time/3000)); };
+        })(circle, a));
+        this.addChild(circle);
+      }
+      timer.start();
+    }
+  }
+});
