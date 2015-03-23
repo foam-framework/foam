f78a2dcc5919fae313ec5b304b785391cd67e8c0
Jackson Stone <jacksonic@google.com>
2015-03-23 15:53:20 -0400

    DocRef links using masterModelList DAO instead of lookups

--- js/foam/documentation/DocRef.js
+++ js/foam/documentation/DocRef.js
@@ -21,7 +21,7 @@ CLASS({
   label: 'Documentation Reference',
   documentation: 'A reference to a documented Model or feature of a Model',
 
-  imports: ['documentViewRef'],
+  imports: ['documentViewRef', 'masterModelList'],
 
   documentation: function() { /*
     <p>A link to another place in the documentation. See $$DOC{ref:'DocView'}
@@ -106,23 +106,6 @@ CLASS({
      while the second name is an instance of a feature on that Model, and subsequent
      names are sub-objects on those instances.</p>
   */
-      var errorHandler = function() { 
-        if ( abortOnFail) {
-          return;
-        } else {
-          this.X.ModelDAO.find(reference, { 
-            put: function(m) {
-              if ( m ) {
-                m.arequire && m.arequire();
-                this.resolveReference(m.id, true);
-              }
-            }.bind(this)
-          });
-          return;
-        }
-      }.bind(this);
-  
-  
       this.valid = false;
 
       if (!reference) return;
@@ -136,15 +119,141 @@ CLASS({
       if (args[0].length <= 0) {
         if (!this.documentViewRef || !this.documentViewRef.get().resolvedRoot.valid) {
           return; // abort
+        } else {
+          //  fill in root to make reference absolute, and try again
+          return this.resolveReference(this.documentViewRef.get().resolvedRoot.resolvedRef + reference);
         }
-
-        // fill in root to make reference absolute, and try again
-        return this.resolveReference(this.documentViewRef.get().resolvedRoot.resolvedRef + reference);
-
-      } else {
-        // resolve path and model
-        model = this.X[args[0]];
       }
+      
+      var refChunk = ""+reference;
+      while (refChunk.length > 0) {
+//         this.X.ModelDAO.find(refChunk, { // arequire instead
+//             put: function(m) {
+//               this.resolveFeature(m, reference);
+//             }.bind(this)
+//         });
+        this.masterModelList.where(EQ(Model.ID, refChunk)).select({ 
+            put: function(m) {
+              this.resolveFeature(m, reference);
+            }.bind(this)
+        });
+
+        var slice = refChunk.lastIndexOf('.');
+        if (slice == -1) {
+          break;
+        } else {
+          refChunk = refChunk.substring(0, refChunk.lastIndexOf('.'));
+        }
+      }
+      
+    },
+    
+    getInheritanceList: function(model, list) {
+      // find all base models of the given model, put into list
+      this.X.masterModelList.where(IN(Model.ID, model.traits)).select(list);
+
+      if ( model.extendsModel ) {
+        this.X.masterModelList.where(EQ(Model.ID, model.extendsModel)).select({
+            put: function(ext) {
+              list.put(ext);
+              this.getInheritanceList(ext, list);
+            }.bind(this)
+        }); 
+      } else {
+        list.eof(); // no more extendsModels to follow, finished
+      }
+    },
+    
+    resolveFeature: function(m, reference) {
+      var model = m;
+      var newResolvedRef = m.id;
+      var featureName = reference.replace(model.id, "");
+      console.log("Feature: ", featureName, " for ref ", reference, " and model ", model.id);
+   
+      var features = featureName.split('.');
+      
+      // check for inner models
+      while (features.length > 0 && model) {
+        if (model[features[0]] && 
+            model[features[0]].model_ && 
+            model[features[0]].model_.id == 'Model') {
+          model = model[features[0]];
+          newResolvedRef += '.' + features.shift();  
+        } else {
+          break;
+        }
+      }
+
+      // inner models (if present) have been accounted for, so we have our root model
+      this.resolvedRoot = this.model_.create({
+        resolvedModelChain: [ model ],
+        resolvedRef: newResolvedRef,
+        valid: true,
+        resolvedRoot: undefined // otherwise it would be the same as 'this'
+      });
+      
+      var ancestry = [model];
+      this.getInheritanceList(model, {
+        put: function(m) { ancestry.put(m); },
+        eof: function() {
+          // Check for a feature, and check inherited features too
+          // If we have a Model definition, we make the jump from definition to an
+          // instance of a feature definition here
+          var foundObject = null;
+          if (features.length > 0) {
+            // feature specified "Model.feature" or ".feature"
+            ancestry.every(function(ancestor) { 
+              foundObject = ancestor.getFeature(features[0]);
+              if ( ! foundObject ) {    
+                return false;
+              } else {
+                newResolvedRef += "." + features.shift();
+                return true;
+              }
+            });
+          }
+    
+          // allow further specification of sub properties or lists
+          if ( foundObject && features.length > 0 ) {
+            if ( ! features.every(function (arg) {
+                var newObject;
+    
+                // null arg is an error at this point
+                if ( arg.length <= 0 ) return false;
+    
+                // check if arg is the name of a sub-object of foundObject
+                var argCaller = Function('return this.'+arg);
+                if (argCaller.call(foundObject)) {
+                  newObject = argCaller.call(foundObject);
+    
+                // otherwise if foundObject is a list, check names of each thing inside
+                } else if (foundObject.mapFind) {
+                  foundObject.mapFind(function(f) {
+                    if (f && f.name && f.name === arg) {
+                      newObject = f;
+                    }
+                  })
+                }
+                foundObject = newObject; // will reset to undefined if we failed to resolve the latest part
+                if ( ! foundObject ) {
+                  return false;
+                } else {
+                  newResolvedRef += "." + arg;
+                  return true;
+                }
+              })) {
+              return; // the loop failed to resolve something
+            }
+          }
+          
+          this.resolvedRef = newResolvedRef;
+          this.valid = true;
+        }.bind(this)
+      });
+    },      
+/*      
+      // resolve path and model
+      model = this.X[args[0]];
 
       this.resolvedModelChain = [];
       this.resolvedRef = "";
@@ -245,7 +354,8 @@ CLASS({
           resolvedRoot: undefined // otherwise it would be the same as 'this'
       });
       this.valid = true;
-    },
+*/
+      
   },
 
 });
