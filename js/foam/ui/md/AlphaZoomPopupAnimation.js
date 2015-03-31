6a6870597bc052e37d333fe55889da1cc9f7bb64
Mark Dittmer <markdittmer@google.com>
2015-03-31 16:18:19 -0400

    Cutomizable animations for MD Popup: alpha+zoom and slide-in "positioned"

---
+++ js/foam/ui/md/AlphaZoomPopupAnimation.js
@@ -0,0 +1,79 @@
+/**
+ * @license
+ * Copyright 2015 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+CLASS({
+  name: 'AlphaZoomPopupAnimation',
+  package: 'foam.ui.md',
+
+  properties: [
+    {
+      name: 'popup',
+      required: true
+    },
+    {
+      name: 'easing',
+      defaultValue: Movement.easeOut(0.9)
+    },
+    {
+      model_: 'FloatProperty',
+      name: 'openDuration',
+      defaultValue: 250
+    },
+    {
+      model_: 'FloatProperty',
+      name: 'closeDuration',
+      defaultValue: 250
+    },
+    {
+      name: 'openAnimation',
+      lazyFactory: function() {
+        return Movement.animate(
+            this.openDuration,
+            function() {
+              var popup = this.popup;
+              popup.alpha = 1;
+              if ( popup.overlayView )
+                popup.overlayView.alpha =
+                    popup.overlayView.openAlpha;
+              popup.zoom = 1;
+            }.bind(this),
+            this.easing,
+            function() { this.popup.className = 'open'; }.bind(this));
+      }
+    },
+    {
+      name: 'closeAnimation',
+      lazyFactory: function() {
+        return Movement.animate(
+            this.closeDuration,
+            function() {
+              var popup = this.popup;
+              popup.alpha = 0;
+              if ( popup.overlayView )
+                popup.overlayView.alpha = 0;
+              popup.zoom = 0.1;
+            }.bind(this),
+            this.easing,
+            function() { this.popup.className = 'closed'; }.bind(this));
+      }
+    }
+  ],
+
+  methods: [
+    {
+      name: 'initHTML',
+      code: function() {
+        this.popup.alpha = 0;
+        this.popup.zoom = 0.1;
+      }
+    }
+  ]
+});
--- js/foam/ui/md/PopupView.js
+++ js/foam/ui/md/PopupView.js
@@ -14,7 +14,10 @@ CLASS({
   package: 'foam.ui.md',
   extendsModel: 'foam.flow.Element',
 
-  requires: [ 'foam.ui.md.ModalOverlayView' ],
+  requires: [
+    'foam.ui.md.ModalOverlayView',
+    'foam.ui.md.PositionedPopupAnimation as Animation'
+  ],
   exports: [ 'as popup' ],
 
   constants: { ELEMENT_NAME: 'popup' },
@@ -26,9 +29,48 @@ CLASS({
       defaultValue: 'closed'
     },
     {
+      model_: 'StringProperty',
+      name: 'cssPosition',
+      defaultValue: 'fixed',
+      postSet: function(old, nu) {
+        if ( ! this.$ ) return;
+        this.$.style.position = this.cssPosition;
+      }
+    },
+    {
+      model_: 'StringProperty',
+      name: 'cssInnerPosition',
+      defaultValue: 'fixed',
+      postSet: function(old, nu) {
+        if ( ! this.innerView ) return;
+        this.innerView.style.position = this.cssInnerPosition;
+      }
+    },
+    {
+      name: 'left',
+      defaultValue: '',
+      postSet: function(old, nu) {
+        if ( ! this.innerView || old === nu ) return;
+        this.innerView.style.left = this.left ? this.left + 'px' : '';
+      }
+    },
+    {
+      name: 'top',
+      defaultValue: '',
+      postSet: function(old, nu) {
+        if ( ! this.innerView || old === nu ) return;
+        this.innerView.style.top = this.top ? this.top + 'px' : '';
+      }
+    },
+    {
       model_: 'FloatProperty',
       name: 'alpha',
-      defaultValue: 0
+      defaultValue: 1
+    },
+    {
+      model_: 'FloatProperty',
+      name: 'zoom',
+      defaultValue: 1
     },
     {
       model_: 'ViewFactoryProperty',
@@ -56,6 +98,9 @@ CLASS({
       name: 'containerView'
     },
     {
+      name: 'innerView'
+    },
+    {
       model_: 'FloatProperty',
       name: 'openDuration',
       defaultValue: 300
@@ -70,23 +115,9 @@ CLASS({
       defaultValue: Movement.easeOut(0.9)
     },
     {
-      name: 'openAnimation',
-      factory: function() {
-        return Movement.animate(
-            this.openDuration,
-            function() { this.alpha = 1; }.bind(this),
-            this.animationEase,
-            function() { this.className = 'open'; }.bind(this));
-      }
-    },
-    {
-      name: 'closeAnimation',
-      factory: function() {
-        return Movement.animate(
-            this.closeDuration,
-            function() { this.alpha = 0; }.bind(this),
-            this.animationEase,
-            function() { this.className = 'closed'; }.bind(this));
+      name: 'animation',
+      lazyFactory: function() {
+        return this.Animation.create({ popup: this });
       }
     }
   ],
@@ -95,7 +126,7 @@ CLASS({
     {
       name: 'construct',
       code: function() {
-        this.overlayView = this.overlay();
+        this.overlayView = this.overlay({ cssPosition: 'inerhit' });
         this.delegateView = this.delegate();
         this.addChild(this.overlayView);
         this.addDataChild(this.delegateView);
@@ -107,28 +138,91 @@ CLASS({
       code: function() {
         this.SUPER.apply(this, arguments);
         this.containerView = this.X.$(this.id + '-container');
+        this.innerView = this.X.$(this.id + '-view');
+
+        this.$.style.position = this.cssPosition;
 
         Events.dynamic(function() {
-          this.alpha;
-          if ( this.overlayView )
-            this.overlayView.alpha = this.overlayView.openAlpha * this.alpha;
-          if ( this.containerView )
-            this.containerView.style.opacity = this.alpha;
+          this.alpha; this.zoom;
+          if ( this.containerView ) {
+            var cvStyle = this.containerView.style;
+            cvStyle.opacity = this.alpha;
+            cvStyle.zoom = this.zoom;
+          }
         }.bind(this));
+
+        this.animation && this.animation.initHTML && this.animation.initHTML();
+
+        // TODO(markdittmer): Testing only.
+        var self = this;
+        var toggle = (function() {
+          var isOpen = false;
+          return function() {
+            if ( isOpen ) self.close();
+            else          self.open();
+            isOpen = !isOpen;
+            GLOBAL.setTimeout(toggle, 1000);
+          };
+        })();
+        // this.open();
+        toggle();
+      }
+    },
+    {
+      name: 'initInnerHTML',
+      code: function() {
+        this.SUPER.apply(this, arguments);
+        this.animation && this.animation.initInnerHTML &&
+            this.animation.initInnerHTML();
       }
     },
     {
       name: 'open',
       code: function() {
         this.className = '';
-        this.openAnimation();
+        if ( this.animation ) {
+          this.animation.setInitialPosition &&
+              this.animation.setInitialPosition(true);
+          this.animation.openAnimation &&
+              this.animation.openAnimation();
+        }
       }
     },
     {
       name: 'close',
       code: function() {
         this.className = '';
-        this.closeAnimation();
+        if ( this.animation ) {
+          this.animation.setInitialPosition &&
+              this.animation.setInitialPosition(false);
+          this.animation.closeAnimation &&
+              this.animation.closeAnimation();
+        }
+      }
+    },
+    {
+      name: 'getPositionedAnimation',
+      code: function(positionPropName, isOpen) {
+        return Movement.animate(
+            isOpen ? this.openDuration : this.closeDuration,
+            function() {
+              if ( isOpen ) {
+                // this[positionPropName] = 0;
+              } else {
+                var parent = this.cssPosition === 'fixed' ?
+                    this.document.body : this.$.parentNode;
+                this[positionPropName] =
+                    (positionPropName.indexOf('left') >= 0 ||
+                    positionPropName.indexOf('right' >= 0)) ?
+                    parent.clientWidth : parent.clientHeight;
+              }
+              if ( this.overlayView )
+                this.overlayView.alpha = isOpen ? this.overlayView.openAlpha : 0;
+            }.bind(this),
+            this.animationEase,
+            function() {
+              this.className = isOpen ? 'open' : 'closed';
+            }.bind(this));
       }
     }
   ],
@@ -137,17 +231,20 @@ CLASS({
     function toInnerHTML() {/*
       %%overlayView
       <popup-container id="{{this.id}}-container">
-        %%delegateView
+        <popup-view id="{{this.id}}-view">
+          %%delegateView
+        </popup-view>
       </popup-container>
     */},
     function CSS() {/*
       popup.closed { display: none; }
+      popup.hidden { visibility: hidden; }
       popup {
         display: block;
         z-index: 100;
       }
       popup-container {
-        position: fixed;
+        position: inherit;
         top: 0px;
         left: 0px;
         right: 0px;
@@ -156,6 +253,9 @@ CLASS({
         justify-content: center;
         align-items: center;
         z-index: 1000;
+      }
+      popup-view {
+        display: block;
       }
     */}
   ]
