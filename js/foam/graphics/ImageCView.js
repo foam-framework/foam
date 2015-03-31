ebecffb6c8ae5b8fdfa87b960086a4f1e96720e3
kgrgreer <kgrgreer@gmail.com>
2015-03-31 09:39:42 -0400

    Converted ImageCView to foam.graphics.

---
+++ js/foam/graphics/ImageCView.js
@@ -0,0 +1,45 @@
+/**
+ * @license
+ * Copyright 2015 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+CLASS({
+  package: 'foam.graphics',
+  name: 'ImageCView',
+  extendsModel: 'foam.graphics.CView',
+
+  properties: [
+    {
+      name: 'src',
+      label: 'Source'
+    }
+  ],
+
+  methods: {
+    initCView: function() {
+      this.SUPER();
+      this.image_ = new Image();
+      this.image_.onload = function() {
+        this.view.paint();
+      }.bind(this);
+      this.image_.src = this.src;
+      this.view.paint();
+    },
+
+    paintSelf: function() {
+      this.canvas.drawImage(this.image_, 0, 0);
+    }
+  }
+});
