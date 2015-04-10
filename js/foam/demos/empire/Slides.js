5ba27efe2a03b331e4a751843aac4023a259ebe0
kgrgreer <kgrgreer@gmail.com>
2015-04-10 15:06:03 -0400

    INitial check-in of empire slides.

---
+++ js/foam/demos/empire/Slides.js
@@ -0,0 +1,43 @@
+/**
+ * @license
+ * Copyright 2015 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the License);
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+CLASS({
+  package: 'foam.demos.empire',
+  name: 'Slides',
+  extendsModel: 'foam.flow.Section',
+
+  constants: { ELEMENT_NAME: 'foam-demos-flow' },
+
+  requires: [
+    'foam.graphics.Circle',
+    'foam.flow.CodeSample',
+    'foam.flow.Slides'
+  ],
+
+  methods: {
+    init: function() {
+      this.SUPER.apply(this, arguments);
+      this.X.registerElement('circle', 'foam.graphics.Circle');
+    }
+  },
+
+  templates: [
+    function CSS() {/*
+      deck {
+        padding-left: 16px;
+      }
+      h1 {
+        margin-top: 16px;
+      }
+    */},
+    { name: 'toHTML' }
+  ]
+});
