cc0c646f1bbfb09eff870203ef7a4bd662973e0a
Braden Shepherdson <braden.shepherdson@gmail.com>
2015-04-26 00:53:50 +0000

    New doc browser: First hack at an alternative core for a doc browser.
    Much faster rendering, but no DocRef or Diagrams integration, incomplete and
    unstyled. It can certainly be fast, if we don't cause lots of re-rendering.

---
+++ js/foam/documentation/new/DocBrowser.js
@@ -0,0 +1,89 @@
+/**
+ * @license
+ * Copyright 2015 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+CLASS({
+  package: 'foam.documentation.new',
+  name: 'DocBrowser',
+  extendsModel: 'foam.ui.View',
+  requires: [
+    'foam.documentation.new.DocFeatureInheritanceTracker',
+    'foam.documentation.new.InheritanceEngine',
+    'foam.documentation.new.ui.ModelDocView',
+    'Model',
+    'MDAO',
+    'foam.dao.FindFallbackDAO',
+  ],
+
+  exports: [
+    'FEATURE_TYPES',
+    'documentViewRef',
+    'modelDAO',
+  ],
+
+  constants: {
+    FEATURE_TYPES: [
+      'properties',
+      'methods',
+      'listeners',
+      'actions',
+      'templates',
+      'relationships',
+    ],
+  },
+
+  properties: [
+    {
+      name: 'documentViewRef',
+      factory: function() {
+        var ref = typeof this.data === 'string' ?  this.data :
+            (this.data ? this.data.id : '');
+        return this.SimpleValue.create(ref);
+      }
+    },
+    {
+      name: 'modelDAO',
+      factory: function() {
+        return this.X.ModelDAO;
+      }
+    },
+    {
+      name: 'engine',
+      factory: function() {
+        return this.InheritanceEngine.create();
+      }
+    },
+  ],
+
+  templates: [
+    function toHTML() {/*
+      <div class="doc-browser">
+        <div id="<%= this.id %>-inner" class="doc-browser-model">
+          <% this.addInitializer(function() {
+            var ref = self.documentViewRef.get();
+            if (ref) {
+              self.engine.loadModel(ref)(function(docs) {
+                var view = self.ModelDocView.create({ data: docs });
+                self.X.$(self.id + '-inner').innerHTML = view.toHTML();
+                view.initHTML();
+              });
+            }
+          }); %>
+        </div>
+      </div>
+    */},
+  ]
+});
---
+++ js/foam/documentation/new/ui/ModelDocView.js
@@ -0,0 +1,92 @@
+/**
+ * @license
+ * Copyright 2015 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+CLASS({
+  name: 'ModelDocView',
+  package: 'foam.documentation.new.ui',
+  extendsModel: 'foam.ui.View',
+  requires: [
+    'foam.documentation.new.ui.PropertyDocView',
+    'foam.documentation.new.ui.MethodDocView',
+    /*
+    'foam.documentation.new.ui.ListenerDocView',
+    'foam.documentation.new.ui.ActionDocView',
+    'foam.documentation.new.ui.TemplateDocView',
+    'foam.documentation.new.ui.RelationshipDocView',
+    */
+  ],
+
+  imports: [
+    'documentViewRef',
+  ],
+
+  properties: [
+    {
+      name: 'data',
+      documentation: 'The documentation object returned from the ' +
+          'InheritanceEngine.',
+    },
+    {
+      model_: 'ViewFactoryProperty',
+      name: 'docView',
+      factory: function() {
+        var typeMap = {
+          properties: this.PropertyDocView,
+          methods: this.MethodDocView,
+        };
+        var Y = this.Y;
+        return function(args, opt_X) {
+          var type = args.data.type;
+          return typeMap[type].create(args, opt_X || Y);
+        };
+      }
+    },
+  ],
+
+  templates: [
+    function toHTML() {/*
+      <div id="<%= this.id %>" class="doc-model">
+        <div class="doc-header">
+          <div class="doc-header-name">
+            <%= this.data.model_.name %>
+          </div>
+          <div class="doc-header-package">
+            <%= this.data.model_.package %>
+          </div>
+          </div>
+        </div>
+        <% this.featureBlockHTML(out, 'properties'); %>
+        <% this.featureBlockHTML(out, 'methods'); %>
+      </div>
+    */},
+    function featureBlockHTML() {/*
+      <% var type = arguments[1]; %>
+      <div class="doc-section">
+        <h3><%= capitalize(type) %></h3>
+        <%
+          var map = this.data[type];
+          var keys = Object.keys(map);
+          for(var i = 0; i < keys.length; i++) {
+            var view = this.docView({ data: map[keys[i]] });
+            out(view.toHTML());
+            view.initHTML();
+          }
+        %>
+      </div>
+  */},
+  ],
+});
