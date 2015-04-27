c63400f193403a78cc33f54edd89308ddf987a3f
Jackson Stone <jacksonic@google.com>
2015-04-27 15:45:39 -0400

    Converting PropertyView to AsyncViewLoader

--- js/foam/ui/AsyncViewLoader.js
+++ js/foam/ui/AsyncViewLoader.js
@@ -41,7 +41,8 @@
     },
     {
       name: 'model',
-      label: 'View model name'
+      label: 'View model name, model definition, or JSON with a factory_ specified.',
+      defaultValue: 'foam.ui.TextFieldView'
     },
     {
       name: 'args',
@@ -52,26 +53,82 @@
       label: "Additional arguments to this.copyFrom(...) when ready."
     },
     {
-      name: 'view'
-    }
+      name: 'view',
+      type: 'foam.ui.View',
+      adapt: function(_, v) { return v.toView_(); },
+      documentation: function() {/*
+        The new sub-$$DOC{ref:'foam.ui.View'} generated for the given $$DOC{ref:'Property'}.
+      */}
+    },
   ],
 
   methods: {
     init: function() {
-      arequire(this.model, this.X)(function(m) {
-        // lookup again to ensure we get registerModel replacements
-        var v = this.X.lookup(this.model, this.X).create(this.args, this.X);
-        console.assert(v, "AsyncViewLoader failed to load ", this.model);
-        if ( this.copyFrom ) {
-          v.copyFrom(this.copyFrom);
+      this.SUPER();
+      this.construct();
+    },
+
+    overwriteArgs: function(other) { /* Override/Append to args, typically
+      used to merge in $$DOC{ref:'.model'} if it is a JSON object. */
+      for (var key in other) {
+        if ( key !== 'model' ) { // HACK: special case to ensure model-for-model works
+          this.args[key] = other[key];
         }
-        this.view = v.toView_();
-        this.addDataChild(this.view);
+      }
+    },
+
+    construct: function() {
+      if ( typeof this.model === 'string' ) { // string model name
+        return this.requireModelName();
+      }
+      if ( this.model.model_ && typeof this.model.model_ === 'string' ) { // JSON instance def'n
+        this.view = FOAM(this.model); // FOAMalize the definition
+        return this.requireViewInstance();
+      }
+      if ( this.model.model_ ) { // JSON with Model instance specified in model_
+        this.overwriteArgs(this.model);
+        this.view = this.model.model_.create(this.args, this.X); // clone-ish
+        return this.finishRender();
+      }
+      if ( this.model.factory_ ) { // JSON with string factory_ name
+        // remove 'view' from copyFrom JSON
+        this.copyFrom = {__proto__: this.copyFrom, view: undefined};
+        this.overwriteArgs(this.model);
+        return this.requireModelName(this.model.factory_);
+      }
+      if ( typeof this.model === 'function' ) {
+        this.view = this.model(this.args, this);
+        return this.finishRender();
+      }
+      if ( this.model.create ) {
+        this.view = this.model.create(this.args);
+        return this.finishRender();
+      }
+      console.warn("AsyncViewLoader: View load with invalid model. ", this.model, this.args, this.copyFrom);
+    },
+
+    requireViewInstance: function() {
+      this.view.arequire()(function(m) {
+        this.finishRender();
+      }.bind(this));
+    },
+
+    requireModelName: function(name) {
+      arequire(name, this.X)(function(m) {
+        // lookup again to ensure we get registerModel replacements
+        this.view = this.X.lookup(name, this.X).create(this.args, this.X);
         this.finishRender();
       }.bind(this));
     },
 
     finishRender: function() {
+      if ( this.copyFrom ) {
+        var vId = v.id;
+        v.copyFrom(this.copyFrom);
+        v.id = vId;
+      }
+      this.addDataChild(this.view);
+
       var el = this.X.$(this.id);
       if ( el ) {
         el.outerHTML = this.toHTML();
@@ -87,7 +144,19 @@
 
     initHTML: function() {
       this.view && this.view.initHTML();
-    }
+    },
+
+    toString: function() { /* Name info. */ return 'AsyncViewLoader(' + this.model + ', ' + this.view + ')'; },
+
+    fromElement: function(e) { /* passthru */
+      this.view.fromElement(e);
+      return this;
+    },
+
+
+
+
+
   },
 
  });
--- js/foam/ui/PropertyView.js
+++ js/foam/ui/PropertyView.js
@@ -15,14 +15,14 @@
  * limitations under the License.
  */
 
-
 CLASS({
   package: 'foam.ui',
   name: 'PropertyView',
-  extendsModel: 'foam.ui.BasePropertyView',
+  extendsModel: 'foam.ui.AsyncViewLoader',
 
   documentation: function() {/*
-    Used by $$DOC{ref:'foam.ui.DetailView'} to generate a sub-$$DOC{ref:'foam.ui.View'} for one
+    Apply this trait to a $$DOC{ref:'BaseView'} (such as $$DOC{ref:'HTMLView'}).</p>
+    <p>Used by $$DOC{ref:'foam.ui.DetailView'} to generate a sub-$$DOC{ref:'foam.ui.View'} for one
     $$DOC{ref:'Property'}. The $$DOC{ref:'foam.ui.View'} chosen can be based off the
     $$DOC{ref:'Property.view',text:'Property.view'} value, the $$DOC{ref:'.innerView'} value, or
     $$DOC{ref:'.args'}.model_.
@@ -30,36 +30,100 @@ CLASS({
 
   properties: [
     {
-      name:  'id',
-      label: 'Element ID',
-      type:  'String',
+      name: 'prop',
+      type: 'Property',
       documentation: function() {/*
-        The DOM element id for the outermost tag of
-        this $$DOC{ref:'foam.ui.View'}. Set this when creating a PropertyView.
+          The $$DOC{ref:'Property'} for which to generate a $$DOC{ref:'foam.ui.View'}.
+      */},
+      postSet: function(old, nu) {
+        if ( old && this.bound_ ) this.unbindData(this.data);
+        if ( nu && ! this.bound_ ) this.bindData(this.data);
+      }
+    },
+    {
+      name: 'data',
+      documentation: function() {/*
+          The $$DOC{ref:'.data'} for which to generate a $$DOC{ref:'foam.ui.View'}.
+      */},
+      postSet: function(old, nu) {
+        if ( old && this.bound_ ) this.unbindData(old);
+        if ( nu ) this.bindData(nu);
+      }
+    },
+    {
+      name: 'childData'
+    }
+    {
+      name: 'innerView',
+      help: 'Override for prop.view',
+      documentation: function() {/*
+        The optional name of the desired sub-$$DOC{ref:'foam.ui.View'}. If not specified,
+        prop.$$DOC{ref:'Property.view'} is used.
+      */},
+      postSet: function(old,nu) {
+        this.model = nu;
+      }
+    },
+    {
+      name: 'view',
+      type: 'foam.ui.View',
+      adapt: function(_, v) { return v && v.toView_ ? v.toView_() : v; },
+      documentation: function() {/*
+        The new sub-$$DOC{ref:'foam.ui.View'} generated for the given $$DOC{ref:'Property'}.
       */}
+    },
+    {
+      name: 'args',
+      documentation: function() {/*
+        Optional arguments to be used for sub-$$DOC{ref:'foam.ui.View'} creation.
+      */}
+    },
+    {
+      name: 'bound_',
+      model_: 'BooleanProperty',
+      defaultValue: false
     }
   ],
 
   methods: {
-    finishPropertyRender: function() {
+    unbindData: function(oldData) {
+      if ( ! this.bound_ || ! oldData || ! this.prop ) return;
+      var pValue = oldData.propertyValue(this.prop.name);
+      Events.unlink(pValue, this.childData$);
+      this.bound_ = false;
+    },
+
+    bindData: function(data) {
+      if ( this.bound_ || ! data || ! this.prop) return;
+      var pValue = data.propertyValue(this.prop.name);
+      Events.link(pValue, this.childData$);
+      this.bound_ = true;
+    },
+
+    toString: function() { /* Name info. */ return 'PropertyView(' + this.prop.name + ', ' + this.view + ')'; },
+
+    destroy: function( isParentDestroyed ) { /* Passthrough to $$DOC{ref:'.view'} */
+      // always unbind, since if our parent was the top level destroy we need
+      // to unhook if we were created as an addSelfDataChild
+      this.unbindData(this.data);
+      this.SUPER( isParentDestroyed );
+    },
+
+    construct: function() {
+      // if not bound yet and we do have data set, bind it
+      this.bindData(this.data);
       this.SUPER();
-      if ( ! this.$ ) return;
-      this.$.outerHTML = this.toInnerHTML();
-      this.initInnerHTML();
     },
 
-    toInnerHTML: function() { /* Passthrough to $$DOC{ref:'.view'} */
-      return this.view ? this.view.toHTML() : "";
+    finishRender: function() {
+      this.SUPER();
+      this.view.prop = this.prop;
     },
 
-    toHTML: function() {
-      /* If the view is ready, pass through to it. Otherwise create a place
-      holder tag with our id, which we replace later. */
-      return this.view ? this.toInnerHTML() :  ('<div id="'+this.id+'"></div>');
-    },
-
-    initHTML: function() {
-      this.view && this.view.initHTML();
+    addDataChild: function(child) {
+      Events.link(this.childData$, child.data$);
+      this.addChild(child);
     }
   }
+
 });
