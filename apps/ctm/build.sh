f45db1caea8930197e859bca4d4a0efefca6a96c
Mark Dittmer <markdittmer@google.com>
2015-06-18 09:42:20 -0400

    apps/ctm build script: build in subdirectory and minify FOAM

--- apps/ctm/build.sh
+++ apps/ctm/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 BASEDIR=$(readlink -f $(dirname "$0"))
-BUILD_DIR=~/Downloads/ctm
+BUILD_DIR="$BASEDIR"/build
 FOAM_DIR=../..
 
 pushd "$BASEDIR"
@@ -11,5 +11,9 @@ mkdir -p "$BUILD_DIR"
 
 node --harmony "$FOAM_DIR/tools/foam.js" foam.build.BuildApp appDefinition=foam.apps.ctm.CTMApp "targetPath=$BUILD_DIR"
 cp $FOAM_DIR/core/foam.css $BUILD_DIR/foam.css
+
+cd "$BUILD_DIR"
+uglifyjs -b semicolons=false,beautify=false foam.js -c unused=false > foam-min.js
+mv foam-min.js foam.js
 
 popd
