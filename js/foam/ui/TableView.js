028073b654be4677750c739a1441a8ec685a474a
kgrgreer <kgrgreer@gmail.com>
2015-06-22 17:59:28 -0400

    Changed TableView.model property to be a ModelProperty.

--- js/foam/ui/TableView.js
+++ js/foam/ui/TableView.js
@@ -55,8 +55,8 @@ CLASS({
 
   properties: [
     {
+      model_: 'ModelProperty',
       name:  'model',
-      type:  'Model',
       defaultValueFn: function() { return this.X.model ||
                                    ( this.data && this.data.model ); }
     },
