a2140e846fe32fd07ca517cb7f2d4f3d972fef82
Mark Dittmer <markdittmer@google.com>
2015-07-03 08:56:55 -0400

    ctm: Fixes for built app correctness and layout

--- js/foam/apps/ctm/TaskManagerDetailView.js
+++ js/foam/apps/ctm/TaskManagerDetailView.js
@@ -137,8 +137,6 @@ CLASS({
       </task-manager>
     */},
     function CSS() {/*
-      @import url(http://fonts.googleapis.com/css?family=Roboto:400,500);
-      @import url(https://fonts.googleapis.com/icon?family=Material+Icons);
       body, task-manager {
         width: 100%;
         height: 100%;
