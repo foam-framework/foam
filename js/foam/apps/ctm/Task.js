2ed72341b316b8c34dfcff2253eeb85f75e1dd2e
Mark Dittmer <markdittmer@google.com>
2015-07-09 15:27:47 -0400

    ctm(l): Default task icon: Use inline data

--- js/foam/apps/ctm/Task.js
+++ js/foam/apps/ctm/Task.js
@@ -38,7 +38,7 @@ CLASS({
       name: 'iconUrl',
       label: '',
       view: 'foam.ui.ImageView',
-      defaultValue: 'https://www.gstatic.com/images/icons/material/product/1x/chrome_16dp.png',
+      defaultValue: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAACkUlEQVR4AWWTTYhbRRzAfzPzPkzNNmUTbcm6yDauVVrYQlrQelj2IHiwgkfxoNKLlz3YIi14qh8H9SCCB/Ek6OLZiyc/ghUFPypSpFh7sGYbS2PWbLLm482b+duBEIL5PX4zzMDvzZvDUzCPgLp17sV7ssG4mFgrXrt+9YNPOoDwP9Tszu+bT6TxbXPOZ3ZTKQ4hgoiA94hyLbx5t9vafQewTFA/1usAqKW0ui8pfZYq1kIAhHD6AgizkDu+s270JLADoO8qx7x5/nDirWr43K75EABMZmB2jdH+0TSKP22P0+iOhIFmd+Hiz5XC6sldh3GQaANJiq6tZvmS6WTXf7FkvVzlRuJIJPIcrMX+AvC6QlAPfPRC+0hrWD7faLIYRxSPHufvZy5waXsBL7BR67H816ukgysksaBjDbibwH2qvnW2siO9NsCzl7Z56pbFvLbFG18sYB0IYBRcPN3lWOcMmgw0oAVcWtLRvrjoEYKfH6tgH3qYr5vFaYyAdcKX1/bjS2uo1KBijTIa4rwYAXgRFNA6kPBtsogWprEQnKD8zErAZ6L7WdYTEfzEj+VP99jKaGwU071Iw6n7B+NodJVpLAJjt6cQ1KEPn9vWRlWZcLK8ytnaGX66fjcA9ZU+5e7b7mjhigEhaHP5A1hR2fcbLP9afVnp6C1mKOiEE5XD+NxRuX2Z94/3QMn0dOf9JvCeyn9YJ7p8JD5o9hpE0SkQmL3pP/9y9ZEB5dRO40ykkfTlcSDXZg/kwd/sULLTuc+/EcBP/gGXZbyyNJ6PjTzNAfI7osMQ3D3R3uncuLmRO/9Sbu0Nj1AcWZ5fGQ8RT7hz+OxwMl3CQ1DJV+vAPIvX7i2t79eVreXmsAB9CM7zH+STX2CZEVcOAAAAAElFTkSuQmCC',
       hidden: true
     },
     {
