5231210610a18cab481ece20a47e6c571a5294ee
kgrgreer <kgrgreer@gmail.com>
2015-07-10 15:47:52 -0400

    U2Demo update.

--- demos/U2Demo.js
+++ demos/U2Demo.js
@@ -1,1 +1,5 @@
-E('b').c('color: ', E('font').attr('color', 'red').c('red')).write(document);
+E('b').c('bold', E('br')).write(document);
+
+E('b').c(
+  'color: ',
+  E('font').attr('color', 'red').c('red')).write(document);
