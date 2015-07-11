fb766899ebd0cb8e509d2d31bba17c3bc03777c9
Kevin Greer <kgr@chromium.org>
2015-07-11 17:17:19 -0400

    Updated U2Demo.

--- demos/U2Demo.js
+++ demos/U2Demo.js
@@ -2,4 +2,11 @@ E('b').c('bold', E('br')).write(document);
 
 E('b').c(
   'color: ',
-  E('font').attr('color', 'red').c('red')).write(document);
+  E('font').attr('color', 'red').c('red', E('br'))).write(document);
+
+var e = E('font').c('text');
+console.log('id: ', e.id);
+
+e.write(document);
+
+e.attr('color', 'orange');
