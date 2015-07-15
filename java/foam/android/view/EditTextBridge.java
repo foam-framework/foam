84b8abe5038e6eccfc71f95de2222a0a25816c3b
Braden Shepherdson <braden.shepherdson@gmail.com>
2015-07-15 13:28:36 -0400

    Android: Add EditIntBridge, IntViewBridge. Rewire EditTextBridge to share code.

--- java/foam/android/view/EditTextBridge.java
+++ java/foam/android/view/EditTextBridge.java
@@ -1,48 +1,23 @@
 package foam.android.view;
 
 import android.content.Context;
-import android.support.design.widget.TextInputLayout;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.util.AttributeSet;
 import android.widget.EditText;
 
-import foam.core.Value;
-
 /**
- * FOAM wrapper for Android's EditView.
+ * FOAM wrapper for Android's {@link EditText} for editing {@link String}s.
  */
-public class EditTextBridge extends TwoWayViewBridge<TextInputLayout, String> {
+public class EditTextBridge extends AbstractEditTextBridge<String> {
   public EditTextBridge(Context context) {
-    this(context, null);
+    super(context);
   }
   public EditTextBridge(Context context, AttributeSet attrs) {
-    view = new TextInputLayout(context);
-    EditText text = attrs == null ? new EditText(context) : new EditText(context, attrs);
-    view.addView(text);
-
-    view.setPadding(8, 16, 8, 8);
-
-    text.addTextChangedListener(new TextWatcher() {
-      @Override
-      public void afterTextChanged(Editable e) {
-        updateValueFromView(e.toString());
-      }
-
-      @Override
-      public void onTextChanged(CharSequence s, int start, int before, int count) {
-      }
-
-      @Override
-      public void beforeTextChanged(CharSequence s, int start, int before, int count) {
-      }
-    });
+    super(context, attrs);
   }
 
-  public void setValue(Value<String> v) {
-    super.setValue(v);
-    String label = getLabel(v);
-    if (label != null) view.setHint(label);
+  @Override
+  protected String convertStringToValue(String s) {
+    return s;
   }
 
   protected void updateViewFromValue() {
