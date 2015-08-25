146559cfce6f667424653a98233ea3d553c839ab
Jackson Stone <jacksonic@google.com>
2015-08-25 12:56:24 -0400

    AppBuilder fixed model preview back action

--- js/foam/apps/builder/questionnaire/ModelPreviewWizard.js
+++ js/foam/apps/builder/questionnaire/ModelPreviewWizard.js
@@ -30,17 +30,11 @@ CLASS({
   actions: [
     {
       name: 'next',
-      label: 'Done with Preview',
+      isAvailable: function() { return false; }
     },
     {
-      name: 'exit',
-      isAvailable: function() { return false; }
-    }
-  ],
-
-  methods: [
-    function onNext() {
-      // skip SUPER(), we don't want to save
+      name: 'back',
+      label: 'Done with Preview',
     }
   ],
 
