e7dd7ea757f4c6675d103de64e5a1cf18ded2142
Joel Hughes <joel.l.hughes@gmail.com>
2015-09-01 16:04:55 -0400

    KeyView.postSet if subkey not in contet, perform a find on 'value'. Stop gap until KeyView tries to determine the models primary key.

--- js/foam/ui/KeyView.js
+++ js/foam/ui/KeyView.js
@@ -35,10 +35,16 @@ CLASS({
       name: 'data',
       postSet: function(_, value) {
         var self = this;
+        this.X.lookup(this.subType).getPrototype();
         var subKey = this.X.lookup(this.subType + '.' + this.subKey);
         var sink = { put: function(o) { self.innerData = o; } };
-        if ( subKey.name === 'id' ) this.dao.find(value, sink);
-        else this.dao.where(EQ(subKey, value)).limit(1).select(sink);
+        //console.log('KeyView.data dao='+this.dao+' this.subType='+this.subType+' this.subKey='+this.subKey+' subKey='+subKey+' subKey.name='+this.subKey.name+' value='+value);
+        if (subKey) {
+          if ( subKey.name === 'id' ) this.dao.find(value, sink);
+          else this.dao.where(EQ(subKey, value)).limit(1).select(sink);
+        } else if (value) {
+          this.dao.find(value, sink);
+        }
       }
     },
     {
@@ -59,7 +65,7 @@ CLASS({
 
   methods: {
     toHTML: function() {
-      this.children = [];
+        this.children = [];
       var view = this.innerView({
         id: this.id,
         model: this.model,
