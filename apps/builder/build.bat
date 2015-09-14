ebd6fe0e95153e629227580981f8b9520259928c
Jackson Stone <jacksonic@google.com>
2015-09-14 13:50:07 -0400

    Windows: FABLE build.bat fixed file iteration

--- apps/builder/build.bat
+++ apps/builder/build.bat
@@ -1,20 +1,21 @@
 @ECHO OFF
 
 set BASEDIR=%cd%
-set BUILD_DIR="%BASEDIR%"\build
+set BUILD_DIR=%BASEDIR%\build
 set FOAM_DIR=..\..
 set APP_DEF=foam.apps.builder.App
-set BASEDIR_FILES="kiosk_view.html designer_view.html bg.js kiosk_bg.js questionnaire_bg.js config.js manifest.json _locales 128.png builder.css fonts empty.html"
+set BASEDIR_FILES=kiosk_view.html designer_view.html bg.js kiosk_bg.js questionnaire_bg.js config.js manifest.json _locales 128.png builder.css fonts empty.html
 
 
 md "%BUILD_DIR%"
 
 node --harmony "%FOAM_DIR%\tools\foam.js" foam.build.BuildApp appDefinition=%APP_DEF% "targetPath=%BUILD_DIR%"
 copy "%FOAM_DIR%\core\foam.css" "%BUILD_DIR%\foam.css"
-for /F "tokens=1* delims= " %%f in ("%BASEDIR_FILES%") do (
-  del /P /F /A "%BUILD_DIR%\%%f"
-  rmdir /S /Q "%BUILD_DIR%\%%f"
-  copy "%BASEDIR%\%%f" "%BUILD_DIR%\%%f"   
+echo "%BASEDIR_FILES%"
+for %%f in (%BASEDIR_FILES%) do (
+  del /P /F /A %BUILD_DIR%\%%f
+  rmdir /S /Q %BUILD_DIR%\%%f
+  copy %BASEDIR%\%%f %BUILD_DIR%\%%f
 )
 
 cd "%BUILD_DIR%"
