65296b2076a3644a04db6f8897cc2cc789f0aabf
Adam Van Ymeren <adamvy@google.com>
2015-09-22 12:05:32 -0400

    Cleanup ClientDAO formatting.

--- js/foam/core/dao/ClientDAO.js
+++ js/foam/core/dao/ClientDAO.js
@@ -16,149 +16,135 @@
  */
 
 CLASS({
-   "package": "foam.core.dao",
-   "name": "ClientDAO",
-   "extendsModel": "AbstractDAO",
-   "properties": [
-      {
-         "model_": "Property",
-         "name": "asend",
-         "required": true,
-         "help": "afunc used to send request to the DAOServer."
-      },
-      {
-         "model_": "Property",
-         "name": "model",
-         "required": true
-      },
-      {
-         "model_": "Property",
-         "name": "subject",
-         "factory": function () { return this.model.name + 'DAO'; }
-      }
-   ],
-   "methods": [
-      {
-         "model_": "Method",
-         "name": "oneShot_",
-         "code": function (method, params, sink) {
-      var self = this;
-      this.asend(function(resp) {
-        if ( !sink ) return;
-        if ( ! resp ) sink && sink.error && sink.error(method, params[0]);
-        if ( resp.put ) {
-          if ( resp.put.model_ )
-            self.notify_('put', [resp.put]);
-          else
-            self.notify_('put', [params[0]]);
-          sink && sink.put && sink.put(resp.put);
-        } else if ( resp.remove ) {
-          self.notify_('remove', [params[0]]);
-          sink && sink.remove && sink.remove(resp.remove);
-        } else if ( resp.error ) sink.error(resp.error);
-      }, {
-        subject: self.subject,
-        method: method,
-        params: params
-      });
+  model_: "Model",
+  package: "foam.core.dao",
+  name: "ClientDAO",
+  extendsModel: "AbstractDAO",
+  properties: [
+    {
+      name: "asend",
+      required: true,
+      help: "afunc used to send request to the DAOServer."
     },
-         "args": []
-      },
-      {
-         "model_": "Method",
-         "name": "put",
-         "code": function (obj, sink) {
-      this.oneShot_('put', [obj], sink);
+    {
+      name: "model",
+      required: true
     },
-         "args": []
-      },
-      {
-         "model_": "Method",
-         "name": "remove",
-         "code": function (obj, sink) {
-      this.oneShot_('remove', [obj], sink);
-    },
-         "args": []
-      },
-      {
-         "model_": "Method",
-         "name": "find",
-         "code": function (q, sink) {
-      this.oneShot_('find', [q], sink);
-    },
-         "args": []
-      },
-      {
-         "model_": "Method",
-         "name": "removeAll",
-         "code": function (sink, options) {
-      // If sink.remove is not defined, we can skip the expensive returning of data.
-      // If we need results back, the server returns an array of removed values.
-      var hasSink = !!(sink && sink.remove);
-      var future = afuture();
-      this.asend(function(response) {
-        if (hasSink && response) {
-          if (sink.remove) response.forEach(sink.remove);
-        }
-        sink && sink.eof && sink.eof();
-        future.set();
-      }, {
-        subject: self.subject,
-        method: 'removeAll',
-        params: [hasSink, options]
-      });
-      return future;
-    },
-         "args": []
-      },
-      {
-         "model_": "Method",
-         "name": "select",
-         "code": function (sink, options) {
-      var future = afuture();
-
-      var self = this;
-
-      // XXX: This used to be sink.model_ || Array.isArray, but that would eg.
-      // send an instance of MDAO, rather than its data.
-      if ( Expr.isInstance(sink) || Array.isArray(sink) || ! sink ) {
-        this.asend(function(response) {
-          if ( ! response ) sink && sink.error && sink.error();
-          future.set(response || sink);
+    {
+      name: "subject",
+      factory: function () { return this.model.id + 'DAO'; }
+    }
+  ],
+  methods: [
+    {
+      name: "oneShot_",
+      code: function (method, params, sink) {
+        var self = this;
+        this.asend(function(resp) {
+          if ( !sink ) return;
+          if ( ! resp ) sink && sink.error && sink.error(method, params[0]);
+          if ( resp.put ) {
+            if ( resp.put.model_ )
+              self.notify_('put', [resp.put]);
+            else
+              self.notify_('put', [params[0]]);
+            sink && sink.put && sink.put(resp.put);
+          } else if ( resp.remove ) {
+            self.notify_('remove', [params[0]]);
+            sink && sink.remove && sink.remove(resp.remove);
+          } else if ( resp.error ) sink.error(resp.error);
         }, {
           subject: self.subject,
-          method: 'select',
-          params: [sink || null, options]
+          method: method,
+          params: params
         });
-      } else {
-        var fc = this.createFlowControl_();
-
+      }
+    },
+    {
+      name: "put",
+      code: function (obj, sink) {
+        this.oneShot_('put', [obj], sink);
+      }
+    },
+    {
+      name: "remove",
+      code: function (obj, sink) {
+        this.oneShot_('remove', [obj], sink);
+      }
+    },
+    {
+      name: "find",
+      code: function (q, sink) {
+        this.oneShot_('find', [q], sink);
+      }
+    },
+    {
+      name: "removeAll",
+      code: function (sink, options) {
+        // If sink.remove is not defined, we can skip the expensive returning of data.
+        // If we need results back, the server returns an array of removed values.
+        var hasSink = !!(sink && sink.remove);
+        var future = afuture();
         this.asend(function(response) {
-          if ( ! response ) {
-            sink && sink.error && sink.error('');
-            future.set(sink);
-            return;
+          if (hasSink && response) {
+            if (sink.remove) response.forEach(sink.remove);
           }
-          for ( var i = 0; i < response.length; i++ ) {
-            if ( fc.stopped ) break;
-            if ( fc.errorEvt ) {
-              sink.error && sink.error(fc.errorEvt);
-              break;
+          sink && sink.eof && sink.eof();
+          future.set();
+        }, {
+          subject: self.subject,
+          method: 'removeAll',
+          params: [hasSink, options]
+        });
+        return future;
+      }
+    },
+    {
+      name: "select",
+      code: function (sink, options) {
+        var future = afuture();
+
+        var self = this;
+
+        // XXX: This used to be sink.model_ || Array.isArray, but that would eg.
+        // send an instance of MDAO, rather than its data.
+        if ( Expr.isInstance(sink) || Array.isArray(sink) || ! sink ) {
+          this.asend(function(response) {
+            if ( ! response ) sink && sink.error && sink.error();
+            future.set(response || sink);
+          }, {
+            subject: self.subject,
+            method: 'select',
+            params: [sink || null, options]
+          });
+        } else {
+          var fc = this.createFlowControl_();
+
+          this.asend(function(response) {
+            if ( ! response ) {
+              sink && sink.error && sink.error('');
+              future.set(sink);
+              return;
             }
-            sink.put && sink.put(response[i], null, fc);
-          }
-          sink.eof && sink.eof();
-          future.set(sink);
-        }, {
-          subject: self.subject,
-          method: 'select',
-          params: [null, options]
-        });
-      }
+            for ( var i = 0; i < response.length; i++ ) {
+              if ( fc.stopped ) break;
+              if ( fc.errorEvt ) {
+                sink.error && sink.error(fc.errorEvt);
+                break;
+              }
+              sink.put && sink.put(response[i], null, fc);
+            }
+            sink.eof && sink.eof();
+            future.set(sink);
+          }, {
+            subject: self.subject,
+            method: 'select',
+            params: [null, options]
+          });
+        }
 
-      return future.get;
-    },
-         "args": []
+        return future.get;
       }
-   ]
+    }
+  ]
 });
