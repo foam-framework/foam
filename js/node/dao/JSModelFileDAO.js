c26bdc60f8424545a19f1e9c510649fa057e6c1b
Adam Van Ymeren <adamvy@google.com>
2015-09-22 12:11:55 -0400

    Cleanup JSModelFileDAO formatting.

--- js/node/dao/JSModelFileDAO.js
+++ js/node/dao/JSModelFileDAO.js
@@ -1,6 +1,6 @@
-/**
+/*
  * @license
- * Copyright 2014 Google Inc. All Rights Reserved.
+ * Copyright 2015 Google Inc. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,86 +16,76 @@
  */
 
 CLASS({
-   "model_": "Model",
-   "id": "node.dao.JSModelFileDAO",
-   "package": "node.dao",
-   "name": "JSModelFileDAO",
-   "extendsModel": "AbstractDAO",
-   "requires": [],
-   "imports": [],
-   "exports": [],
-   "properties": [
-      {
-         "model_": "Property",
-         "name": "daoListeners_",
-         "hidden": true,
-         "transient": true,
-         "factory": function () { return []; }
-      },
-      {
-         "model_": "Property",
-         "name": "fs",
-         "factory": function () { return require('fs'); }
-      },
-      {
-         "model_": "Property",
-         "name": "path",
-         "factory": function () { return require('path'); }
-      },
-      {
-         "model_": "StringProperty",
-         "name": "prefix",
-         "defaultValueFn": function () {
+  model_: "Model",
+  package: "node.dao",
+  name: "JSModelFileDAO",
+  extendsModel: "AbstractDAO",
+  properties: [
+    {
+      name: "daoListeners_",
+      hidden: true,
+      transient: true,
+      factory: function () { return []; }
+    },
+    {
+      name: "fs",
+      factory: function () { return require('fs'); }
+    },
+    {
+      name: "path",
+      factory: function () { return require('path'); }
+    },
+    {
+      model_: "StringProperty",
+      name: "prefix",
+      defaultValueFn: function () {
         return 'js';
       }
-      }
-   ],
-   "actions": [],
-   "constants": [],
-   "methods": [
-      {
-         "model_": "Method",
-         "name": "put",
-         "code": function (obj, sink) {
-      var path =
-        this.path.normalize(this.prefix + this.path.sep +
-                            obj.package.replace(/\./g, this.path.sep) +
-                            this.path.sep + obj.name + '.js');
+    }
+  ],
+  methods: [
+    {
+      name: "put",
+      code: function (obj, sink) {
+        var path =
+            this.path.normalize(this.prefix + this.path.sep +
+                                obj.package.replace(/\./g, this.path.sep) +
+                                this.path.sep + obj.name + '.js');
 
-      var current = '';
-      var parts = path.split(this.path.sep);
-      var fs = this.fs;
-      var i = 0;
-      var self = this;
+        var current = '';
+        var parts = path.split(this.path.sep);
+        var fs = this.fs;
+        var i = 0;
+        var self = this;
 
-      var size;
-      var buffer;
+        var size;
+        var buffer;
 
-      aseq(
-        awhile(function() { return i < parts.length - 1; },
-               aseq(
-                 function(ret) {
-                   current += parts[i++] + self.path.sep;
-                   fs.stat(current, ret);
-                 },
-                 function(ret, err, data) {
-                   if ( err ) {
-                     if ( err.code == 'ENOENT' ) {
-                       fs.mkdir(current, ret);
+        aseq(
+          awhile(function() { return i < parts.length - 1; },
+                 aseq(
+                   function(ret) {
+                     current += parts[i++] + self.path.sep;
+                     fs.stat(current, ret);
+                   },
+                   function(ret, err, data) {
+                     if ( err ) {
+                       if ( err.code == 'ENOENT' ) {
+                         fs.mkdir(current, ret);
+                         return;
+                       }
+
+                       sink && sink.error && sink.error(err);
                        return;
                      }
 
-                     sink && sink.error && sink.error(err);
-                     return;
-                   }
+                     if ( ! data.isDirectory() ) {
+                       sink && sink.error && sink.error(current, ' should be a directory or not exist.');
+                       return;
+                     }
 
-                   if ( ! data.isDirectory() ) {
-                     sink && sink.error && sink.error(current, ' should be a directory or not exist.');
-                     return;
-                   }
-
-                   ret();
-                 })),
+                     ret();
+                   })),
           aseq(
             function(ret) {
               current += parts[i];
@@ -127,15 +117,14 @@ CLASS({
                     ret();
                   }))(ret);
             })
-      )(function(){sink && sink.put && sink.put(obj);});
-    },
-         "args": []
+        )(function(){sink && sink.put && sink.put(obj);});
       }
-   ],
-   "templates": [
-     {
-       name: 'apache2Header',
-       template: '/*\n * @license\n * Copyright <%= new Date().getFullYear() %> Google Inc. All Rights Reserved.\n *\n * Licensed under the Apache License, Version 2.0 (the "License");\n * you may not use this file except in compliance with the License.\n * You may obtain a copy of the License at\n *\n *     http://www.apache.org/licenses/LICENSE-2.0\n *\n * Unless required by applicable law or agreed to in writing, software\n * distributed under the License is distributed on an "AS IS" BASIS,\n * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.\n * See the License for the specific language governing permissions and\n * limitations under the License.\n */\n\n'
-     }
-   ],
+    }
+  ],
+  templates: [
+    {
+      name: "apache2Header",
+      template: "/*\u000a * @license\u000a * Copyright <%= new Date().getFullYear() %> Google Inc. All Rights Reserved.\u000a *\u000a * Licensed under the Apache License, Version 2.0 (the \"License\");\u000a * you may not use this file except in compliance with the License.\u000a * You may obtain a copy of the License at\u000a *\u000a *     http://www.apache.org/licenses/LICENSE-2.0\u000a *\u000a * Unless required by applicable law or agreed to in writing, software\u000a * distributed under the License is distributed on an \"AS IS\" BASIS,\u000a * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.\u000a * See the License for the specific language governing permissions and\u000a * limitations under the License.\u000a */\u000a\u000a"
+    }
+  ]
 });
