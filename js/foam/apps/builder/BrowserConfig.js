0fb46fc1c1b3bc039ac9342edb23367b68371bee
Mark Dittmer <markdittmer@google.com>
2015-09-28 11:27:36 -0400

    App Builder: Integrate Settings page popup into custom menu. Manipulates settings stored in Builder's persistent context. Towards #356

--- js/foam/apps/builder/BrowserConfig.js
+++ js/foam/apps/builder/BrowserConfig.js
@@ -17,21 +17,25 @@ CLASS({
   requires: [
     'foam.apps.builder.BrowserConfigCitationView',
     'foam.apps.builder.MenuView',
+    'foam.apps.builder.SettingsView',
+    'foam.apps.builder.ViewMenuItem',
     'foam.ui.DAOListView',
+    'foam.ui.md.PopupView',
   ],
   imports: [
+    'document',
     'menuSelection$',
     'menuDAO$',
   ],
 
   constants: {
-    MENU_CLOSE: ['menu-close']
+    MENU_CLOSE: [ 'menu-close' ],
   },
 
   properties: [
     {
       model_: 'FunctionProperty',
-      name: 'menuListFactory',
+      name: 'selectionListFactory',
       defaultValue: function() {
         var view = this.DAOListView.create({
           data: this.menuDAO,
@@ -46,11 +50,44 @@ CLASS({
     },
     {
       model_: 'FunctionProperty',
+      name: 'viewListFactory',
+      defaultValue: function() {
+        var view = this.DAOListView.create({
+          data: [
+            this.ViewMenuItem.create({
+              label: 'Settings',
+              viewFactory: {
+                factory_: 'foam.ui.md.PopupView',
+                cardClass: 'md-card-shell',
+                delegate: 'foam.apps.builder.SettingsView',
+              },
+            }, this.Y),
+          ].dao,
+          // TODO(markdittmer): Give this model a more general name.
+          rowView: this.BrowserConfigCitationView,
+          mode: 'read-only',
+        }, this.Y.sub({
+          selection$: this.viewSelection$,
+        }));
+        view.subscribe(view.ROW_CLICK, this.publish.bind(this, this.MENU_CLOSE));
+        return view;
+      },
+    },
+    {
+      model_: 'FunctionProperty',
       name: 'menuFactory',
       defaultValue: function() {
         return this.MenuView.create({
-          list: this.menuListFactory.bind(this),
+          selectionList: this.selectionListFactory.bind(this),
+          viewList: this.viewListFactory.bind(this),
         }, this.Y);
+      },
+    },
+    {
+      type: 'foam.apps.builder.ViewMenuItem',
+      name: 'viewSelection',
+      postSet: function(_, nu) {
+        if ( nu ) nu.viewFactory({ data: this }).open();
       },
     },
     {
