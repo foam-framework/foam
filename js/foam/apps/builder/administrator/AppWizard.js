9c3327bbaa95aa02aaebfd46e07989af3bbff6fd
Jackson Stone <jacksonic@google.com>
2015-09-28 11:38:35 -0400

    FABLE admin app cleanup

--- js/foam/apps/builder/administrator/AppWizard.js
+++ js/foam/apps/builder/administrator/AppWizard.js
@@ -20,16 +20,12 @@ CLASS({
   ],
 
   requires: [
-    'foam.apps.builder.administrator.NewDAOWizard',
-    'foam.apps.builder.administrator.NewOrExistingDAOWizard',
   ],
 
   properties: [
     {
       name: 'nextViewFactory',
-      defaultValue: {
-        factory_: 'foam.apps.builder.administrator.NewOrExistingDAOWizard',
-      },
+      defaultValue: null,
     },
     {
       name: 'title',
@@ -38,9 +34,7 @@ CLASS({
     {
       model_: 'foam.apps.builder.wizard.WizardViewFactoryProperty',
       name: 'modelViewFactory',
-      defaultValue: {
-        factory_: 'foam.apps.builder.administrator.NewOrExistingModelWizard',
-      },
+      defaultValue: null,
     },
   ],
 
