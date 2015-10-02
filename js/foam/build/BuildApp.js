db40a19aa6703ec3406230424fdb7cdb8adcc5be
Adam Van Ymeren <adamvy@google.com>
2015-10-02 13:08:37 -0400

    Revert "Remove FOAMTagView usage from foam.build.BuildApp to save payload size."
    This reverts commit 7eed5c167aabdf5fde1040b98c18aec25af01985.

--- js/foam/build/BuildApp.js
+++ js/foam/build/BuildApp.js
@@ -97,6 +97,7 @@ CLASS({
       name: 'extraModels',
       help: 'Extra models to include in the image regardless of if they were arequired or not.',
       adapt: function(_, s) { if ( typeof s === 'string' ) return s.split(','); return s; },
+      factory: function() { return ['foam.ui.FoamTagView']; }
     },
     {
       model_: 'StringArrayProperty',
@@ -459,17 +460,7 @@ CLASS({
         apar(
           function(ret) { this.buildCoreJS_(ret); }.bind(this),
           function(ret) { this.buildAppJS_(ret); }.bind(this)),
-        function(ret, corejs, appjs) {
-          appjs += "window.addEventListener('load', function() { X.writeView(";
-          if ( this.defaultView ) {
-            appjs += "X.lookup('" + this.defaultView + "').create({ data: ";
-          }
-          appjs += "X.lookup('" + this.controller + "').create()";
-          if ( this.defaultView ) {
-            appjs += "})";
-          }
-          appjs += ");});";
-
+          function(ret, corejs, appjs) {
             var file = this.File.create({
               path: this.targetPath + this.path.sep + this.jsFileName,
               contents: corejs + appjs
@@ -497,7 +488,7 @@ CLASS({
     }
   },
   templates: [
-    function HTML() {/*<html<% if ( this.appcacheManifest ) { %> manifest="%%getManifestFileName"<% } %>><head><meta charset="utf-8"><%= this.htmlHeaders.join('') %><% if ( this.includeFoamCSS ) { %><link rel="stylesheet" type="text/css" href="foam.css"/><% } %><% if ( this.icon ) { %><link rel="icon" sizes="128x128" href="<%= this.icon %>"/><% } %><script src="%%jsFileName"></script></head><body></body></html>*/},
+    function HTML() {/*<html<% if ( this.appcacheManifest ) { %> manifest="%%getManifestFileName"<% } %>><head><meta charset="utf-8"><%= this.htmlHeaders.join('') %><% if ( this.includeFoamCSS ) { %><link rel="stylesheet" type="text/css" href="foam.css"/><% } %><% if ( this.icon ) { %><link rel="icon" sizes="128x128" href="<%= this.icon %>"/><% } %><script src="%%jsFileName"></script></head><body><foam model="<%= this.controller %>"<% if ( this.defaultView ) { %> view="<%= this.defaultView %>"<% } %>></foam></body></html>*/},
     function MANIFEST() {/*CACHE MANIFEST
 # version <%= this.version %>
 <% if ( this.appDefinition ) { %># hash: <%= this.appDefinition.hashCode() %><% } %>
