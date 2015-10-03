f65c74fa026d2706485efd0dddb9786114d17d34
Kevin Greer <kgr@chromium.org>
2015-10-03 11:08:13 -0400

    WatLobby: Clock upgrade.

--- js/com/google/watlobby/Lobby.js
+++ js/com/google/watlobby/Lobby.js
@@ -151,7 +151,13 @@ CLASS({
       var foam = this.ImageCView.create({x: 10, y: this.height-60, width: 837/5, height: 269/5, src: 'img/foampowered_red.png'});
       this.addChild(foam);
 
-      var clock = this.ClockView.create({x: this.width-240, y: 240, r: (120-10)/2, scaleX: 4, scaleY: 4});
+      var clock = this.ClockView.create({
+        drawTicks: true,
+        x: this.width-250,
+        y: 250,
+        r: (120-10)/2,
+        scaleX: 4,
+        scaleY: 4});
       this.addChild(clock);
 
       this.collider.start();
