d972fc4513b2c82a068b83fe6fea849db07350bd
Jackson Stone <jacksonic@google.com>
2015-10-22 10:17:53 -0400

    Added test files for PaperWeb, added pako zlib library in requireable model

---
+++ js/com/google/paper/TestA.js
@@ -0,0 +1,40 @@
+/**
+ * @license
+ * Copyright 2015 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+CLASS({
+  package: 'com.google.paper',
+  name: 'TestA',
+
+  requires: [
+    'com.nodeca.Pako',
+  ],
+
+  properties: [
+    {
+      name: 'pako',
+      factory: function() {
+        var p = this.Pako.create();
+        return p.pako;
+      }
+    },
+    {
+      name: 'name',
+      postSet: function(old, nu) {
+        this.label = "bytes: " + nu.length +", compressed: " + this.pako.deflate(nu).length;
+      }
+    },
+    {
+      name: 'label',
+    },
+  ],
+
+
+});
---
+++ js/com/google/paper/TestAView.js
@@ -0,0 +1,31 @@
+/**
+ * @license
+ * Copyright 2015 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+CLASS({
+  package: 'com.google.paper',
+  name: 'TestAView',
+
+  extends: 'foam.ui.md.DetailView',
+
+
+  templates: [
+    function toHTML() {/*
+      <div id="%%id" <%= this.cssClassAttr() %> >
+        $$name
+        $$label
+      </div>
+    */},
+
+  ],
+
+});
+
+
