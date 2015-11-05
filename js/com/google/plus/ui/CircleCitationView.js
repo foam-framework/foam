4699097a03535705ffba99e4a06548f30c1ebf99
Jackson Stone <jacksonic@google.com>
2015-11-05 10:19:06 -0500

    plus.ShareList added

---
+++ js/com/google/plus/ui/CircleCitationView.js
@@ -0,0 +1,44 @@
+/**
+ * @license
+ * Copyright 2015 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+CLASS({
+  name: 'CircleCitationView',
+  package: 'com.google.plus.ui',
+  extends: 'foam.ui.md.DetailView',
+
+  requires: [
+    'com.google.plus.Circle'
+  ],
+
+  properties: [
+    ['className', 'plus-circle-citation-view'],
+
+  ],
+
+  templates: [
+    function toHTML() {/*
+      <div id="%%id" <%= this.cssClassAttr() %> >
+        $$displayName{ floatingLabel: false, mode: 'read-only' }
+      </div>
+    */},
+    function CSS() {/*
+      .plus-person-circle-view {
+        display: flex;
+      }
+    */},
+  ],
+});
