eff4262d0077728c141fc0b6b295a878532f67eb
Adam Van Ymeren <adamvy@google.com>
2015-11-06 11:07:09 -0500

    Update launcher to always launch a window onstartup/install/launch

--- apps/watlobby/bg.js
+++ apps/watlobby/bg.js
@@ -1,11 +1,5 @@
 var period = 1;
 
-chrome.runtime.onInstalled.addListener(function() {
-  chrome.alarms.create("update", {
-    periodInMinutes: period
-  });
-});
-
 chrome.runtime.onUpdateAvailable.addListener(function() {
   // Restart is for Kiosk apps, no-op in non-kiosk
   chrome.runtime.restart();
@@ -28,24 +22,43 @@ chrome.alarms.onAlarm.addListener(function(alarms) {
   });
 });
 
-function launchWindow() {
-  var windows = chrome.app.window.getAll();
-  for ( var i = 0 ; i < windows.length ; i++ ) {
-    windows[i].close();
-  }
+var launchWindow = (function() {
+  var launching = false;
 
-  chrome.app.window.create("main.html", {
-    frame: 'none',
-    innerBounds: {
-      left: -3240,
-      top: 0,
-      width: 2700,
-      height: 1920
+  return function() {
+    var windows = chrome.app.window.getAll();
+    for ( var i = 0 ; i < windows.length ; i++ ) {
+      windows[i].close();
     }
-  }, function(w) {
-  });
-}
+
+    if ( ! launching ) {
+      launching = true;
+      chrome.app.window.create("main.html", {
+        frame: 'none',
+        innerBounds: {
+          left: -3240,
+          top: 0,
+          width: 2700,
+          height: 1920
+        }
+      }, function(w) {
+        launching = false;
+      });
+    }
+  };
+})();
 
 chrome.app.runtime.onLaunched.addListener(function() {
+  launchWindow();
+});
+
+chrome.runtime.onInstalled.addListener(function() {
+  launchWindow();
+  chrome.alarms.create("update", {
+    periodInMinutes: period
+  });
+});
+
+chrome.runtime.onStartup.addListener(function() {
   launchWindow();
 });
