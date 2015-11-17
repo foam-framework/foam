foam-framework/foam

commit ecd2f5cf3b2114334e1171a47a5579550b0eb447

Add some swiftType to some types in mm3Types.

core/mm3Types.js
@@ -52,6 +52,10 @@ CLASS({
       help: 'The Java type of this property.'
     },
     {
+      name: 'swiftType',
+      defaultValue: 'String',
+    }
+    {
       name: 'view',
       labels: ['javascript'],
       defaultValue: 'foam.ui.TextFieldView',
@@ -245,6 +249,10 @@ CLASS({
       help: 'The Java type of this property.'
     },
     {
+      name: 'swiftType',
+      defaultValue: 'Int',
+    }
+    {
       name: 'view',
       labels: ['javascript'],
       defaultValue: 'foam.ui.IntFieldView',
@@ -340,6 +348,10 @@ CLASS({
       displayWidth: 10,
       defaultValue: 'double',
       help: 'The Java type of this property.'
+    },
+    {
+      name: 'swiftType',
+      defaultValue: 'Float',
     },
     {
       name: 'displayWidth',

commit bded9ce59c050e168f8b4c635177de7535927b41

Fix syntax error introduced in ecd2f5cf3b2114334e1171a47a5579550b0eb447

core/mm3Types.js
@@ -54,7 +54,7 @@ CLASS({
     {
       name: 'swiftType',
       defaultValue: 'String',
-    }
+    },
     {
       name: 'view',
       labels: ['javascript'],
@@ -251,7 +251,7 @@ CLASS({
     {
       name: 'swiftType',
       defaultValue: 'Int',
-    }
+    },
     {
       name: 'view',
       labels: ['javascript'],
@@ -317,7 +317,7 @@ CLASS({
     {
       name: 'swiftType',
       defaultValue: 'CLong',
-    }
+    },
   ]
 });
 