6229fd29610866c57f7a100ff118db1c7220ab31
Jackson Stone <jacksonic@google.com>
2015-11-20 17:18:54 -0500

    Hack to get updates from the server to hit clients (polling)

--- js/com/google/ow/Client.js
+++ js/com/google/ow/Client.js
@@ -236,6 +236,13 @@ CLASS({
       });
       this.Y.registerModel(WebSocket, 'foam.core.dao.WebSocketDAO');
 
+      // hack to fix missing updates when the server creates new streamDAO items
+      var pollEnvelope = this.Envelope.create({ owner: this.currentUser.id, id: 'pollEnv1212' });
+      this.X.setInterval(function() {
+        pollEnvelope.owner = this.currentUser.id;
+        this.streamDAO.put(pollEnvelope)
+      }.bind(this), 1000);
+
       // TODO(markdittmer): Bring this back once we fully u2-ify our MD styles.
       // this.SharedStyles.create(null, this.Y);
     },
