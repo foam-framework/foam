27e42f0ad7d2def79b0e2e448830e0c82715b628
Adam Van Ymeren <adamvy@google.com>
2015-11-21 19:59:58 -0500

    Revert "Cleanup formatting of EMail related models."
    This reverts commit 2e3b72d8cc9f7c36b32e1c936b3fc131deb9000c.

--- js/com/google/mail/GMailMessage.js
+++ js/com/google/mail/GMailMessage.js
@@ -1,4 +1,4 @@
-/*
+/**
  * @license
  * Copyright 2015 Google Inc. All Rights Reserved.
  *
@@ -16,50 +16,62 @@
  */
 
 CLASS({
-  model_: "Model",
-  package: "com.google.mail",
-  name: "GMailMessage",
-  plural: "messages",
-  properties: [
-    {
-      model_: "StringProperty",
-      name: "historyId",
-      help: "The ID of the last history record that modified this message."
-    },
-    {
-      model_: "StringProperty",
-      name: "id",
-      help: "The immutable ID of the message."
-    },
-    {
-      model_: "StringArrayProperty",
-      name: "labelIds",
-      help: "List of IDs of labels applied to this message."
-    },
-    {
-      name: "payload",
-      subType: "MessagePart",
-      help: "The parsed email structure in the message parts."
-    },
-    {
-      model_: "StringProperty",
-      name: "raw",
-      help: "The entire email message in an RFC 2822 formatted string. Returned in messages.get and drafts.get responses when the format=RAW parameter is supplied."
-    },
-    {
-      model_: "IntProperty",
-      name: "sizeEstimate",
-      help: "Estimated size in bytes of the message."
-    },
-    {
-      model_: "StringProperty",
-      name: "snippet",
-      help: "A short part of the message text."
-    },
-    {
-      model_: "StringProperty",
-      name: "threadId",
-      help: "The ID of the thread the message belongs to. To add a message or draft to a thread, the following criteria must be met: \u000a- The requested threadId must be specified on the Message or Draft.Message you supply with your request. \u000a- The References and In-Reply-To headers must be set in compliance with the <a href=\"https://tools.ietf.org/html/rfc2822\"RFC 2822 standard. \u000a- The Subject headers must match."
-    }
-  ]
+   "model_": "Model",
+   "id": "com.google.mail.GMailMessage",
+   "package": "com.google.mail",
+   "name": "GMailMessage",
+   "plural": "messages",
+   "properties": [
+      {
+         "model_": "StringProperty",
+         "name": "historyId",
+         "help": "The ID of the last history record that modified this message."
+      },
+      {
+         "model_": "StringProperty",
+         "name": "id",
+         "help": "The immutable ID of the message."
+      },
+      {
+         "model_": "StringArrayProperty",
+         "name": "labelIds",
+         "help": "List of IDs of labels applied to this message."
+      },
+      {
+         "model_": "Property",
+         "name": "payload",
+         "subType": "MessagePart",
+         "help": "The parsed email structure in the message parts."
+      },
+      {
+         "model_": "StringProperty",
+         "name": "raw",
+         "help": "The entire email message in an RFC 2822 formatted string. Returned in messages.get and drafts.get responses when the format=RAW parameter is supplied."
+      },
+      {
+         "model_": "IntProperty",
+         "name": "sizeEstimate",
+         "help": "Estimated size in bytes of the message."
+      },
+      {
+         "model_": "StringProperty",
+         "name": "snippet",
+         "help": "A short part of the message text."
+      },
+      {
+         "model_": "StringProperty",
+         "name": "threadId",
+         "help": "The ID of the thread the message belongs to. To add a message or draft to a thread, the following criteria must be met: \u000a- The requested threadId must be specified on the Message or Draft.Message you supply with your request. \u000a- The References and In-Reply-To headers must be set in compliance with the <a href=\"https://tools.ietf.org/html/rfc2822\"RFC 2822 standard. \u000a- The Subject headers must match."
+      }
+   ],
+   "actions": [],
+   "constants": [],
+   "messages": [],
+   "methods": [],
+   "listeners": [],
+   "templates": [],
+   "models": [],
+   "tests": [],
+   "relationships": [],
+   "issues": []
 });
--- js/com/google/mail/GMailRestDAO.js
+++ js/com/google/mail/GMailRestDAO.js
@@ -1,4 +1,4 @@
-/*
+/**
  * @license
  * Copyright 2015 Google Inc. All Rights Reserved.
  *
@@ -16,203 +16,233 @@
  */
 
 CLASS({
-  package: "com.google.mail",
-  name: "GMailRestDAO",
-  extends: "foam.core.dao.RestDAO",
-  requires: [
-    "com.google.mail.GMailHistory"
-  ],
-  imports: [
-    "ajsonp"
-  ],
-  properties: [
-    {
-      name: "daoListeners_",
-      visibility: "hidden",
-      hidden: true,
-      transient: true,
-      factory: function () { return []; }
-    },
-    {
-      name: "model",
-      label: "Type of data stored in this DAO."
-    },
-    {
-      model_: "ArrayProperty",
-      name: "paramProperties",
-      help: "Properties that are handled as separate parameters rather than in the query.",
-      subType: "Property"
-    },
-    {
-      model_: "IntProperty",
-      name: "batchSize",
-      defaultValue: 200
-    },
-    {
-      model_: "IntProperty",
-      name: "skipThreshold",
-      defaultValue: 1000
-    },
-    {
-      name: "url",
-      label: "REST API URL.",
-      defaultValue: "https://www.googleapis.com/gmail/v1/users"
-    },
-    {
-      name: "modelName",
-      defaultValueFn: function () { return this.model.plural; }
-    },
-    {
-      name: "xhr",
-      transient: true,
-      factory: function () { return this.X.XHR.create({ responseType: 'json' }, this.Y); }
-    },
-    {
-      name: "ajsonp",
-      visibility: "hidden",
-      hidden: true,
-      transient: true
-    }
-  ],
-  methods: [
-    {
-      name: "buildURL",
-      code: function () {
-        return this.url + '/me/' + this.modelName;
+   "model_": "Model",
+   "id": "com.google.mail.GMailRestDAO",
+   "package": "com.google.mail",
+   "name": "GMailRestDAO",
+   "extends": "foam.core.dao.RestDAO",
+   "requires": [
+      "com.google.mail.GMailHistory"
+   ],
+   "imports": [
+      "ajsonp"
+   ],
+   "exports": [],
+   "properties": [
+      {
+         "model_": "Property",
+         "name": "daoListeners_",
+         "hidden": true,
+         "transient": true,
+         "factory": function () { return []; }
+      },
+      {
+         "model_": "Property",
+         "name": "model",
+         "label": "Type of data stored in this DAO."
+      },
+      {
+         "model_": "ArrayProperty",
+         "name": "paramProperties",
+         "help": "Properties that are handled as separate parameters rather than in the query.",
+         "subType": "Property"
+      },
+      {
+         "model_": "IntProperty",
+         "name": "batchSize",
+         "defaultValue": 200
+      },
+      {
+         "model_": "IntProperty",
+         "name": "skipThreshold",
+         "defaultValue": 1000
+      },
+      {
+         "model_": "Property",
+         "name": "url",
+         "label": "REST API URL.",
+         "defaultValue": "https://www.googleapis.com/gmail/v1/users"
+      },
+      {
+         "model_": "Property",
+         "name": "modelName",
+         "defaultValueFn": function () { return this.model.plural; }
+      },
+      {
+         "model_": "Property",
+         "name": "xhr",
+         "transient": true,
+         "factory": function () { return this.X.XHR.create({ responseType: 'json' }, this.Y); }
+      },
+      {
+         "model_": "Property",
+         "name": "ajsonp",
+         "hidden": true,
+         "transient": true
       }
+   ],
+   "actions": [],
+   "constants": [],
+   "messages": [],
+   "methods": [
+      {
+         "model_": "Method",
+         "name": "buildURL",
+         "code": function () {
+      return this.url + '/me/' + this.modelName;
     },
-    {
-      name: "buildSelectParams",
-      code: function (sink, query) {
-        var params = [];
-        if ( CountExpr.isInstance(sink) ) params.push('maxResults=1');
-        if ( this.GMailHistory.isSubModel(this.model) &&
-             GtExpr.isInstance(query) &&
-             query.arg1 === this.GMailHistory.ID ) {
-          params.push('startHistoryId=' + query.arg2.f());
-        }
-        if ( this.model.LABEL_IDS && EqExpr.isInstance(query) &&
-             this.model.LABEL_IDS == query.arg1 ) {
-          params.push('labelIds=' + encodeURIComponent(query.arg2));
-        }
-        // TODO: Split MQLable queries apart from non MQLable queries.
-        // if ( query ) params.push('q=' + encodeURIComponent(query.toMQL()));
-        return params;
+         "args": []
+      },
+      {
+         "model_": "Method",
+         "name": "buildSelectParams",
+         "code": function (sink, query) {
+      var params = [];
+      if ( CountExpr.isInstance(sink) ) params.push('maxResults=1');
+      if ( this.GMailHistory.isSubModel(this.model) &&
+           GtExpr.isInstance(query) &&
+           query.arg1 === this.GMailHistory.ID ) {
+        params.push('startHistoryId=' + query.arg2.f());
       }
-    },
-    {
-      name: "select",
-      code: function (sink, options) {
-        sink = sink || [];
-
-        if ( options && ! CountExpr.isInstance(sink) ) sink = this.decorateSink_(sink, options);
-
-        options = options || {};
-        var params = this.buildSelectParams(sink, options.query);
-        if ( options.limit && ! options.query ) params.push('maxResults=' + options.limit);
-        var url = this.buildURL();
-
-        var fc = this.createFlowControl_();
-        var self = this;
-        var future = afuture();
-        var pageToken;
-
-        awhile(
-          function() { return ! fc.stopped && ! fc.errorEvt; },
-          aseq(
-            function(ret) {
-              var p = pageToken ? params.concat('pageToken=' + pageToken) : params;
-              self.xhr.asend(ret, url + "?" + p.join('&'));
-            },
-            function(ret, data) {
-              if ( ! data || ! data[self.modelName] ) {
-                fc.error("No data");
-                ret();
-                return;
-              }
-
-              if ( CountExpr.isInstance(sink) ) {
-                sink.count = data.resultSizeEstimate;
-                fc.stop();
-              }
-
-              var items = data[self.modelName];
-              var i = 0;
-
-              pageToken = data.nextPageToken;
-
-              awhile(function() { return i < items.length; },
-                     function(ret) {
-                       if ( fc.stopped ) {
-                         i = items.length;
-                         ret();
-                         return;
-                       }
-                       if ( fc.errorEvt ) {
-                         sink.error && sink.error(fc.errorEvt);
-                         i = items.length;
-                         ret();
-                         return;
-                       }
-                       self.onSelectData(ret, items[i++], sink, fc);
-                     })(ret);
-            },
-            function(ret) {
-              if (!pageToken) {
-                fc.stop();
-              }
-              ret();
-            }))(function() {
-              sink.eof && sink.eof();
-              future.set(sink);
-            });
-
-        return future.get;
+      if ( this.model.LABEL_IDS && EqExpr.isInstance(query) &&
+           this.model.LABEL_IDS == query.arg1 ) {
+        params.push('labelIds=' + encodeURIComponent(query.arg2));
       }
+      // TODO: Split MQLable queries apart from non MQLable queries.
+      // if ( query ) params.push('q=' + encodeURIComponent(query.toMQL()));
+      return params;
     },
-    {
-      name: "onSelectData",
-      code: function (ret, data, sink, fc) {
-        sink.put && sink.put(this.jsonToObj(data), null, fc);
-        ret();
-      }
-    },
-    {
-      name: "find",
-      code: function (key, sink, options) {
-        options = options || {};
-        var self = this;
-        var obj;
+         "args": []
+      },
+      {
+         "model_": "Method",
+         "name": "select",
+         "code": function (sink, options) {
+      sink = sink || [];
+
+      if ( options && ! CountExpr.isInstance(sink) ) sink = this.decorateSink_(sink, options);
+
+      options = options || {};
+      var params = this.buildSelectParams(sink, options.query);
+      if ( options.limit && ! options.query ) params.push('maxResults=' + options.limit);
+      var url = this.buildURL();
+
+      var fc = this.createFlowControl_();
+      var self = this;
+      var future = afuture();
+      var pageToken;
+
+      awhile(
+        function() { return ! fc.stopped && ! fc.errorEvt; },
         aseq(
           function(ret) {
-            self.xhr.asend(
-              ret,
-              self.buildFindURL(key) +
-                "?" + self.buildFindParams(options.urlParams).join('&'));
+            var p = pageToken ? params.concat('pageToken=' + pageToken) : params;
+            self.xhr.asend(ret, url + "?" + p.join('&'));
           },
-          function(ret, response, xhr) {
-            if ( xhr.status < 200 || xhr.status >= 300 || ! response  ) {
-              sink && sink.error & sink.error(['Failed to find message.', xhr.status]);
+          function(ret, data) {
+            if ( ! data || ! data[self.modelName] ) {
+              fc.error("No data");
+              ret();
               return;
             }
-            obj = self.jsonToObj(response);
-            sink && sink.put && sink.put(obj);
+
+            if ( CountExpr.isInstance(sink) ) {
+              sink.count = data.resultSizeEstimate;
+              fc.stop();
+            }
+
+            var items = data[self.modelName];
+            var i = 0;
+
+            pageToken = data.nextPageToken;
+
+            awhile(function() { return i < items.length; },
+                   function(ret) {
+                     if ( fc.stopped ) {
+                       i = items.length;
+                       ret();
+                       return;
+                     }
+                     if ( fc.errorEvt ) {
+                       sink.error && sink.error(fc.errorEvt);
+                       i = items.length;
+                       ret();
+                       return;
+                     }
+                     self.onSelectData(ret, items[i++], sink, fc);
+                   })(ret);
+          },
+          function(ret) {
+            if (!pageToken) {
+              fc.stop();
+            }
             ret();
-          })(function(){});
-      }
+          }))(function() {
+            sink.eof && sink.eof();
+            future.set(sink);
+          });
+
+      return future.get;
     },
-    {
-      name: "buildFindURL",
-      code: function (key) {
-        return this.url + '/me/' + this.modelName + '/' + key;
-      }
+         "args": []
+      },
+      {
+         "model_": "Method",
+         "name": "onSelectData",
+         "code": function (ret, data, sink, fc) {
+      sink.put && sink.put(this.jsonToObj(data), null, fc);
+      ret();
     },
-    {
-      name: "buildFindParams",
-      code: function (urlParams) {
-        var params = urlParams || [];
-        return params;
+         "args": []
+      },
+      {
+         "model_": "Method",
+         "name": "find",
+         "code": function (key, sink, options) {
+      options = options || {};
+      var self = this;
+      var obj;
+      aseq(
+        function(ret) {
+          self.xhr.asend(
+            ret,
+            self.buildFindURL(key) +
+              "?" + self.buildFindParams(options.urlParams).join('&'));
+        },
+        function(ret, response, xhr) {
+          if ( xhr.status < 200 || xhr.status >= 300 || ! response  ) {
+            sink && sink.error & sink.error(['Failed to find message.', xhr.status]);
+            return;
+          }
+          obj = self.jsonToObj(response);
+          sink && sink.put && sink.put(obj);
+          ret();
+        })(function(){});
+    },
+         "args": []
+      },
+      {
+         "model_": "Method",
+         "name": "buildFindURL",
+         "code": function (key) {
+      return this.url + '/me/' + this.modelName + '/' + key;
+    },
+         "args": []
+      },
+      {
+         "model_": "Method",
+         "name": "buildFindParams",
+         "code": function (urlParams) {
+      var params = urlParams || [];
+      return params;
+    },
+         "args": []
       }
-    }
-  ]
+   ],
+   "listeners": [],
+   "templates": [],
+   "models": [],
+   "tests": [],
+   "relationships": [],
+   "issues": []
 });
