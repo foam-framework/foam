ecbd8f83197fb244a460f13c351acb00d5b86efd
Kevin Greer <kgr@chromium.org>
2015-12-09 13:35:12 -0500

    U2: Added focus/blur support to Element.

--- js/foam/u2/Element.js
+++ js/foam/u2/Element.js
@@ -83,6 +83,7 @@ CLASS({
         }
 
         this.visitChildren('load');
+        if ( this.focused ) this.id$el.focus();
       },
       unload: function() {
         this.state = this.UNLOADED;
@@ -148,7 +149,6 @@ CLASS({
         this.id$el[key] = value;
       },
       onAddChildren: function() {
-        console.log('onAddChildren: ', arguments);
         var e = this.id$el;
         if ( ! e ) {
           console.warn('Missing Element: ', this.id);
@@ -307,6 +307,10 @@ CLASS({
       }
     },
     {
+      type: 'Boolean',
+      name: 'focused'
+    },
+    {
       name: 'outerHTML',
       transient: true,
       getter: function() { return this.output(this.createOutputStream()).toString(); }
@@ -410,9 +414,12 @@ CLASS({
     // Focus
     //
     function focus() {
+      this.focused = true;
+      if ( this.state == this.LOADED ) this.id$el.focus(); 
     },
 
     function blur() {
+      this.focused = false;
     },
 
     //
