foam-framework/foam

commit b428f48e35840d00a3991a6492371249c0f5bb12

U2 DAOController: Delay back/save actions to let data settle first. Fixes #495
This fixes problems where a field is changed but not blurred when the action is
clicked.

js/foam/u2/DAOUpdateController.js
@@ -29,6 +29,7 @@ CLASS({
   ],
 
   imports: [
+    'document',
     'stack',
   ],
 
@@ -90,6 +91,20 @@ CLASS({
     {
       name: 'back',
       ligature: 'arrow_back',
+      code: function() {
+        var active = this.document.activeElement;
+        active && active.blur();
+        // The blur above might have triggered data to update, so we call
+        // doBack(), which is framed. That gives the data time to settle.
+        this.doBack();
+      }
+    },
+  ],
+
+  listeners: [
+    {
+      name: 'doBack',
+      framed: true,
       code: function() {
         this.stack.popView();
       }

js/foam/u2/DAOCreateController.js
@@ -28,6 +28,7 @@ CLASS({
   ],
 
   imports: [
+    'document',
     'stack',
   ],
 
@@ -91,6 +92,21 @@ CLASS({
       name: 'save',
       ligature: 'check',
       code: function() {
+        var active = this.document.activeElement;
+        active && active.blur();
+        // Framed to allow any last-second data updates to propagate.
+        // They might be caused by the blur above, and we want to wait for the
+        // data to propagate.
+        this.doBack();
+      }
+    },
+  ],
+
+  listeners: [
+    {
+      name: 'doBack',
+      framed: true,
+      code: function() {
         this.dao.put(this.data, {
           put: function() {
             this.stack.popView();
@@ -99,6 +115,7 @@ CLASS({
       }
     },
   ],
+
 
   methods: [
     function initE() {

commit ac857b64adf50e353c4f5a7eba2fa5c44bc69f18

EF: Fix delete action of DAOUpdateController broken by b428f48e35840d00a3991a6492371249c0f5bb12

js/foam/u2/DAOUpdateController.js
@@ -99,16 +99,6 @@ CLASS({
         this.doBack();
       }
     },
-  ],
-
-  listeners: [
-    {
-      name: 'doBack',
-      framed: true,
-      code: function() {
-        this.stack.popView();
-      }
-    },
     {
       name: 'delete',
       ligature: 'delete',
@@ -121,6 +111,16 @@ CLASS({
             // TODO:
           }
         });
+      }
+    }
+  ],
+
+  listeners: [
+    {
+      name: 'doBack',
+      framed: true,
+      code: function() {
+        this.stack.popView();
       }
     }
   ],