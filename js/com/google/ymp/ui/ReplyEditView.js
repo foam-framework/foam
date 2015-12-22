babe2fa42bec4d409ae066f22b28b6c8c3ebb858
Jackson Stone <jacksonic@google.com>
2015-12-22 13:31:26 -0500

    YMP: Missing ReplyEditView

---
+++ js/com/google/ymp/ui/ReplyEditView.js
@@ -0,0 +1,32 @@
+/**
+ * @license
+ * Copyright 2015 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+CLASS({
+  package: 'com.google.ymp.ui',
+  name: 'ReplyEditView',
+  extends: 'foam.u2.View',
+  templates: [
+    function initE() {/*#U2
+      <div class="$">
+        <p>Reply</p>
+        <:content displayHeight="4" />
+      </div>
+    */},
+    function CSS() {/*
+      $ {
+        display: flex;
+        flex-direction: column;
+        overflow: hidden;
+        padding: 8px;
+      }
+    */},
+  ]
+});
