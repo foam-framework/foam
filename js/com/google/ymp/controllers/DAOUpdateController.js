90f6fae1eb14cca830f26d4a72656e3b72e0421b
Mark Dittmer <markdittmer@google.com>
2016-01-05 14:04:23 -0500

    YMp: Make currently viewed postId memorable for deep linking.

---
+++ js/com/google/ymp/controllers/DAOUpdateController.js
@@ -0,0 +1,53 @@
+/**
+ * @license
+ * Copyright 2016 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+CLASS({
+  package: 'com.google.ymp.controllers',
+  name: 'DAOUpdateController',
+  extends: 'foam.u2.DAOUpdateController',
+
+  imports: [
+    'postId$',
+  ],
+
+  properties: [
+    {
+      type: 'String',
+      name: 'postId',
+    },
+  ],
+
+  listeners: [
+    {
+      name: 'doBack',
+      framed: true,
+      code: function() {
+        this.postId = '';
+        this.stack.popView();
+      },
+    },
+  ],
+
+  templates: [
+    function CSS() {/*
+      $-body {
+        overflow-x: hidden;
+        overflow-y: auto;
+        height: 100%;
+      }
+      ^body {
+        overflow-x: hidden;
+        overflow-y: auto;
+        height: 100%;
+      }
+    */},
+  ],
+});
