8c569c0d0fccf320f43ebba5746273e005ac20ab
kgrgreer <kgrgreer@gmail.com>
2016-01-26 16:59:18 -0500

    FO3: Moved tests to fo3test.js.

--- core/experimental/fo3.js
+++ core/experimental/fo3.js
@@ -447,58 +447,6 @@ MODEL({
 });
 
 
-// Tests:
-
-CLASS({
-  name: 'Person',
-
-  constants: [
-    {
-      name: 'KEY',
-      value: 'value'
-    }
-  ],
-
-  properties: [
-    {
-      name: 'name'
-    },
-    {
-      name: 'age'
-    }
-  ],
-
-  methods: [
-    {
-      name: 'sayHello',
-      code: function() { console.log('Hello World!'); }
-    },
-    function sayGoodbye() { console.log('Goodbye from ' + this.name); }
-  ]
-});
-
-var p = Person.create({name: 'Adam', age: 0});
-console.log(p.name, p.age, p.KEY);
-p.sayHello();
-p.sayGoodbye();
-
-
-/*
-// 3058ms, Jan 26, 2016, X1 Carbon
-console.time('b1');
-for ( var i = 0 ; i < 10000000 ; i++ )
-  p.age++;
-console.timeEnd('b1');
-
-
-// 1251ms, Jan 26, 2016, X1 Carbon
-console.time('b2');
-for ( var i = 0 ; i < 1000000 ; i++ )
-  Person.create({name: 'john', age: i});
-console.timeEnd('b2');
-*/
-
-
 /*
   Notes:
 
