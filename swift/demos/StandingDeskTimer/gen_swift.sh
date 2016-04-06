393749f93d542f02dd81a197c3c4126985acfa0f
Mike Carcasole <mcarcaso@google.com>
2016-04-05 22:08:07 -0400

    Create a Swift demo app of foam.demos.StandingDeskTimer.

---
+++ swift/demos/StandingDeskTimer/gen_swift.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+# Generates Swift classes from FOAM models and builds the xcode file.
+# For local development, use this instead of rungyp.sh
+
+cd $(dirname $0) || exit 1
+
+# Generate the FOAM classes.
+mkdir -p Generated
+rm Generated/*
+
+CLASS_PATH=./js
+
+node --harmony ../../../tools/foam.js \
+     --classpath "$CLASS_PATH" \
+     --flags debug,swift,compiletime \
+      foam.tools.GenSwift \
+      outfolder="Generated" \
+      names=" \
+        foam.demos.StandingDeskTimer \
+        foam.swift.ui.FoamUILabel \
+        foam.swift.ui.FoamUITextField \
+        foam.swift.ui.FoamFloatUITextField \
+        StandingDeskTimerDetailView \
+      "
