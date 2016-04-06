b578f556d01468b4c8eb5616d7bbb383c0349a97
Mike Carcasole <mcarcaso@google.com>
2016-04-06 12:43:21 -0400

    Update swift StandingDeskTimer build file to not explicitly require DetailView components.

--- swift/demos/StandingDeskTimer/gen_swift.sh
+++ swift/demos/StandingDeskTimer/gen_swift.sh
@@ -18,10 +18,5 @@ node --harmony ../../../tools/foam.js \
       outfolder="Generated" \
       names=" \
         foam.demos.StandingDeskTimer \
-        foam.swift.ui.FoamUILabel \
-        foam.swift.ui.FoamEnumUILabel \
-        foam.swift.ui.FoamUISwitch \
-        foam.swift.ui.FoamUITextField \
-        foam.swift.ui.FoamFloatUITextField \
         StandingDeskTimerDetailView \
       "
