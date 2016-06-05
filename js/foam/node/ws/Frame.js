23819d87868685e0fe006562643656433e144cec
Adam Van Ymeren <adamvy@google.com>
2016-06-05 13:02:03 -0400

    Convert WebSocket Frame reading to be a per-byte state machine for reading headers.
    This prevents errors when a partial header is received off of the wire.

--- js/foam/node/ws/Frame.js
+++ js/foam/node/ws/Frame.js
@@ -30,6 +30,16 @@ CLASS({
     ['bufferPos', 0],
     ['needed', 0],
     {
+      name: 'headerState',
+      postSet: function(old, f) {
+        if ( typeof f !== 'function' ) {
+          console.log("Set from", old, "to", f);
+        }
+      }
+    },
+    'state',
+    ['length_', 0],
+    {
       type: 'Boolean',
       name: 'framing',
       defaultValue: true
@@ -41,6 +51,12 @@ CLASS({
     },
   ],
   methods: [
+    function init() {
+      console.log("Setting header state");
+      this.headerState = this.frameHeader;
+      console.log("Set to", this.headerState);
+      this.state = this.readHeader;
+    },
     function toData() {
       this.length = this.buffer.length;
       var headerSize = this.buffer.length > 65535 ? 10 :
@@ -78,78 +94,123 @@ CLASS({
 
       return buffer;
     },
-    function onData(data, i) {
-      if ( this.framing ) {
-        var byte = data.readUInt8(i++);
-        this.opcode = byte & 0x0f;
-        this.fin = !! ( byte & 0x80 );
-        this.rsv1 = !! ( byte & 0x40 );
-        this.rsv2  = !! ( byte & 0x20 );
-        this.rsv3 = !! ( byte & 0x10 );
 
-        byte = data.readUInt8(i++);
-        this.mask = !! ( byte & 0x80 );
-        var length = byte & 0x7f;
+    function frameHeader(byte) {
+      this.opcode = byte & 0x0f;
+      this.fin = !! ( byte & 0x80 );
+      this.rsv1 = !! ( byte & 0x40 );
+      this.rsv2  = !! ( byte & 0x20 );
+      this.rsv3 = !! ( byte & 0x10 );
 
-        if ( length == 126 ) {
-          length = 0;
-          byte = data.readUInt8(i++);
-          length += byte << 8;
-          byte = data.readUInt8(i++);
-          length += byte;
-        } else if ( length == 127 ) {
-          length = 0;
-          var tolarge = false;
-          byte = data.readUInt8(i++);
-          if ( byte !== 0 ) tolarge = true;
-          //length += byte << 56;
+      this.headerState = this.maskLength0;
+    },
 
-          byte = data.readUInt8(i++);
-          if ( byte !== 0 ) tolarge = true;
-          //length += byte << 48;
+    function maskLength0(byte) {
+      this.mask = !! ( byte & 0x80 );
+      this.length_ = byte & 0x7f;
 
-          byte = data.readUInt8(i++);
-          if ( byte !== 0 ) tolarge = true;
-          //length += byte << 40;
-
-          byte = data.readUInt8(i++);
-          if ( byte !== 0 ) tolarge = true;
-          //length += byte << 32;
-
-          byte = data.readUInt8(i++);
-          length += byte << 24;
-          byte = data.readUInt8(i++);
-          length += byte << 16;
-          byte = data.readUInt8(i++);
-          length += byte << 8;
-          byte = data.readUInt8(i++);
-          length += byte;
-
-          if ( tolarge ) {
-            console.error("Payload too large.");
-            this.socket.end();
-            return;
-          }
-        }
-        this.length = length;
-        this.buffer = new Buffer(this.length);
-        this.bufferPos = 0;
-        this.needed = this.length;
-        this.framing = false;
-
-        if ( this.mask ) {
-          this.masking_key = [];
-          byte = data.readUInt8(i++);
-          this.masking_key.push(byte);
-          byte = data.readUInt8(i++);
-          this.masking_key.push(byte);
-          byte = data.readUInt8(i++);
-          this.masking_key.push(byte);
-          byte = data.readUInt8(i++);
-          this.masking_key.push(byte);
-        }
+      if ( this.length_ == 126 ) {
+        this.headerState = this.lengthShort0;
+      } else if ( this.length_ === 127 ) {
+        this.headerState = this.lengthShort1;
+      } else {
+        this.headerState = this.maskingKey0;
       }
+    },
 
+    function lengthShort0(byte) {
+      this.length_ = 0;
+      this.length_ += byte << 8;
+      this.headerState = this.lengthShort1;
+    },
+
+    function lengthShort1(byte) {
+      this.length_ += byte;
+      this.headerState = this.maskingKey0;
+    },
+
+    function lengthLong0(byte) {
+      this.length_ = 0;
+      if ( byte !== 0 ) this.state = this.tooLarge;
+      this.headerState = this.lengthLong1;
+    },
+
+    function lengthLong1(byte) {
+      if ( byte !== 0 ) this.state = this.tooLarge;
+      this.headerState = this.lengthLong2;
+    },
+
+    function lengthLong2(byte) {
+      if ( byte !== 0 ) this.state = this.tooLarge;
+      this.headerState = this.lengthLong3;
+    },
+
+    function lengthLong3(byte) {
+      if ( byte !== 0 ) this.state = this.tooLarge;
+      this.headerState = this.lengthLong4;
+    },
+
+    function lengthLong4(byte) {
+      this.length_ += byte << 24;
+      this.headerState = this.lengthLong5;
+    },
+
+    function lengthLong5(byte) {
+      this.length_ += byte << 16;
+      this.headerState = this.lengthLong6;
+    },
+
+    function lengthLong6(byte) {
+      this.length_ += byte << 8;
+      this.headerState = this.lengthLong7;
+    },
+
+    function lengthLong7(byte) {
+      this.length_ += byte;
+      this.headerState = this.maskingKey0;
+    },
+
+    function maskingKey0(byte) {
+      this.length = this.length_
+      this.buffer = new Buffer(this.length);
+      this.bufferPos = 0;
+      this.needed = this.length;
+
+      if ( this.mask ) {
+        this.masking_key = [];
+        this.masking_key.push(byte);
+        this.headerState = this.maskingKey1;
+      } else {
+        this.headerState = this.frameHeader;
+        this.state = this.readData;
+      }
+    },
+
+    function maskingKey1(byte) {
+      this.masking_key.push(byte);
+      this.headerState = this.maskingKey2;
+    },
+
+    function maskingKey2(byte) {
+      this.masking_key.push(byte);
+      this.headerState = this.maskingKey3;
+    },
+
+    function maskingKey3(byte) {
+      this.masking_key.push(byte);
+      this.headerState = this.frameHeader;
+      this.state = this.readData;
+    },
+
+    function readHeader(data, i) {
+      while ( this.state === this.readHeader &&
+              i < data.byteLength ) {
+        this.headerState(data.readUInt8(i++));
+      }
+      return i;
+    },
+
+    function readData(data, i) {
       var amount = Math.min(data.length - i, this.needed);
       data.copy(this.buffer, this.bufferPos, i, i + amount);
 
@@ -168,6 +229,15 @@ CLASS({
       }
 
       return i;
+    },
+
+    function tooLarge(data, i) {
+      console.error('WebSocket payload too large');
+      this.socket.end();
+    },
+
+    function onData(data, i) {
+      return this.state(data, i);
     }
   ]
 });
