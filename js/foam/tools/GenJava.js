e5656ce831df6c2a99a7512a9192ea8ba6c0a0ad
Mike Carcasole <mikecarcasole@gmail.com>
2016-11-17 13:23:29 -0500

    A couple of changes to GenJava. Use javaClassName as the fileNameProperty when generating Java code and when generating the model map, only include Models instead of anything with a javaClassName.

--- js/foam/tools/GenJava.js
+++ js/foam/tools/GenJava.js
@@ -28,6 +28,10 @@ CLASS({
       factory: function() { return this.JavaSource2.create(); },
     },
     {
+      name: 'fileNameProperty',
+      defaultValue: 'javaClassName',
+    },
+    {
       name: 'fileExtension',
       defaultValue: 'java',
     },
@@ -56,7 +60,7 @@ import java.util.HashMap;
 public class FoamModelMap {
   public static Model get(String modelId) {
     switch (modelId) {
-<% models.filter(function(m) { return !!m.javaClassName; }).forEach(function(m) { %>
+<% models.filter(function(m) { return m.model_.name == 'Model'; }).forEach(function(m) { %>
       case "<%= m.package %><%= m.package ? '.' : ''%><%= m.name%>":
         return <%= m.package || util.template.defaultPackage  %>
             .<%= m.javaClassName %>
