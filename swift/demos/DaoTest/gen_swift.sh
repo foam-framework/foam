d24d9aa10f6caff3298917406c94c5c9c74506e9
Mike Carcasole <mikecarcasole@gmail.com>
2016-12-09 11:11:27 -0500

    Fill in the swiftCode for the DaoTest that was originally made for java and make a playground that uses it.

---
+++ swift/demos/DaoTest/gen_swift.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Generates Swift classes from FOAM models.
+
+cd $(dirname $0) || exit 1
+
+# Generate the FOAM classes.
+rm DaoPlayground.playground/Sources/*
+cp ../../foam/* DaoPlayground.playground/Sources/
+
+CLASS_PATH=../../../java/demos/DaoTest/js
+
+node --harmony ../../../tools/foam.js \
+     --classpath "$CLASS_PATH" \
+     --flags debug,swift,compiletime \
+      foam.tools.GenSwift \
+      outfolder="DaoPlayground.playground/Sources" \
+      names=" \
+        foam.dao.nativesupport.ArrayDAO \
+        tests.TestModel \
+      "
